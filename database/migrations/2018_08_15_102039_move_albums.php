8403ca29e48e821191e2a151265c3b279a919634
ildyria <beviguier@gmail.com>
2018-08-15 12:48:56 +0200

    objects...

--- database/migrations/2018_08_15_102039_move_albums.php
+++ database/migrations/2018_08_15_102039_move_albums.php
@@ -16,11 +16,15 @@ class MoveAlbums extends Migration
         $results = DB::table('lychee_albums')->select('*')->get();
         foreach ($results as $result)
         {
-            $id = $result['id'];
+            $id = $result->id;
             $id = substr($id,1,10);
             $id[0] = strval(intval($id[0]) % 4);
             DB::table('albums')->insert([
-                ['id' => $id, 'title' => $result['title'], 'description' => $result['description'], 'public' => $result['public'], 'visible_hidden' => $result['visible']]
+                ['id' => $id,
+                'title' => $result->title,
+                'description' => $result->description,
+                'public' => $result->public,
+                'visible_hidden' => $result->visible]
             ]);
         }
     }
--- database/migrations/2018_08_15_103716_move_photos.php
+++ database/migrations/2018_08_15_103716_move_photos.php
@@ -16,36 +16,36 @@ class MovePhotos extends Migration
         $results = DB::table('lychee_photos')->select('*')->get();
         foreach ($results as $result)
         {
-            $id = $result['id'];
+            $id = $result->id;
             $id = substr($id,1,10);
             $id[0] = strval(intval($id[0]) % 4);
 
-            $album = $result['id'];
+            $album = $result->id;
             $album = substr($album,1,10);
             $album[0] = strval(intval($album[0]) % 4);
             DB::table('photos')->insert([
                 ['id' => $id,
-                'title' => $result['title'],
-                'description' => $result['description'],
-                'url' => $result['url'],
-                'tags' => $result['tags'],
-                'public' => $result['public'],
-                'type' => $result['type'],
-                'width' => $result['width'],
-                'height' => $result['height'],
-                'size' => $result['size'],
-                'iso' => $result['iso'],
-                'aperture' => $result['aperture'],
-                'make' => $result['make'],
-                'model' => $result['model'],
-                'shutter' => $result['shutter'],
-                'focal' => $result['focal'],
-                'takestamp' => $result['takestamp'],
-                'star' => $result['star'],
-                'thumbUrl' => $result['thumbUrl'],
+                'title' => $result->title,
+                'description' => $result->description,
+                'url' => $result->url,
+                'tags' => $result->tags,
+                'public' => $result->public,
+                'type' => $result->type,
+                'width' => $result->width,
+                'height' => $result->height,
+                'size' => $result->size,
+                'iso' => $result->iso,
+                'aperture' => $result->aperture,
+                'make' => $result->make,
+                'model' => $result->model,
+                'shutter' => $result->shutter,
+                'focal' => $result->focal,
+                'takestamp' => $result->takestamp,
+                'star' => $result->star,
+                'thumbUrl' => $result->thumbUrl,
                 'album_id' => $album,
-                'checksum' => $result['checksum'],
-                'medium' => $result['medium']]
+                'checksum' => $result->checksum,
+                'medium' => $result->medium]
             ]);
         }
     }
