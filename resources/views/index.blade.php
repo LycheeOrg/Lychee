1ad7011c54d071cea95da8226d8d1b9b4048061e
vagrant <vagrant@lampready>
2019-01-06 21:30:56 -0500

    Fixes #24
    Edited default app name in `config/app.php` to 'Lychee' instead of
    'Laravel'.
    
    Edited `index.blade.php` to pull `app.name` from `config` rather than
    hardcoded to Lychee in the `<title>` tag of the head.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
 
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <title>Lychee</title>
+    <title>{{ config('app.name') }}</title>
 
     <meta name="author" content="Tobias Reich">
 
