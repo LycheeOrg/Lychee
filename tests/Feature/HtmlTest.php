5788606f4b58200be94961194ea5e9849f3e3031
ildyria <beviguier@gmail.com>
2019-02-12 15:37:25 +0100

    fix cache problem

--- tests/Feature/HtmlTest.php
+++ tests/Feature/HtmlTest.php
@@ -5,53 +5,63 @@ namespace Tests\Feature;
 use App\Configs;
 use App\Http\Controllers\SessionController;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class HtmlTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testHtml()
-    {
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+	public function testHtml()
+	{
 		// check if we can actually get a nice answer
-	    $response = $this->get('/');
-	    $response->assertOk();
+		$response = $this->get('/');
+		$response->assertOk();
 
-	    // cache config
-	    Configs::set('username', '');
-	    Configs::set('password', '');
+		$clear = false;
+		$configs = Configs::get(false);
 
-	    $configs = Configs::get(false);
+		if ($configs['password'] == '' && $configs['username'] == '') {
+			$clear = true;
+			$response = $this->post('/api/Settings::setLogin', [
+				'username' => 'lychee',
+				'password' => 'password'
+			]);
+			$response->assertSee("true");
 
-	    // only check if that is unset
-	    if($configs['password'] == '' && $configs['username'] == '')
-	    {
-		    $response = $this->post('/api/Settings::setLogin', ['username' => 'lychee', 'password' => 'password']);
-		    $response->assertSee("true");
+			$response = $this->post('/api/Session::logout');
+			$response->assertSee("true");
+		}
 
-		    $response = $this->post('/api/Session::logout');
-		    $response->assertSee("true");
+		$this->assertFalse(SessionController::noLogin());
 
-			$this->assertFalse(SessionController::noLogin());
 
-		    $response = $this->post('/api/Session::login', ['user' => 'foo', 'password' => 'bar']);
-		    $response->assertSee("false");
+		$response = $this->post('/api/Session::login', ['user'     => 'foo',
+		                                                'password' => 'bar'
+		]);
+		$response->assertSee("false");
 
-		    $response = $this->post('/api/Session::login', ['user' => 'lychee', 'password' => 'bar']);
-		    $response->assertSee("false");
+		$response = $this->post('/api/Session::login', [
+			'user'     => 'lychee',
+			'password' => 'bar'
+		]);
+		$response->assertSee("false");
 
-		    $response = $this->post('/api/Session::login', ['user' => 'foo', 'password' => 'password']);
-		    $response->assertSee("false");
+		$response = $this->post('/api/Session::login', [
+			'user'     => 'foo',
+			'password' => 'password'
+		]);
+		$response->assertSee("false");
 
-		    $response = $this->post('/api/Session::login', ['user' => 'lychee', 'password' => 'password']);
-		    $response->assertSee("true");
+		if ($clear) {
+			$response = $this->post('/api/Session::login', ['user'     => 'lychee',
+			                                                'password' => 'password'
+			]);
+			$response->assertSee("true");
 
-		    Configs::set('username', '');
-		    Configs::set('password', '');
-	    }
-    }
+			Configs::set('username', '');
+			Configs::set('password', '');
+		}
+	}
 }
