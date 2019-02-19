164535e3f6d320507c3cb2982449a8bc36e759fe
Martin Stone <martin@d7415.co.uk>
2019-02-19 19:41:41 +0000

    Add migration (untested).

---
+++ database/migrations/2019_02_19_193500_rename_justified_layout.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Configs;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeIdType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    // rename
+	    Configs::where('key','justified_layout')->update([
+			    "key" => "layout"),
+	    ]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    // rename
+	    Configs::where('key','layout')->update([
+			    "key" => "justified_layout"),
+	    ]);
+    }
+}
