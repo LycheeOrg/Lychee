359ffd128ababc07630a8e59a1721bb3dce5fbfc
ildyria <beviguier@gmail.com>
2019-03-07 12:29:04 +0100

    include social to a blade include

---
+++ resources/views/includes/socials.blade.php
@@ -0,0 +1,16 @@
+@if($infos['facebook'] != '')
+    <a href="{{ $infos['facebook'] }}" class="socialicons" id="facebook" target="_blank"></a>
+@endif
+@if($infos['flickr'] != '')
+    <a href="{{ $infos['flickr'] }}" class="socialicons" id="flickr" target="_blank"></a>
+@endif
+@if($infos['twitter'] != '')
+    <a href="{{ $infos['twitter'] }}" class="socialicons" id="twitter" target="_blank"></a>
+@endif
+@if($infos['instagram'] != '')
+    <a href="{{ $infos['instagram'] }}" class="socialicons" id="instagram" target="_blank"></a>
+@endif
+@if($infos['youtube'] != '')
+    <a href="{{ $infos['youtube'] }}" class="socialicons" id="youtube" target="_blank"></a>
+@endif
+<div style="clear: both;"></div>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -26,22 +26,7 @@
 
 
     <div id="socials">
-        @if($infos['facebook'] != '')
-            <a href="{{ $infos['facebook'] }}" class="socialicons" id="facebook" target="_blank"></a>
-        @endif
-        @if($infos['flickr'] != '')
-            <a href="{{ $infos['flickr'] }}" class="socialicons" id="flickr" target="_blank"></a>
-        @endif
-        @if($infos['twitter'] != '')
-            <a href="{{ $infos['twitter'] }}" class="socialicons" id="twitter" target="_blank"></a>
-        @endif
-        @if($infos['instagram'] != '')
-            <a href="{{ $infos['instagram'] }}" class="socialicons" id="instagram" target="_blank"></a>
-        @endif
-        @if($infos['youtube'] != '')
-            <a href="{{ $infos['youtube'] }}" class="socialicons" id="youtube" target="_blank"></a>
-        @endif
-        <div style="clear: both;"></div>
+        @include('includes.socials')
     </div><!-- socials -->
 
     @include('includes.footer')
