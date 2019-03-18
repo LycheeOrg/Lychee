0f3103a6c8fe94a839259f33694ab23500b11cc8
kamil4 <kamil.01482@iskra.name>
2019-03-17 22:25:29 -0500

    Don't fail if user.css is not writable

--- app/Http/Controllers/DiagnosticsController.php
+++ app/Http/Controllers/DiagnosticsController.php
@@ -83,8 +83,8 @@ class DiagnosticsController extends Controller
 			$errors += ['Error: \'uploads/\' is missing or has insufficient read/write privileges'];
 		}
 		if (Helpers::hasPermissions(Config::get('defines.dirs.LYCHEE_DIST').'/user.css')===false) {
-			$errors .= ('Warning: \'dist/user.css\' does not exist or has insufficient read/write privileges.' . PHP_EOL);
-			if (Helpers::hasPermissions(Config::get('defines.dirs.LYCHEE_DIST'))===false)			$errors .= ('Warning: \'dist/\' has insufficient read/write privileges.' . PHP_EOL);
+			$errors += ['Warning: \'dist/user.css\' does not exist or has insufficient read/write privileges.'];
+			if (Helpers::hasPermissions(Config::get('defines.dirs.LYCHEE_DIST'))===false)			$errors += ['Warning: \'dist/\' has insufficient read/write privileges.'];
 		}
 
 		//        if (Helpers::hasPermissions(Config::get('defines.dirs.LYCHEE_DATA'))===false)           $errors += ['Error: \'data/\' is missing or has insufficient read/write privileges'];
