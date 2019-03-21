e6d546a264c4adffc4093f487173464e4b0e4852
ildyria <beviguier@gmail.com>
2019-03-21 13:52:56 +0100

    Fixes #151 by adding a fail back to GD

--- app/Image/ImagickHandler.php
+++ app/Image/ImagickHandler.php
@@ -55,7 +55,9 @@ class ImagickHandler implements ImageHandlerInterface
 		}
 		catch (ImagickException $exception) {
 			Logs::error(__METHOD__, __LINE__, $exception->getMessage());
-			return false;
+			Logs::notice(__METHOD__, __LINE__, 'Trying with GD');
+			$gd = new GdHandler($this->compressionQuality);
+			return $gd->scale($source,$destination,$newWidth,$newHeight,$resWidth,$resHeight);
 		}
 
 		return true;
@@ -90,7 +92,9 @@ class ImagickHandler implements ImageHandlerInterface
 		}
 		catch (ImagickException $exception) {
 			Logs::error(__METHOD__, __LINE__, $exception->getMessage());
-			return false;
+			Logs::notice(__METHOD__, __LINE__, 'Trying with GD');
+			$gd = new GdHandler($this->compressionQuality);
+			return $gd->crop($source,$destination,$newWidth,$newHeight);
 		}
 
 		return true;
