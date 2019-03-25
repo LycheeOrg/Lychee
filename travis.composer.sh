bede324853f4078c859634b2496930ba3931ae34
ildyria <beviguier@gmail.com>
2019-03-25 13:41:05 +0100

    better printing

--- travis.composer.sh
+++ travis.composer.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-set -ev
+set -e
 
 if [ $TRAVIS_COMPOSER_DEV = "yes" ]
 then
+  echo "composer self-update"
   composer self-update
+  echo "composer install --no-interaction"
   composer install --no-interaction
 else
+  echo "composer self-update"
   composer self-update
+  echo "composer install --no-interaction --no-dev"
   composer install --no-interaction --no-dev
 fi
 
--- travis.tests.sh
+++ travis.tests.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-set -ev
+set -e
 
 if [ $TRAVIS_COMPOSER_DEV = "yes" ]
 then
+  echo "vendor/bin/phpunit --verbose"
   vendor/bin/phpunit --verbose
 else
   echo "vendor/bin/phpunit is not provided"
