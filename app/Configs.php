a839c59e2690f6a024f6f13037a4230792273f00
荒野無燈 <i@80x86.io>
2019-04-10 22:18:52 +0800

    app/Configs: fixup 'Creating default object from empty value' error

--- app/Configs.php
+++ app/Configs.php
@@ -100,6 +100,10 @@ class Configs extends Model
 	{
 
 		$config = Configs::where('key', '=', $key)->first();
+		//first() may return null, fixup 'Creating default object from empty value' error
+		if ($config == null) {
+			return true;
+		}
 		$config->value = $value;
 		if (!$config->save()) {
 			Logs::error(__METHOD__, __LINE__, $config->getErrors());
