b5ff9867374468da184579122de4f6b4c04dfd74
ildyria <beviguier@gmail.com>
2019-04-11 13:04:56 +0200

    Add annotation to User.php

--- app/User.php
+++ app/User.php
@@ -1,18 +1,52 @@
 <?php
+/** @noinspection PhpUndefinedClassInspection */
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property int $upload
+ * @property int $lock
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|Album[] $albums
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read Collection|Album[] $shared
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereLock($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @method static Builder|User whereUpload($value)
+ * @method static Builder|User whereUsername($value)
+ * @mixin Eloquent
+ */
 class User extends Authenticatable
 {
 	use Notifiable;
 
 	/**
 	 * The attributes that are mass assignable.
-	 *
-	 * @var array
 	 */
 	protected $fillable = [
 		'username',
@@ -21,8 +55,6 @@ class User extends Authenticatable
 
 	/**
 	 * The attributes that should be hidden for arrays.
-	 *
-	 * @var array
 	 */
 	protected $hidden = [
 		'password',
@@ -34,7 +66,9 @@ class User extends Authenticatable
 
 
 	/**
-	 * Albums owned
+	 * Return the albums owned by the user
+	 *
+	 * @return HasMany
 	 */
 	public function albums()
 	{
@@ -44,7 +78,9 @@ class User extends Authenticatable
 
 
 	/**
-	 * Albums visible (shared)
+	 * Return the albums shared to the user
+	 *
+	 * @return BelongsToMany
 	 */
 	public function shared()
 	{
