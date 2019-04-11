477760d816ac8b28ba80af8f1e8fede83abe47f5
Benoît Viguier <ildyria@users.noreply.github.com>
2019-04-11 11:08:23 +0200

    Fill created_at by the sysstamp of v3 (#185)
    * fix migration from v3

--- database/migrations/2018_08_15_102039_move_albums.php
+++ database/migrations/2018_08_15_102039_move_albums.php
@@ -1,58 +1,59 @@
 <?php
 
 use App\Album;
+use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 
 class MoveAlbums extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-    	if(count(Album::all()) == 0) {
-		    if (Schema::hasTable(env('DB_OLD_LYCHEE_PREFIX', '') . 'lychee_albums')) {
-			    $results = DB::table(env('DB_OLD_LYCHEE_PREFIX', '') . 'lychee_albums')->select('*')->get();
-			    foreach ($results as $result) {
-				    $album = new Album();
-				    $album->id = $result->id;
-				    $album->title = $result->title;
-				    $album->description = $result->description;
-				    $album->public = $result->public;
-				    $album->visible_hidden = $result->visible;
-				    $album->license = $result->license;
-				    $album->save();
-			    }
-		    } else {
-			    echo env('DB_OLD_LYCHEE_PREFIX', '') . "lychee_albums does not exists!\n";
-		    }
-	    }
-    	else
-	    {
-		    echo "albums is not empty.\n";
-	    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (count(Album::all()) == 0) {
+			if (Schema::hasTable(env('DB_OLD_LYCHEE_PREFIX', '').'lychee_albums')) {
+				$results = DB::table(env('DB_OLD_LYCHEE_PREFIX', '').'lychee_albums')->select('*')->get();
+				foreach ($results as $result) {
+					$album = new Album();
+					$album->id = $result->id;
+					$album->title = $result->title;
+					$album->description = $result->description;
+					$album->public = $result->public;
+					$album->visible_hidden = $result->visible;
+					$album->license = $result->license;
+					$album->created_at = date('Y-m-d H:i:s', $result->sysstamp);
+					$album->save();
+				}
+			}
+			else {
+				echo env('DB_OLD_LYCHEE_PREFIX', '')."lychee_albums does not exists!\n";
+			}
+		}
+		else {
+			echo "albums is not empty.\n";
+		}
+
+
+		Album::reset_takestamp();
+	}
 
 
 
-	    Album::reset_takestamp();
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-	    if(env('DB_DROP_CLEAR_TABLES_ON_ROLLBACK',false)) {
-		    if (Schema::hasTable('lychee_albums')) {
-			    DB::table('albums')->delete();
-		    }
-	    }
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		if (env('DB_DROP_CLEAR_TABLES_ON_ROLLBACK', false)) {
+			if (Schema::hasTable('lychee_albums')) {
+				DB::table('albums')->delete();
+			}
+		}
+	}
 }
