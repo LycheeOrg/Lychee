fe56a83e98a7e909ad0db7f46c01c7484743a0a4
ildyria <beviguier@gmail.com>
2019-04-11 13:01:07 +0200

    Add annotation to PageContent.php

--- app/PageContent.php
+++ app/PageContent.php
@@ -1,18 +1,53 @@
 <?php
+/** @noinspection PhpUndefinedClassInspection */
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Markdown;
 
+/**
+ * App\PageContent
+ *
+ * @property int $id
+ * @property int $page_id
+ * @property string $content
+ * @property string $class
+ * @property string $type
+ * @property int $order
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|PageContent newModelQuery()
+ * @method static Builder|PageContent newQuery()
+ * @method static Builder|PageContent query()
+ * @method static Builder|PageContent whereClass($value)
+ * @method static Builder|PageContent whereContent($value)
+ * @method static Builder|PageContent whereCreatedAt($value)
+ * @method static Builder|PageContent whereId($value)
+ * @method static Builder|PageContent whereOrder($value)
+ * @method static Builder|PageContent wherePageId($value)
+ * @method static Builder|PageContent whereType($value)
+ * @method static Builder|PageContent whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class PageContent extends Model
 {
+	/**
+	 * Return content.
+	 * It can be an image -> create a img tag, `content` is the url of the image
+	 * It can be a div -> create a div tag, `content` is then compiled from Markdown to HTML
+	 *
+	 * @return string
+	 */
 	function get_content()
 	{
 
 		$return = '';
 		if ($this->type == 'img') {
-			$return = '<div class="'.$this->class.'"><img src="'.$this->content.'"/></div>';
+			$return = '<div class="'.$this->class.'"><img src="'.$this->content.'" alt="image" /></div>';
 		}
 		elseif ($this->type == 'div') {
 			$return = '<div class="'.$this->class.'">';
