LycheeOrg/Lychee

commit 34f108f9603b8f08d24684ba5fd323130815c533

Cleaning Annotations (#188)
* remove unnecessary annotation

app/Http/Controllers/SearchController.php
@@ -1,4 +1,5 @@
 <?php
+/** @noinspection PhpUndefinedClassInspection */
 
 namespace App\Http\Controllers;
 
@@ -69,7 +70,7 @@ class SearchController extends Controller
 		for ($i = 0; $i < count($escaped_terms); ++$i) {
 			$escaped_term = $escaped_terms[$i];
 			$query = $query->Where(
-				function ($query) use ($id, $escaped_term) {
+				function (Builder $query) use ($id, $escaped_term) {
 					$query->where('title', 'like', '%'.$escaped_term.'%')
 						->orWhere('description', 'like', '%'.$escaped_term.'%')
 						->orWhere('tags', 'like', '%'.$escaped_term.'%');
@@ -92,7 +93,7 @@ class SearchController extends Controller
 		for ($i = 0; $i < count($escaped_terms); ++$i) {
 			$escaped_term = $escaped_terms[$i];
 			$query = $query->Where(
-				function ($query) use ($id, $escaped_term) {
+				function (Builder $query) use ($id, $escaped_term) {
 					$query->where('title', 'like', '%'.$escaped_term.'%')
 						->orWhere('description', 'like', '%'.$escaped_term.'%');
 				});

app/Album.php
@@ -176,8 +176,7 @@ class Album extends Model
 		$alb = $this->get_all_sub_albums();
 		$alb[] = $this->id;
 
-		/** @noinspection PhpUndefinedMethodInspection (select) */
-		$thumbs_types = Photo::select('thumbUrl', 'thumb2x', 'type')
+		$thumbs_types = Photo::select(['thumbUrl', 'thumb2x', 'type'])
 			->whereIn('album_id', $alb)
 			->orderBy('star', 'DESC')
 			->orderBy(Configs::get_value('sortingPhotos_col'), Configs::get_value('sortingPhotos_order'))
@@ -274,9 +273,7 @@ class Album extends Model
 	{
 		$album_list = $this->get_all_sub_albums([$this->id]);
 
-		/** @noinspection PhpUndefinedMethodInspection (WhereIn) */
 		$min = Photo::whereIn('album_id', $album_list)->min('takestamp');
-		/** @noinspection PhpUndefinedMethodInspection (WhereIn) */
 		$max = Photo::whereIn('album_id', $album_list)->max('takestamp');
 		$this->min_takestamp = $min;
 		$this->max_takestamp = $max;
@@ -314,19 +311,16 @@ class Album extends Model
 			->where('owner_id', '<>', $id)
 			->where('parent_id', '=', null)
 			->Where(
-				function ($query) use ($id) {
+				function (Builder $query) use ($id) {
 					// album is shared with user
-					/** @noinspection PhpUndefinedMethodInspection (whereIn) */
-					$query->whereIn('id', function ($query) use ($id) {
-						/** @noinspection PhpUndefinedMethodInspection (select) */
+					$query->whereIn('id', function (Builder $query) use ($id) {
 						$query->select('album_id')
 							->from('user_album')
 							->where('user_id', '=', $id);
 					})
 						// or album is visible to user
 						->orWhere(
-							function ($query) {
-								/** @noinspection PhpUndefinedMethodInspection (where) */
+							function (Builder $query) {
 								$query->where('public', '=', true)->where('visible_hidden', '=', true);
 							});
 				})

app/Configs.php
@@ -1,5 +1,4 @@
 <?php
-/** @noinspection PhpUndefinedClassInspection */
 
 namespace App;
 
@@ -56,12 +55,10 @@ class Configs extends Model
 		}
 
 		try {
-			/** @noinspection PhpUndefinedMethodInspection (select) */
 			$query = Configs::select([
 				'key',
 				'value'
 			]);
-			/** @noinspection PhpUndefinedMethodInspection (pluck) */
 			$return = $query->pluck('value', 'key')->all();
 
 			$return['sortingPhotos'] = 'ORDER BY '.$return['sortingPhotos_col'].' '.$return['sortingPhotos_order'];
@@ -115,7 +112,6 @@ class Configs extends Model
 			try {
 				Logs::error(__METHOD__, __LINE__, $key.' does not exist in config (local) !');
 			}
-				/** @noinspection PhpFullyQualifiedNameUsageInspection */
 			catch (Exception $e) {
 				// yeah we do nothing because we cannot do anything in that case ...  :p
 			}
@@ -138,7 +134,6 @@ class Configs extends Model
 	public static function set(string $key, $value)
 	{
 
-		/** @noinspection PhpUndefinedMethodInspection (where, first) */
 		$config = Configs::where('key', '=', $key)->first();
 
 		// first() may return null, fixup 'Creating default object from empty value' error
@@ -150,7 +145,6 @@ class Configs extends Model
 
 		$config->value = $value;
 		try {
-			/** @noinspection PhpUndefinedMethodInspection (save) */
 			$config->save();
 		}
 		catch (Exception $e) {
@@ -188,9 +182,8 @@ class Configs extends Model
 	 * @param $query
 	 * @return mixed
 	 */
-	public function scopePublic($query)
+	public function scopePublic(Builder $query)
 	{
-		/** @noinspection PhpUndefinedMethodInspection (where) */
 		return $query->where('confidentiality', '=', 0);
 	}
 
@@ -202,9 +195,8 @@ class Configs extends Model
 	 * @param $query
 	 * @return mixed
 	 */
-	public function scopeInfo($query)
+	public function scopeInfo(Builder $query)
 	{
-		/** @noinspection PhpUndefinedMethodInspection (where) */
 		return $query->where('confidentiality', '<=', 2);
 	}
 
@@ -216,9 +208,8 @@ class Configs extends Model
 	 * @param $query
 	 * @return mixed
 	 */
-	public function scopeAdmin($query)
+	public function scopeAdmin(Builder $query)
 	{
-		/** @noinspection PhpUndefinedMethodInspection (where) */
 		return $query->where('confidentiality', '<=', 3);
 	}
 }

app/Http/Controllers/AlbumController.php
@@ -1,6 +1,4 @@
 <?php
-/** @noinspection PhpUndefinedNamespaceInspection */
-
 /** @noinspection PhpUndefinedClassInspection */
 
 namespace App\Http\Controllers;

app/Http/Controllers/AlbumsController.php
@@ -1,4 +1,5 @@
 <?php
+/** @noinspection PhpUndefinedClassInspection */
 
 namespace App\Http\Controllers;
 

app/Http/Controllers/DemoController.php
@@ -3,37 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Album;
+use App\Metadata\GitHubFunctions;
+use App\ModelFunctions\AlbumFunctions;
+use App\ModelFunctions\ConfigFunctions;
+use App\ModelFunctions\SessionFunctions;
 use App\Photo;
-use Illuminate\Http\Request;
 
 
 class DemoController extends Controller
 {
-	public function js(Request $request)
+	public function js()
 	{
 		$functions = array();
+
+		$configFunctions = new ConfigFunctions();
+		$sessionFunctions =  new SessionFunctions();
+		$githubFunctions = new GitHubFunctions();
+		$albumFunctions = new AlbumFunctions();
 
 		/**
 		 * Session::init
 		 */
 
-		$session_init = new SessionController();
+		$session_init = new SessionController($configFunctions, $sessionFunctions, $githubFunctions);
 		$return_session = array();
 		$return_session['name'] = 'Session::init()';
 		$return_session['type'] = 'string';
-		$return_session['data'] = json_encode($session_init->init($request));
+		$return_session['data'] = json_encode($session_init->init());
 
 		$functions[] = $return_session;
 
 		/**
 		 * Albums::get
 		 */
-		$albums_controller = new AlbumsController();
+		$albums_controller = new AlbumsController($albumFunctions);
 
 		$return_albums = array();
 		$return_albums['name'] = 'Albums::get';
 		$return_albums['type'] = 'string';
-		$return_albums['data'] = json_encode($albums_controller->get($request));
+		$return_albums['data'] = json_encode($albums_controller->get());
 
 		$functions[] = $return_albums;
 
@@ -64,6 +72,7 @@ class DemoController extends Controller
 			$previousPhotoID = '';
 			$return_album_json['photos'] = array();
 			$photo_counter = 0;
+			/** @var Photo[] $photos */
 			$photos = $photos_sql->get();
 			foreach ($photos as $photo_model) {
 
@@ -130,6 +139,7 @@ class DemoController extends Controller
 
 		$albums = Album::where('public', '=', '1')->where('visible_hidden', '=', '1')->get();
 		foreach ($albums as $album) {
+			/** @var Photo $photo */
 			foreach ($album->photos as $photo) {
 				$return_photo = array();
 				$return_photo_json = $photo->prepareData();

app/Http/Controllers/DiagnosticsController.php
@@ -1,5 +1,6 @@
 <?php
 /** @noinspection PhpComposerExtensionStubsInspection */
+/** @noinspection PhpUndefinedClassInspection */
 
 namespace App\Http\Controllers;
 
@@ -7,6 +8,7 @@ use App\Configs;
 use App\Metadata\GitHubFunctions;
 use App\ModelFunctions\ConfigFunctions;
 use App\ModelFunctions\Helpers;
+use Exception;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
@@ -253,7 +255,7 @@ class DiagnosticsController extends Controller
 						$results = DB::select(DB::raw("select version()"));
 						$dbver = $results[0]->{'version()'};
 					}
-					catch (\Exception $e) {
+					catch (Exception $e) {
 						$dbver = 'unknown';
 					}
 					$dbtype = DB::getDriverName();

commit 3e7e93260f2d47b62976a80750e3b14af88ea78f

Fix Search (broken by #188?)

app/Http/Controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Album;
 use App\Photo;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 