2554d4288a5be9b884f177726d0b3c7513b66630
kamil4 <kamil.01482@iskra.name>
2019-04-21 07:28:52 -0500

    Check parent_id when creating albums (#211)
    * Check parent_id when creating albums

--- app/Http/Middleware/UploadCheck.php
+++ app/Http/Middleware/UploadCheck.php
@@ -66,8 +66,8 @@ class UploadCheck
      */
     public function album_check(Request $request, int $id)
     {
-        if ($request->has('albumID')) {
-            $albumID = $request['albumID'];
+        if ($request->has('albumID') || $request->has('parent_id')) {
+            $albumID = $request->has('albumID') ? $request['albumID'] : $request['parent_id'];
             if ($albumID == 'f' || $albumID == 's' || $albumID == 'r' || $albumID == 0)
                 return true;
 
