0196d50ffbcf2217c3d0f421ba83de700de44419
ildyria <beviguier@gmail.com>
2019-05-06 17:09:57 +0200

    tree display

--- app/Console/Commands/import.php
+++ app/Console/Commands/import.php
@@ -2,14 +2,17 @@
 
 namespace App\Console\Commands;
 
-use App\Configs;
+use App\Album;
 use App\Http\Controllers\ImportController;
 use App\ModelFunctions\AlbumFunctions;
-use App\ModelFunctions\Helpers;
 use App\ModelFunctions\PhotoFunctions;
-use App\Photo;
+use Exception;
+use FilesystemIterator;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Session;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
 class import extends Command
 {
@@ -18,15 +21,16 @@ class import extends Command
 	 *
 	 * @var string
 	 */
-	protected $signature = 'lychee:import {dir : directory to import} {--flatten} {--album_id= : Album ID to import to}
-							{--owner_id=0 : User}';
+	protected $signature = 'lychee:import {dir : directory to import} {--flatten} {--album_id= : Album ID to import to}';
+//							{--owner_id=0 : User}';
+// We import as admin as we are in CLI
 
 	/**
 	 * The console command description.
 	 *
 	 * @var string
 	 */
-	protected $description = 'Import a flattened directory into a directory';
+	protected $description = 'Import a flattened directory into an album';
 
 	/**
 	 * @var AlbumFunctions
@@ -37,6 +41,8 @@ class import extends Command
 	 * @var PhotoFunctions
 	 */
 	private $photoFunctions;
+
+
 
 	/**
 	 * Create a new command instance.
@@ -53,31 +59,56 @@ class import extends Command
 		$this->albumFunctions = $albumFunctions;
 	}
 
+
+
 	/**
 	 * Prints the album tree in a DFS fashion
 	 *
-	 * @param albums $photoFunctions
+	 * @param Collection $albums
+	 * @param int $parent_id
+	 * @param int $indent
+	 * @param int $map
+	 * @param bool $is_last
 	 * @return void
 	 */
-	public function print_album(\Illuminate\Database\Eloquent\Collection $albums, int $parent_id, int $indent=0)
+	public function print_album(Collection $albums, int $parent_id, int $indent = 0, int $map = 0, bool $is_last = false)
 	{
 		$child_albums = $albums->where('parent_id', '=', $parent_id);
 		$line_indent = '';
-		for ($i = 0; $i < $indent; $i++) {
-			$line_indent .= ' '; // TODO tree-style
+		$i = $indent - 1;
+		while ($i > 0) {
+			$i--;
+			if ((($map >> $i) & 1) == 1) {
+				$line_indent = '| ' . $line_indent;
+			}
+			else {
+				$line_indent = '  ' . $line_indent;
+			}
 		}
+
+		$tile = $is_last ? '└ ' : '├ ';
+
 		if ($parent_id != 0) {
 			$own_album = $albums->where('id', '=', $parent_id)->first();
-			$this->line(sprintf("%s- %s (id: %d)",
+			$this->line(sprintf("%s%s%s (id: %d)",
 				$line_indent,
+				$tile,
 				$own_album['title'],
-				$own_album['id'],
+				$own_album['id']
 			));
 		}
-		foreach ($child_albums as $album) {
-			$this->print_album($albums, $album->id, $indent + 1);
+
+		$map_next = ($map << 1) | !$is_last;
+		while ($child_albums->count()) {
+			$child_album = $child_albums->pop();
+			$this->print_album($albums, $child_album->id, $indent + 1, $map_next, $child_albums->count() == 0);
+		}
+		if ($is_last)
+		{
+			$this->line(sprintf("%s",$line_indent));
 		}
 	}
+
 
 
 	/**
@@ -91,18 +122,20 @@ class import extends Command
 		$flatten = $this->option('flatten');
 		$album_id = $this->option('album_id');
 		$owner_id = 0; // $this->option('owner_id');
-		$import_controller = new \App\Http\Controllers\ImportController($this->photoFunctions, $this->albumFunctions);
-		Session::put('UserID', $owner_id);
-;
+		$import_controller = new ImportController($this->photoFunctions, $this->albumFunctions);
+		Session::put('UserID', $owner_id);;
 
 		if ($album_id == null) {
 			$album_title = null;
-			$headers = ['Id', 'Title'];
-			$albums = \App\Album::all(['id', 'title', 'parent_id']);
+			$albums = Album::all([
+				'id',
+				'title',
+				'parent_id'
+			]);
 			$this->print_album($albums, 0 /* parent */, 0 /* indent */);
 			do {
 				if ($album_title != null) {
-					$this->line('No album ' . $album_title . ' found. Try again.');
+					$this->line('No album '.$album_title.' found. Try again.');
 				}
 				$album_title = $this->ask('album to insert into (title)?');
 				$sel_albums = $albums->where('title', '=', $album_title);
@@ -118,8 +151,8 @@ class import extends Command
 		}
 
 
-		$dir_iterator = new \RecursiveDirectoryIterator($directory, \FilesystemIterator::SKIP_DOTS);
-		$iterator = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::SELF_FIRST);
+		$dir_iterator = new RecursiveDirectoryIterator($directory, FilesystemIterator::SKIP_DOTS);
+		$iterator = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
 		foreach ($iterator as $file) {
 			if ($file->isDir()) {
 				continue;
@@ -128,7 +161,7 @@ class import extends Command
 			$prefix = "Processing file ".$file;
 			try {
 				$ret = $import_controller->photo($file, $album_id);
-			} catch (\Exception $e) {
+			} catch (Exception $e) {
 				$this->error($prefix." ERROR");
 				$this->error($e);
 				continue;
