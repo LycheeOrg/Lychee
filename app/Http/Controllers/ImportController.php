8c8f94d2a8e699cf2302af6c5c8f7846bb02f389
荒野無燈 <i@80x86.io>
2019-05-07 02:57:42 +0800

    app/Http/Controllers/ImportController.php: allow import video from server

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -52,11 +52,11 @@ class ImportController extends Controller
 	{
 		// No need to validate photo type and extension in this function.
 		// $photo->add will take care of it.
-		$info = getimagesize($path);
+		$mime = mime_content_type($path);
 
 		$nameFile = array();
 		$nameFile['name'] = $path;
-		$nameFile['type'] = $info['mime'];
+		$nameFile['type'] = $mime;
 		$nameFile['tmp_name'] = $path;
 
 		if ($this->photoFunctions->add($nameFile, $albumID) === false) {
@@ -203,8 +203,9 @@ class ImportController extends Controller
 				Logs::error(__METHOD__, __LINE__, 'Could not read file or directory ('.$file.')');
 				continue;
 			}
-			if (@exif_imagetype($file) !== false) {
-				// Photo
+			$extension = Helpers::getExtension($file, true);
+			if (@exif_imagetype($file) !== false || in_array(strtolower($extension), $this->photoFunctions->validExtensions, true)) {
+				// Photo or Video
 				$contains['photos'] = true;
 				if ($this->photo($file, $albumID) === false) {
 					$error = true;
