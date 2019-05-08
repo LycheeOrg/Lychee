cdef1833f440ca69774a7a46df69e6ba0a488ae0
ildyria <beviguier@gmail.com>
2019-05-08 14:56:33 +0200

    lets have a look...

--- tests/Feature/GithubTest.php
+++ tests/Feature/GithubTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use App\Configs;
+use App;
 use App\Metadata\GitHubFunctions;
 use App\ModelFunctions\ConfigFunctions;
 use Tests\TestCase;
@@ -20,6 +20,15 @@ class GithubTest extends TestCase
 	 */
 	private $gitHubFunctions;
 
+	private function git_path()
+	{
+		if (App::runningUnitTests()) {
+			return '.git';
+		}
+		else {
+			return '../.git';
+		}
+	}
 
 
 	/**
@@ -34,6 +43,9 @@ class GithubTest extends TestCase
 
 	public function test_github()
 	{
+		$this->branch = @file_get_contents(sprintf('%s/HEAD', $this->git_path()));
+		$this->warning($this->branch);
+
 		$git_info = $this->gitHubFunctions->get_info();
 		$this->assertEquals('', $git_info);
 	}
