d568b565e64304e7b7dfb6c84941411a75bd81e5
ildyria <beviguier@gmail.com>
2019-05-08 14:16:33 +0200

    try to see what is going wrong with CI

--- tests/Feature/DiagnosticsTest.php
+++ tests/Feature/DiagnosticsTest.php
@@ -3,6 +3,7 @@
 
 namespace Tests\Feature;
 
+use App\Logs;
 use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
@@ -19,9 +20,30 @@ class DiagnosticsTest extends TestCase
 		Session::put('login', true);
 		Session::put('UserID', 0);
 
+		$response = $this->get('/Logs');
+		$response->assertStatus(200); // code 200 something
+
+		$response = $this->post('/api/Logs');
+		$response->assertStatus(200); // code 200 something
+
+		$response = $this->post('/api/Logs::clearNoise');
+		$response->assertStatus(200); // code 200 something
+
+		$response = $this->post('/api/Logs::clear');
+		$response->assertStatus(200); // code 200 something
+
 		$response = $this->get('/Diagnostics');
 		$response->assertStatus(200); // code 200 something
 
+		// get logs
+		$logs = Logs::orderBy('id', 'ASC')->get();
+		$print = '';
+		foreach ($logs as $log) {
+			$print .= $log->created_at." -- ".str_pad($log->type, 7)." -- ".$log->function." -- ".$log->line.". -- ".$log->text."\n";
+		}
+		if ($logs != '') {
+			$this->addWarning($print);
+		}
 		$response = $this->post('/api/Diagnostics');
 		$response->assertStatus(200); // code 200 something too
 
