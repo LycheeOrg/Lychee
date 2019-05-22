ef8fea5e9e1c2eaca124324d6c989c6388e8725b
ildyria <beviguier@gmail.com>
2019-05-22 15:49:40 +0200

    oups

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -7,14 +7,15 @@
     </div>
     @endif
 
-    @if($infos['additional_footer_text'] != '')
-        <p class="personal_text">{{ $infos['additional_footer_text'] }}</p>
-    @endif
-
     @if($infos['copyright_enable'] == '1')
         <p class="home_copyright">
             {{ $locale['FOOTER_COPYRIGHT'] }} {{ $infos['owner'] }} &copy; {{ $infos['copyright_year'] }}</p>
     @endif
+
+    @if($infos['additional_footer_text'] != '')
+        <p class="personal_text">{{ $infos['additional_footer_text'] }}</p>
+    @endif
+
 
     @if($page_config['show_hosted_by'])
         <p class="hosted_by"><a target="_blank" href="https://LycheeOrg.github.io">{{ $locale['HOSTED_WITH_LYCHEE'] }}</a></p>
