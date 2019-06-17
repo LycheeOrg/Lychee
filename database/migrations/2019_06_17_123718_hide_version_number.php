c57c0b9caa89ad2c95fd7f8fed030d1707fe99b8
Benoît Viguier <ildyria@users.noreply.github.com>
2019-06-17 16:17:58 +0200

    Hide version (#288)
    * hide version if not provided
    * synch Lychee-front

---
+++ database/migrations/2019_06_17_123718_hide_version_number.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Configs;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+
+class HideVersionNumber extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (Schema::hasTable('configs')) {
+			DB::table('configs')->insert([
+				[
+					'key' => 'hide_version_number',
+					'value' => '1',
+					'confidentiality' => 3,
+				],
+			]);
+		} else {
+			echo "Table configs does not exists\n";
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		if (env('DB_DROP_CLEAR_TABLES_ON_ROLLBACK', false)) {
+			Configs::where('key', '=', 'hide_version_number')->delete();
+		}
+	}
+}
