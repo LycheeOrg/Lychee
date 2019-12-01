ec01bf6e92ccdfff7a017b25f46347557eedba44
YoshinoriN <nayo-msoft@outlook.jp>
2019-12-01 23:13:34 +0900

    fix: add an existing table check when migration tables
    * if do `migrate:rollback` these table creation will be fail

--- database/migrations/2019_02_21_114356_create_pages_table.php
+++ database/migrations/2019_02_21_114356_create_pages_table.php
@@ -15,51 +15,55 @@ class CreatePagesTable extends Migration
 	 */
 	public function up()
 	{
-		Schema::create('pages', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('title', 150)->default('');
-			$table->string('menu_title', 100)->default('');
-			$table->boolean('in_menu')->default(false);
-			$table->boolean('enabled')->default(false);
-			$table->string('link', 150)->default('');
-			$table->integer('order')->default(0);
-			$table->timestamps();
-		});
+		if (!Schema::hasTable('pages')) {
+			Schema::create('pages', function (Blueprint $table) {
+				$table->increments('id');
+				$table->string('title', 150)->default('');
+				$table->string('menu_title', 100)->default('');
+				$table->boolean('in_menu')->default(false);
+				$table->boolean('enabled')->default(false);
+				$table->string('link', 150)->default('');
+				$table->integer('order')->default(0);
+				$table->timestamps();
+			});
+		}
 
-		DB::table('pages')->insert([
-			//			[
-			//				'title'      => 'contact',
-			//				'menu_title' => 'contact',
-			//				'in_menu'    => true,
-			//				'link'       => '/contact',
-			//				'enabled'    => true,
-			//				'order'      => 0
-			//			],
-			//			[
-			//				'title'      => 'about',
-			//				'menu_title' => 'about',
-			//				'in_menu'    => true,
-			//				'link'       => '/about',
-			//				'enabled'    => true,
-			//				'order'      => 1
-			//			],
-			[
-				'title' => 'gallery',
-				'menu_title' => 'gallery',
-				'in_menu' => true,
-				'link' => '/gallery',
-				'enabled' => true,
-				'order' => 2,
-			],
-			//			[
-			//				'title'      => 'portfolio',
-			//				'menu_title' => 'portfolio',
-			//				'in_menu'    => true,
-			//				'link'       => '/portfolio',
-			//				'enabled'    => true,
-			//				'order'      => 3
-			//			],
-		]);
+		if (!DB::table('pages')->where('title', 'gallery')->exists()) {
+			DB::table('pages')->insert([
+				//			[
+				//				'title'      => 'contact',
+				//				'menu_title' => 'contact',
+				//				'in_menu'    => true,
+				//				'link'       => '/contact',
+				//				'enabled'    => true,
+				//				'order'      => 0
+				//			],
+				//			[
+				//				'title'      => 'about',
+				//				'menu_title' => 'about',
+				//				'in_menu'    => true,
+				//				'link'       => '/about',
+				//				'enabled'    => true,
+				//				'order'      => 1
+				//			],
+				[
+					'title' => 'gallery',
+					'menu_title' => 'gallery',
+					'in_menu' => true,
+					'link' => '/gallery',
+					'enabled' => true,
+					'order' => 2,
+				],
+				//			[
+				//				'title'      => 'portfolio',
+				//				'menu_title' => 'portfolio',
+				//				'in_menu'    => true,
+				//				'link'       => '/portfolio',
+				//				'enabled'    => true,
+				//				'order'      => 3
+				//			],
+			]);
+		}
 	}
 
 	/**
