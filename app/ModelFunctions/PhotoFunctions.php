e02816842bea6f0bea576b6ee5997cb4b28b2abe
tmp-hallenser <1869257+tmp-hallenser@users.noreply.github.com>
2019-12-16 21:15:33 +0100

    Separate file for MOVFormat class (needed for FFmpeg)

--- app/ModelFunctions/PhotoFunctions.php
+++ app/ModelFunctions/PhotoFunctions.php
@@ -755,34 +755,3 @@ class PhotoFunctions
 		return $this->validExtensions;
 	}
 }
-
-// Class for FFMpeg to convert files to mov format
-class MOVFormat extends FFMpeg\Format\Video\DefaultVideo
-{
-	public function __construct($audioCodec = 'copy', $videoCodec = 'copy')
-	{
-		$this
-			->setAudioCodec($audioCodec)
-			->setVideoCodec($videoCodec);
-	}
-
-	public function supportBFrames()
-	{
-		return false;
-	}
-
-	public function getExtraParams()
-	{
-		return ['-f', 'mov'];
-	}
-
-	public function getAvailableAudioCodecs()
-	{
-		return ['copy'];
-	}
-
-	public function getAvailableVideoCodecs()
-	{
-		return ['copy'];
-	}
-}
