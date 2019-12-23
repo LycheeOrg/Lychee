97ba5f8d237aa2a47e6d13a7b95ab276896e36f3
ildyria <beviguier@gmail.com>
2019-12-23 18:36:39 +0100

    fix redirect code to 307: Temporary Redirect (since HTTP/1.1)

--- installer/Middleware/InstallCheck.php
+++ installer/Middleware/InstallCheck.php
@@ -12,7 +12,7 @@ class InstallCheck implements Check
 		$logfile = 'installed.log';
 		if (file_exists($logfile)) {
 			// we directly redirect to gallery
-			http_response_code(303);
+			http_response_code(307);
 			header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 			header('Location: .');
 			exit;
