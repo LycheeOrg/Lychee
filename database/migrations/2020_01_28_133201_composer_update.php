494f822e7ece3a64423e1f98bf7188345e11db79
ildyria <beviguier@gmail.com>
2020-01-28 14:35:31 +0100

    Add composer call on update

---
+++ database/migrations/2020_01_28_133201_composer_update.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class ComposerUpdate extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (!defined('BOOL')) {
+			define('BOOL', '0|1');
+		}
+
+		if (Schema::hasTable('configs')) {
+			DB::table('configs')->insert([
+				[
+					'key' => 'apply_composer_update',
+					'value' => '0',
+					'confidentiality' => '3',
+					'cat' => 'Admin',
+					'type_range' => BOOL,
+				],
+			]);
+		} else {
+			echo "Table configs does not exists\n";
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		if (env('DB_DROP_CLEAR_TABLES_ON_ROLLBACK', false)) {
+			Configs::where('key', '=', 'apply_composer_update')->delete();
+		}
+	}
+}
