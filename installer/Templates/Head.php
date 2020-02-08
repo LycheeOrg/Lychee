04d315cdb77739406c2406358f784df3822360d7
ildyria <beviguier@gmail.com>
2020-02-08 14:14:21 +0100

    fix with respect to comments

--- installer/Templates/Head.php
+++ installer/Templates/Head.php
@@ -22,7 +22,6 @@ class Head implements Template
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Lychee Installer</title>
         <link rel="icon" type="image/png" href="installer/assets/img/favicon/favicon-16x16.png" sizes="16x16"/>
