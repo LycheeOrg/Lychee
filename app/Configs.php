97aa1ce3e878469bd4d937e443a21e039214d489
ildyria <beviguier@gmail.com>
2020-02-12 12:29:13 +0100

    update composer & change 'which' for 'command -v'

--- app/Configs.php
+++ app/Configs.php
@@ -255,14 +255,12 @@ class Configs extends Model
 		// 1: Exiftool is available
 		// 2: Not yet tested if exiftool is available
 
-		$has_exiftool = self::get_value('has_exiftool');
+		$has_exiftool = intval(self::get_value('has_exiftool'));
 
 		// value not yet set -> let's see if exiftool is available
 		if ($has_exiftool == 2) {
-			$status = 0;
-			$output = '';
-			exec('which exiftool 2>&1 > /dev/null', $output, $status);
-			if ($status != 0) {
+			$path = exec('command -v exiftool');
+			if ($path == '') {
 				self::set('has_exiftool', 0);
 				$has_exiftool = false;
 			} else {
@@ -284,18 +282,16 @@ class Configs extends Model
 	public static function hasFFmpeg()
 	{
 		// has_ffmpeg has the following values:
-		// 0: No FFmpeg
-		// 1: FFmpeg is available
-		// 2: Not yet tested if FFmpeg is available
+		// 0: No ffmpeg
+		// 1: ffmpeg is available
+		// 2: Not yet tested if ffmpeg is available
 
-		$has_ffmpeg = self::get_value('has_ffmpeg');
+		$has_ffmpeg = intval(self::get_value('has_ffmpeg'));
 
-		// value not yet set -> let's see if FFmpeg is available
+		// value not yet set -> let's see if ffmpeg is available
 		if ($has_ffmpeg == 2) {
-			$status = 0;
-			$output = '';
-			exec('which FFmpeg 2>&1 > /dev/null', $output, $status);
-			if ($status != 0) {
+			$path = exec('command -v ffmpeg');
+			if ($path == '') {
 				self::set('has_ffmpeg', 0);
 				$has_ffmpeg = false;
 			} else {
