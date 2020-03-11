21c2014be05be5486c477fa1878bdb445bf067ee
ildyria <beviguier@gmail.com>
2020-03-11 14:41:15 +0100

    fix formatting

--- app/Console/Commands/show_logs.php
+++ app/Console/Commands/show_logs.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 
 class show_logs extends Command
 {
-    /**
+	/**
 	 * The name and signature of the console command.
 	 *
 	 * @var string
@@ -21,7 +21,7 @@ class show_logs extends Command
 	 */
 	protected $description = 'Print the logs table.';
 
-    /**
+	/**
 	 * Create a new command instance.
 	 *
 	 * @return void
@@ -38,21 +38,22 @@ class show_logs extends Command
 	 */
 	public function handle()
 	{
-        $n = (int) $this->argument('n');
-        $order = $this->argument('order');
-        $order = ($order == 'ASC' || $order == 'DESC') ? $order : 'DESC';
+		$n = (int) $this->argument('n');
+		$order = $this->argument('order');
+		$order = ($order == 'ASC' || $order == 'DESC') ? $order : 'DESC';
 
-        if (Logs::count() == 0) {
+		if (Logs::count() == 0) {
 			$this->line('Everything looks fine, Lychee has not reported any problems!');
 		} else {
-            $logs = Logs::orderBy('id', $order)->limit($n)->get();
-            foreach ($logs->reverse() as $log) {
-				$this->line($this->magenta($log->created_at). ' -- ' . $this->color_type(str_pad($log->type, 7)) . ' -- '. $this->blue($log->function) .' -- '. $this->green($log->line).' -- ' .$log->text );
-            }
+			$logs = Logs::orderBy('id', $order)->limit($n)->get();
+			foreach ($logs->reverse() as $log) {
+				$this->line($this->magenta($log->created_at) . ' -- ' . $this->color_type(str_pad($log->type, 7)) . ' -- ' . $this->blue($log->function) . ' -- ' . $this->green($log->line) . ' -- ' . $log->text);
+			}
 		}
 	}
 
-	private function color_type($type) {
+	private function color_type($type)
+	{
 		switch ($type) {
 			case 'error  ':
 				return $this->red($type);
@@ -65,29 +66,33 @@ class show_logs extends Command
 		}
 	}
 
-	private function red($string) {
-		return '<fg=red>'.$string.'</>';
+	private function red($string)
+	{
+		return '<fg=red>' . $string . '</>';
 	}
 
-	private function magenta($string) {
-		return '<fg=magenta>'.$string.'</>';
+	private function magenta($string)
+	{
+		return '<fg=magenta>' . $string . '</>';
 	}
 
-	private function green($string) {
-		return '<fg=green>'.$string.'</>';
+	private function green($string)
+	{
+		return '<fg=green>' . $string . '</>';
 	}
 
-	private function yellow($string) {
-		return '<fg=yellow>'.$string.'</>';
+	private function yellow($string)
+	{
+		return '<fg=yellow>' . $string . '</>';
 	}
 
 	private function cyan($string)
 	{
-		return '<fg=cyan>'.$string.'</>';
+		return '<fg=cyan>' . $string . '</>';
 	}
 
 	private function blue($string)
 	{
-		return '<fg=blue>'.$string.'</>';
+		return '<fg=blue>' . $string . '</>';
 	}
 }
