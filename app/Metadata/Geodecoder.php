ae4fe21383485cb1cd7da05e691eaf10e5f4cd5a
ildyria <beviguier@gmail.com>
2020-03-21 15:36:57 +0100

    fix cache problems

--- app/Metadata/Geodecoder.php
+++ app/Metadata/Geodecoder.php
@@ -6,12 +6,12 @@ namespace App\Metadata;
 
 use App\Configs;
 use App\Logs;
+use Cache;
 use Geocoder\Provider\Cache\ProviderCache;
 use Geocoder\Provider\Nominatim\Nominatim;
 use Geocoder\Query\ReverseQuery;
 use Geocoder\StatefulGeocoder;
 use GuzzleHttp\HandlerStack;
-use Illuminate\Support\Facades\Cache;
 use Spatie\GuzzleRateLimiterMiddleware\RateLimiterMiddleware;
 use Spatie\GuzzleRateLimiterMiddleware\Store;
 
@@ -39,29 +39,7 @@ class Geodecoder
 
 		$provider = new Nominatim($httpAdapter, 'https://nominatim.openstreetmap.org', config('app.name'));
 
-		// Use Array Caching (in memory - only helps is involing command via console)
-		// $psr6Cache = new \Cache\Adapter\PHPArray\ArrayCachePool();
-
-		return new ProviderCache($provider, app('cache.psr6'));
-		// in case we want a 3 second cache expiry.
-		// return new \Geocoder\Provider\Cache\ProviderCache($provider, $psr6Cache, 3);
-
-		// if ($caching_type == 'Memory') {
-		// } elseif ($caching_type == 'Harddisk') {
-		// 	// Use filesystem adapter to cache data (writes files to /uploads/cache)
-
-		// 	$filesystemAdapter = new \League\Flysystem\Adapter\Local(Storage::path(''));
-		// 	$filesystem = new \League\Flysystem\Filesystem($filesystemAdapter);
-		// 	$psr6Cache = new \Cache\Adapter\Filesystem\FilesystemCachePool($filesystem);
-
-		// 	return new \Geocoder\Provider\Cache\ProviderCache(
-		// 				$provider, // Provider to cache
-		// 				$psr6Cache // PSR-6 compatible cache
-		// 			);
-		// }
-
-		// // No caching
-		// return $provider;
+		return new ProviderCache($provider, app('cache.store'));
 	}
 
 	/**
