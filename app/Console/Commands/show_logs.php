33ec23c500af4cdd3122a65769a79cefda385cb5
Benoît Viguier <ildyria@users.noreply.github.com>
2020-04-07 14:57:38 +0200

    Add new command for cleaning logs (+ backward compatibility) (#464)
    * Add new command for cleaning logs (+ backward compatibility)

--- app/Console/Commands/show_logs.php
+++ app/Console/Commands/show_logs.php
@@ -12,7 +12,7 @@ class show_logs extends Command
 	 *
 	 * @var string
 	 */
-	protected $signature = 'lychee:logs {n=100 : number of lines} {order=DESC : ASCending or DESCending}';
+	protected $signature = 'lychee:logs {action=show : show or clean} {n=100 : number of lines} {order=DESC : ASCending or DESCending}';
 
 	/**
 	 * The console command description.
@@ -38,12 +38,31 @@ class show_logs extends Command
 	 */
 	public function handle()
 	{
+		$action = $this->argument('action');
 		$n = (int) $this->argument('n');
 		$order = $this->argument('order');
+
+		if ($action == 'clean') {
+			Logs::truncate();
+			$this->line($this->yellow('Log table has been emptied.'));
+
+			return;
+		}
+		// we are in the show part but in the case where 'show' has not be defined.
+		// as a results arguments are shifted: n <- action, order <- n.
+		elseif ($action != 'show') {
+			$n = (int) $this->argument('action');
+			$order = $this->argument('n');
+		}
+		$this->action_show($n, $order);
+	}
+
+	private function action_show($n, $order)
+	{
 		$order = ($order == 'ASC' || $order == 'DESC') ? $order : 'DESC';
 
 		if (Logs::count() == 0) {
-			$this->line('Everything looks fine, Lychee has not reported any problems!');
+			$this->line($this->green('Everything looks fine, Lychee has not reported any problems!'));
 		} else {
 			$logs = Logs::orderBy('id', $order)->limit($n)->get();
 			foreach ($logs->reverse() as $log) {
