dfa2e236d01f4a2fe903d88573df03966c66cbc4
ildyria <beviguier@gmail.com>
2020-04-17 14:50:43 +0200

    correct urls [no ci] - do not merge before final release

--- config/urls.php
+++ config/urls.php
@@ -3,10 +3,10 @@
 return [
 	'update' => [
 		// we need this in case the URL of the project changes
-		'git' => 'http://api.github.com/repos/LycheeOrg/Lychee-Laravel/commits',
+		'git' => 'http://api.github.com/repos/LycheeOrg/Lychee/commits',
 		'json' => 'https://lycheeorg.github.io/update.json',
 	],
 	'git' => [
-		'pull' => 'https://github.com/LycheeOrg/Lychee-Laravel.git',
+		'pull' => 'https://github.com/LycheeOrg/Lychee.git',
 	],
 ];
