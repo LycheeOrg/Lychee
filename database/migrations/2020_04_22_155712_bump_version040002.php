096213d2b8a7e97cc4871979830f02ccb8d263c1
ildyria <beviguier@gmail.com>
2020-04-22 18:36:22 +0200

    formatting

--- database/migrations/2020_04_22_155712_bump_version040002.php
+++ database/migrations/2020_04_22_155712_bump_version040002.php
@@ -5,23 +5,23 @@ use Illuminate\Database\Migrations\Migration;
 
 class BumpVersion040002 extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
 		Configs::where('key', 'version')->update(['value' => '040002']);
-    }
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-		Configs::where('key', 'version')->update(['value' => '040002']);
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', 'version')->update(['value' => '040001']);
+	}
 }
