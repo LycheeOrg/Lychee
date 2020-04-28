3c9e01e427ecdb173f9145dcdf4498cfe263927b
ildyria <beviguier@gmail.com>
2020-04-29 00:03:41 +0200

    bump version

---
+++ database/migrations/2020_04_29_000250_bump_version040003.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Configs;
+use Illuminate\Database\Migrations\Migration;
+
+class BumpVersion040003 extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Configs::where('key', 'version')->update(['value' => '040003']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', 'version')->update(['value' => '040002']);
+	}
+}
