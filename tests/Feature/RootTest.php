6b32af673c41786fd541e0133cf6323446e3bdda
Benoît Viguier <ildyria@users.noreply.github.com>
2020-05-13 21:29:19 +0200

    Test index (#558)
    * tentative to see crash on php 7.3
    * crash succesfully discovered

---
+++ tests/Feature/RootTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @noinspection PhpUndefinedClassInspection */
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class RootTest extends TestCase
+{
+	/**
+	 * Test album functions.
+	 *
+	 * @return void
+	 */
+	public function test_root()
+	{
+		exec('php index.php 2>&1', $return);
+		$return = implode('', $return);
+		$this->assertStringContainsString('This is the root directory and it MUST NOT BE PUBLICALLY ACCESSIBLE', $return);
+	}
+}
