cf8a37fc32e0b847689652bcb20c7264d55aa5e2
bennettscience <brian.bennett2@gmail.com>
2020-05-22 10:44:39 -0400

    - Change URL to `feed` - Default view to RSS instead of Atom to include enclosure

--- config/feed.php
+++ config/feed.php
@@ -16,7 +16,7 @@ return [
 			/*
 			 * The feed will be available on this url.
 			 */
-			'url' => '/rss',
+			'url' => '/feed',
 
 			'title' => 'Latest pictures',
 			'description' => 'Latest added pictures.',
@@ -25,12 +25,7 @@ return [
 			/*
 			 * The view that will render the feed.
 			 */
-			'view' => 'feed::atom',
-
-			/*
-			 * The type to be used in the <link> tag
-			 */
-			'type' => 'application/atom+xml',
+			'view' => 'feed::rss',
 		],
 	],
 ];
