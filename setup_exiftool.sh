e38c7b945f7b95dfb2339d8e6191ab4473f3cdd2
tmp-hallenser <1869257+tmp-hallenser@users.noreply.github.com>
2020-05-23 07:38:05 +0200

    Fix extraction of exiftool for travis

--- setup_exiftool.sh
+++ setup_exiftool.sh
@@ -2,7 +2,7 @@
 set -e
 
 wget https://exiftool.org/Image-ExifTool-11.99.tar.gz
-tar -zxvf Image-ExifTool-11.77.tar.gz
+tar -zxvf Image-ExifTool-11.99.tar.gz
 cd Image-ExifTool-11.99
 perl Makefile.PL
 make test
