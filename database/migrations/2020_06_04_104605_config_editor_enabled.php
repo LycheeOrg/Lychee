a9d044a561862f70830eb09d8efaa3a1be8563cf
Willy Gardiol <willy@gardiol.org>
2020-06-10 13:32:07 +0200

    Added config file migrate

---
+++ database/migrations/2020_06_04_104605_config_editor_enabled.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Configs;
+use Illuminate\Database\Migrations\Migration;
+
+class ConfigEditorEnabled extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		defined('BOOL') or define('BOOL', '0|1');
+		DB::table('configs')->insert([
+			[
+				'key' => 'editor_enabled',
+				'value' => 1,
+				'confidentiality' => 2,
+				'cat' => 'Image Processing',
+				'type_range' => BOOL,
+			],
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', '=', 'editor_enabled')->delete();
+	}
+}
