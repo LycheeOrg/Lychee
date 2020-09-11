843e0b9611137075e7741a4480fd2bd5811ed975
ildyria <beviguier@gmail.com>
2020-09-11 12:43:02 +0200

    sync Lychee-front, fix links in head.blade.php

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,9 +5,9 @@
 @yield('head-css')
 
 <link rel="shortcut icon" href="favicon.ico">
-<link rel="apple-touch-icon" href="Lychee-front/images/apple-touch-icon-ipad.png" sizes="120x120">
-<link rel="apple-touch-icon" href="Lychee-front/images/apple-touch-icon-iphone.png" sizes="152x152">
-<link rel="apple-touch-icon" href="Lychee-front/images/apple-touch-icon-iphone-plus.png" sizes="180x180">
+<link rel="apple-touch-icon" href="img/apple-touch-icon-ipad.png" sizes="120x120">
+<link rel="apple-touch-icon" href="img/apple-touch-icon-iphone.png" sizes="152x152">
+<link rel="apple-touch-icon" href="img/apple-touch-icon-iphone-plus.png" sizes="180x180">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=4.0, user-scalable=yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
