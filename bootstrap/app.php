5cd707e34d728f72be3f850b54d534cd84fe17b4
ildyria <beviguier@gmail.com>
2020-12-15 18:07:19 +0100

    fix tests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,6 @@
 |
 */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-
 $app = new Illuminate\Foundation\Application(
 	$_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
