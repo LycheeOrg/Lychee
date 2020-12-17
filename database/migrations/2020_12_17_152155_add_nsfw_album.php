32ecc820600456b71e397c9439bc1762d5cf1796
ildyria <beviguier@gmail.com>
2020-12-17 22:23:31 +0100

    WIP

--- database/migrations/2020_12_17_152155_add_nsfw_album.php
+++ database/migrations/2020_12_17_152155_add_nsfw_album.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Configs;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,12 +19,23 @@ class AddNsfwAlbum extends Migration
 	 */
 	public function up()
 	{
+		defined('STRING') or define('STRING', 'string');
+		defined('STRING_REQ') or define('STRING_REQ', 'string_required');
+		defined('BOOL') or define('BOOL', '0|1');
+
 		Schema::table(self::ALBUM, function ($table) {
 			$table->boolean(self::NSFW_COLUMN_NAME)->default(false)->after(self::VISIBLE_HIDDEN);
 		});
 		Schema::table(self::ALBUM, function (Blueprint $table) {
 			$table->renameColumn(self::VISIBLE_HIDDEN, self::VIEWABLE);
 		});
+
+		DB::table('configs')->insert([
+			['key' => 'nsfw_visible', 'value' => '1', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => BOOL],
+			['key' => 'nsfw_blur', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => BOOL],
+			['key' => 'nsfw_warning', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => BOOL],
+			['key' => 'nsfw_warning_text', 'value' => '<b>Sensitive content</b><br><p>This album contains sensitive content which some people may find offensive or disturbing.</p>', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => STRING_REQ],
+		]);
 	}
 
 	/**
@@ -39,5 +51,7 @@ class AddNsfwAlbum extends Migration
 		Schema::table(self::ALBUM, function (Blueprint $table) {
 			$table->renameColumn(self::VIEWABLE, self::VISIBLE_HIDDEN);
 		});
+
+		Configs::where('cat', '=', 'Mod NSFW')->delete();
 	}
 }
