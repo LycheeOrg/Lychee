be9b1c99fa6e4e26608a5729a203ce7ecbc56cfe
ildyria <beviguier@gmail.com>
2020-12-18 16:40:32 +0100

    add bump version, to force migration

---
+++ database/migrations/2020_12_18_163800_bump_version040010.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Configs;
+use Illuminate\Database\Migrations\Migration;
+
+class BumpVersion040010 extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Configs::where('key', 'version')->update(['value' => '040010']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', 'version')->update(['value' => '040009']);
+	}
+}
