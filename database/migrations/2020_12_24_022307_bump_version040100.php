3b35cfa0d071eca9406ef7b0e6f8824432d9458d
ildyria <beviguier@gmail.com>
2020-12-24 02:24:58 +0100

    propose version 4.1.0

---
+++ database/migrations/2020_12_24_022307_bump_version040100.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Configs;
+use Illuminate\Database\Migrations\Migration;
+
+class BumpVersion040100 extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Configs::where('key', 'version')->update(['value' => '040100']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', 'version')->update(['value' => '040010']);
+	}
+}
