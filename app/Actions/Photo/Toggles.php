b9e8dd74d522b8c6fc8fe403138e13a0e6ef36f0
ildyria <beviguier@gmail.com>
2020-12-30 23:08:52 +0100

    fix Sqlite

--- app/Actions/Photo/Toggles.php
+++ app/Actions/Photo/Toggles.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Photo;
 
 use App\Models\Photo;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
 
 class Toggles
@@ -11,7 +12,19 @@ class Toggles
 
 	public function do(array $photoIDs): bool
 	{
-		//! DB::raw is safe because WE (dev) have control over $property. It is not influced by user inputs.
-		return Photo::whereIn('id', $photoIDs)->update([$this->property => DB::raw('1 XOR `' . $this->property . '`')]);
+		try {
+			//! DB::raw is safe because WE (dev) have control over $property. It is not influced by user inputs.
+			$no_error = Photo::whereIn('id', $photoIDs)->update([$this->property => DB::raw('1 XOR `' . $this->property . '`')]);
+		} catch (QueryException $e) {
+			// Sqlite we need the slow approach
+			$photos = Photo::whereIn('id', $photoIDs)->get();
+			$no_error = true;
+			foreach ($photos as $photo) {
+				$photo->{$this->property} = $photo->{$this->property} != 1 ? 1 : 0;
+				$no_error &= $photo->save();
+			}
+		}
+
+		return $no_error;
 	}
 }
