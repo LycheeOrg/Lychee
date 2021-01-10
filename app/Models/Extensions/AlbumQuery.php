3956acac11b38b014c6818ca6145a86b68bf74bd
ildyria <beviguier@gmail.com>
2021-01-10 15:42:21 +0100

    even better, no alias for main albums table

--- app/Models/Extensions/AlbumQuery.php
+++ app/Models/Extensions/AlbumQuery.php
@@ -10,19 +10,19 @@ trait AlbumQuery
 {
 	public static function initQuery(): Builder
 	{
-		return self::with(['owner'])->from('albums as aa')->select('aa.*')
+		return self::with(['owner'])->select('albums.*')
 			->addSelect([
 				'max_takestamp' => Photo::leftJoin('albums as a', 'album_id', '=', 'a.id')
 					->select('takestamp')
-					->where('aa._lft', '<=', DB::raw('a._lft'))
-					->where('a._rgt', '<=', DB::raw('aa._rgt'))
+					->where('albums._lft', '<=', DB::raw('a._lft'))
+					->where('a._rgt', '<=', DB::raw('albums._rgt'))
 					->whereNotNull('takestamp')
 					->orderBy('takestamp', 'desc')
 					->limit(1),
 				'min_takestamp' => Photo::leftJoin('albums as a', 'album_id', '=', 'a.id')
 					->select('takestamp')
-					->where('aa._lft', '<=', DB::raw('a._lft'))
-					->where('a._rgt', '<=', DB::raw('aa._rgt'))
+					->where('albums._lft', '<=', DB::raw('a._lft'))
+					->where('a._rgt', '<=', DB::raw('albums._rgt'))
 					->whereNotNull('takestamp')
 					->orderBy('takestamp', 'asc')
 					->limit(1),
