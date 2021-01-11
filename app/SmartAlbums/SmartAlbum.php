f9a01c1fc133a3ed9b488485191be0057e928b91
ildyria <beviguier@gmail.com>
2021-01-11 14:57:05 +0100

    dead code

--- app/SmartAlbums/SmartAlbum.php
+++ app/SmartAlbums/SmartAlbum.php
@@ -2,10 +2,8 @@
 
 namespace App\SmartAlbums;
 
-use AccessControl;
 use App\Models\Album;
 use App\Models\Configs;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection as BaseCollection;
 
 class SmartAlbum extends Album
@@ -14,11 +12,6 @@ class SmartAlbum extends Album
 	 * @var string
 	 */
 	public $description = '';
-
-	/**
-	 * @var Carbon
-	 */
-	public $created_at = null;
 
 	/**
 	 * fake password string.
@@ -33,44 +26,6 @@ class SmartAlbum extends Album
 	 * @var string
 	 */
 	public $license = '';
-
-	/**
-	 * @var Collection[int]
-	 */
-	protected $albumIds = null;
-
-	public function __construct()
-	{
-		parent::__construct();
-		$this->albumIds = new BaseCollection();
-		$this->created_at = new Carbon();
-		$this->smart = true;
-	}
-
-	/**
-	 * Set a restriction on the available albums.
-	 *
-	 * @param Collection[int] $albumIds
-	 *
-	 * @return void
-	 */
-	public function setAlbumIDs(BaseCollection $albumIds): void
-	{
-		$this->albumIds = $albumIds;
-	}
-
-	public function filter($query)
-	{
-		if (!AccessControl::is_admin()) {
-			$query = $query->whereIn('album_id', $this->albumIds);
-		}
-
-		if (AccessControl::is_logged_in() && AccessControl::id() > 0) {
-			$query = $query->orWhere('owner_id', '=', AccessControl::id());
-		}
-
-		return $query;
-	}
 
 	/*------------------------- BOOLEANS --------------------------------- */
 	public function is_full_photo_visible(): bool
@@ -104,13 +59,5 @@ class SmartAlbum extends Album
 	public function get_children()
 	{
 		return new BaseCollection();
-	}
-
-	/**
-	 * We override this method so we can use it elsewhere. :).
-	 */
-	public function get_all_photos()
-	{
-		return $this->get_photos();
 	}
 }
