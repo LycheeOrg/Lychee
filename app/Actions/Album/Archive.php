c5e8a65406d6296338f8fe00df72dea9d9eef282
kamil4 <kamil.01482@iskra.name>
2021-01-19 21:36:50 -0600

    Handle album_id == null

--- app/Actions/Album/Archive.php
+++ app/Actions/Album/Archive.php
@@ -142,11 +142,12 @@ class Archive extends Action
 
 		foreach ($photos as $photo) {
 			// For photos in smart or tag albums, skip the ones that are not
-			// downloadable based on their actual parent album.  Photos from
-			// Unsorted are handled implicitly by the photo ownership test.
+			// downloadable based on their actual parent album.  The test for
+			// album_id == null shouldn't really be needed as all such photos
+			// in smart albums should be owned by the current user...
 			if (
 				$album->smart && !AccessControl::is_current_user($photo->owner_id) &&
-				!$photo->album->is_downloadable()
+				!($photo->album_id == null ? $album->is_downloadable() : $photo->album->is_downloadable())
 			) {
 				continue;
 			}
