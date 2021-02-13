dcad64e8cb0e1dcc86e48bd97484a104f675f6ad
Kamil Iskra <kamil.01482@iskra.name>
2021-02-13 06:26:54 -0600

    Add upload_processing_limit config variable (#919)
    * Add upload_processing_limit config variable
    * sync Lychee-front
    * bump version 4.2.2
    
    Co-authored-by: ildyria <beviguier@gmail.com>

---
+++ database/migrations/2021_02_12_222948_config_upload_processing_limit.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Configs;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class ConfigUploadProcessingLimit extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		defined('INT') or define('INT', 'int');
+
+		DB::table('configs')->insert([
+			[
+				'key' => 'upload_processing_limit',
+				'value' => '4',
+				'confidentiality' => 2,
+				'cat' => 'Image Processing',
+				'type_range' => INT,
+			],
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', '=', 'upload_processing_limit')->delete();
+	}
+}
