d22cf6b508986df621ebc765229fc7e7e1275dec
Matthias Nagel <matthias.h.nagel@posteo.de>
2021-05-02 18:00:00 +0200

    Added migration for database

---
+++ database/migrations/2021_05_02_174300_add_filesize_raw_col.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSmartTagAlbumCols extends Migration
+{
+	private const TABLE_NAME = 'photos';
+	private const COLUMN_NAME = 'filesize_raw';
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table(self::TABLE_NAME, function (Blueprint $table) {
+			$table->unsignedInteger(self::COLUMN_NAME)->default(0)->after('size');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table(self::TABLE_NAME, function (Blueprint $table) {
+			$table->dropColumn(self::COLUMN_NAME);
+		});
+	}
+}
