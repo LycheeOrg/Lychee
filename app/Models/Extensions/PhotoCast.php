f2c7b5fbff40d4f54255ccba9078e68ce6585699
Matthias Nagel <matthias.h.nagel@posteo.de>
2021-06-04 17:14:25 +0200

    Made Photo::toReturnArray an alias for Photo:toArray

--- app/Models/Extensions/PhotoCast.php
+++ app/Models/Extensions/PhotoCast.php
@@ -8,47 +8,9 @@ use Illuminate\Support\Facades\Storage;
 
 trait PhotoCast
 {
-	/**
-	 * Returns photo-attributes into a front-end friendly format. Note that some attributes remain unchanged.
-	 *
-	 * @return array
-	 */
 	public function toReturnArray(): array
 	{
-		return [
-			'id' => strval($this->id),
-			'title' => $this->title,
-			'description' => $this->description,
-			'tags' => $this->tags,
-			'star' => $this->star,
-			'public' => $this->public,
-			'album' => $this->album_id !== null ? strval($this->album_id) : null,
-			'url' => $this->url,
-			'width' => $this->width,
-			'height' => $this->height,
-			'type' => $this->type,
-			'filesize' => $this->filesize,
-			'iso' => $this->iso,
-			'aperture' => $this->aperture,
-			'make' => $this->make,
-			'model' => $this->model,
-			'shutter' => $this->shutter,
-			'focal' => $this->focal,
-			'lens' => $this->lens,
-			'latitude' => $this->latitude,
-			'longitude' => $this->longitude,
-			'altitude' => $this->altitude,
-			'imgDirection' => $this->imgDirection,
-			'location' => $this->location,
-			'live_photo_content_id' => $this->live_photo_content_id,
-			'live_photo_url' => $this->live_photo_url,
-			'created_at' => $this->created_at->format(\DateTimeInterface::ATOM),
-			'updated_at' => $this->updated_at->format(\DateTimeInterface::ATOM),
-			'taken_at' => (!empty($this->taken_at)) ? $this->taken_at->format(\DateTimeInterface::ATOM) : null,
-			'taken_at_orig_tz' => $this->taken_at_orig_tz,
-			'license' => $this->license,
-			'sizeVariants' => $this->size_variants->jsonSerialize(),
-		];
+		return $this->toArray();
 	}
 
 	/**
