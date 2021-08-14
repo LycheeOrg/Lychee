c4e878a5ee9e913b1c859e6fc282d45f6f8943a6
Pierre Champion | Drakirus <prr.champion@gmail.com>
2021-08-14 23:30:10 +0200

    Update Sync.php
    fixes: #1056

--- app/Console/Commands/Sync.php
+++ app/Console/Commands/Sync.php
@@ -15,7 +15,7 @@ class Sync extends Command
 	 *
 	 * @var string
 	 */
-	protected $signature = 'lychee:sync {dir : directory to sync} {--album_id=0 : Album ID to import to} {--owner_id=0 : Owner ID of imported photos} {--resync_metadata : Re-sync metadata of existing files}';
+	protected $signature = 'lychee:sync {dir : directory to sync} {--album_id=0 : Album ID to import to} {--owner_id=0 : Owner ID of imported photos} {--resync_metadata : Re-sync metadata of existing files}  {--delete_imported : Delete the original files} {--import_via_symlink= : Imports photos from via a symlink instead of copying the files} {--not_skip_duplicates : Don\'t Skip photos and albums if they already exist in the gallery}';
 
 	/**
 	 * The console command description.
@@ -31,7 +31,6 @@ class Sync extends Command
 	 */
 	public function handle(Exec $exec)
 	{
-		$this->info('Start syncing.');
 		$directory = $this->argument('dir');
 		$owner_id = (int) $this->option('owner_id'); // in case no ID provided -> import as root user
 		$album_id = (int) $this->option('album_id'); // in case no ID provided -> import to root folder
@@ -39,12 +38,27 @@ class Sync extends Command
 		// Enable CLI formatting of status
 		$exec->statusCLIFormatting = true;
 		$exec->memCheck = false;
-		$exec->delete_imported = false; // we want to sync -> do not delete imported files
-		$exec->import_via_symlink = (Configs::get_value('import_via_symlink', '0') === '1');
-		$exec->skip_duplicates = true;
+		$exec->delete_imported = $this->option('delete_imported');
+		if (!is_null($this->option('import_via_symlink'))) {
+			$exec->import_via_symlink = $this->option('import_via_symlink') === '1';
+			if ($exec->import_via_symlink && $exec->delete_imported) {
+				$this->error('--delete_imported  and --import_via_symlink flags are conflicting.');
+				return 1;
+			}
+		} else {
+			$exec->import_via_symlink = (Configs::get_value('import_via_symlink', '0') === '1');
+			if ($exec->import_via_symlink && $exec->delete_imported) {
+				$this->error('--delete_imported flag and Config "import_via_symlink" setting are conflicting.');
+				$this->info('  Use --import_via_symlink=0 with --delete_imported to overwrite the Config.');
+				return 1;
+			}
+		}
+		$exec->skip_duplicates = !$this->option('skip_duplicates');
 		$exec->resync_metadata = $this->option('resync_metadata');
 
 		AccessControl::log_as_id($owner_id);
+
+		$this->info('Start syncing.');
 
 		try {
 			$exec->do($directory, $album_id);
