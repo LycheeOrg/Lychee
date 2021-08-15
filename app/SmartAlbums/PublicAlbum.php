fb8eb4222fbde01b3dc6cfd78b9ce7c68fbefd57
Matthias Nagel <matthias.h.nagel@posteo.de>
2021-08-15 18:13:15 +0200

    Fixed caching bug

--- app/SmartAlbums/PublicAlbum.php
+++ app/SmartAlbums/PublicAlbum.php
@@ -21,6 +21,13 @@ class PublicAlbum extends BaseSmartAlbum
 		if (!self::$instance) {
 			self::$instance = new self();
 		}
+		// Actually, this statement is only needed due to testing.
+		// The same instance of this class is used for all tests, because
+		// the singleton stays alive during tests.
+		// This implies that the relation of photos is never be reloaded
+		// but remains constant during all tests (it equals the empty set)
+		// and the tests fails.
+		self::$instance->unsetRelation('photos');
 
 		return self::$instance;
 	}
