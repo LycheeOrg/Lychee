fe5adb8ed8395c8928ce7e573a93503653190854
Matthias Nagel <matthias.h.nagel@posteo.de>
2021-09-16 20:21:03 +0200

    Fixed migration.

--- database/migrations/2021_08_06_184400_refactor_album_model.php
+++ database/migrations/2021_08_06_184400_refactor_album_model.php
@@ -1,10 +1,12 @@
 <?php
 
 use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Kalnoy\Nestedset\NodeTrait;
 
 /**
  * Migration for new architecture of albums.
@@ -525,6 +527,8 @@ class RefactorAlbumModel extends Migration
 			}
 		}
 
+		RefactorAlbumModel_AlbumModel::query()->fixTree();
+
 		$userAlbumRelations = DB::table('user_album')->lazyById();
 		foreach ($userAlbumRelations as $userAlbumRelation) {
 			DB::table('user_base_album')->insert([
@@ -640,6 +644,8 @@ class RefactorAlbumModel extends Migration
 					'showtags' => $tagAlbum->show_tags,
 				]);
 		}
+
+		RefactorAlbumModel_AlbumModel::query()->fixTree();
 
 		$userBaseAlbumRelations = DB::table('user_base_album')->lazyById();
 		foreach ($userBaseAlbumRelations as $userBaseAlbumRelation) {
@@ -793,4 +799,25 @@ class RefactorAlbumModel extends Migration
 			$table->dropForeign($indexName);
 		}
 	}
+}
+
+/**
+ * Model class specific for this migration.
+ *
+ * Migrations are required to be also runnable in the future after the code
+ * base will have evolved.
+ * To this end, migrations must not rely on a specific implementation of
+ * models, because these models may change in the future, but the migration
+ * must conduct its task with respect to a table layout which was valid at
+ * the time when the migration was written.
+ * In conclusion, this implies that migration should not use models but use
+ * low-level DB queries when necessary.
+ * Unfortunately, we need the `fixTree()` algorithm and there is no
+ * implementation which uses low-level DB queries.
+ */
+class RefactorAlbumModel_AlbumModel extends Model
+{
+	use NodeTrait;
+
+	protected $table = 'albums';
 }
