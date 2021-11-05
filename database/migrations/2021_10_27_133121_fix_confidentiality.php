815f896055f63b289f54bf8565c095a4e1ab4183
Kamil Iskra <kamil.01482@iskra.name>
2021-11-05 09:51:13 -0500

    Make necessary configs available to the front end (#1128)

---
+++ database/migrations/2021_10_27_133121_fix_confidentiality.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Configs;
+use Illuminate\Database\Migrations\Migration;
+
+class FixConfidentiality extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Configs::where('key', 'editor_enabled')->update(['confidentiality' => '0']);
+		Configs::where('key', 'upload_processing_limit')->update(['confidentiality' => '0']);
+		Configs::where('key', 'public_photos_hidden')->update(['confidentiality' => '0']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', 'editor_enabled')->update(['confidentiality' => '2']);
+		Configs::where('key', 'upload_processing_limit')->update(['confidentiality' => '2']);
+		Configs::where('key', 'public_photos_hidden')->update(['confidentiality' => '2']);
+	}
+}
