c0ba97c876f6d39671d0dc0f000a2dfa219efa21
ildyria <beviguier@gmail.com>
2021-11-16 16:23:26 +0100

    update to version 4.3.6

---
+++ database/migrations/2021_11_16_162058_bump_version040306.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Configs;
+use Illuminate\Database\Migrations\Migration;
+
+class BumpVersion040306 extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Configs::where('key', 'version')->update(['value' => '040306']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Configs::where('key', 'version')->update(['value' => '040305']);
+	}
+}
