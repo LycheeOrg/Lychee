60be7bb7028ba7053a7c3e4b25d09f75df35755a
Matthias Nagel <matthias.h.nagel@posteo.de>
2021-12-25 20:04:17 +0100

    Added 'admin' middlewware to routes in 'api-admin'.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -78,6 +78,7 @@ class Kernel extends HttpKernel
 			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
 			\App\Http\Middleware\VerifyCsrfToken::class,
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
+			'admin',
 		],
 	];
 
