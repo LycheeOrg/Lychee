bd84f153bbc869aa9b1e06b4d1eec1eb28c0e0c5
ildyria <beviguier@gmail.com>
2022-01-16 19:47:28 +0100

    add migration to enforce optimization of tables: solve slow down due to indexes missing

---
+++ database/migrations/2022_01_16_181337_optimize_tables.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\ConsoleSectionOutput;
+
+class OptimizeTables extends Migration
+{
+	private ConsoleOutput $output;
+	private ConsoleSectionOutput $msgSection;
+
+	public function __construct()
+	{
+		$this->output = new ConsoleOutput();
+		$this->msgSection = $this->output->section();
+	}
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$connection = Schema::connection(null)->getConnection();
+		$tables = $connection->getDoctrineSchemaManager()->listTableNames();
+
+		$driverName = $connection->getDriverName();
+		$sql = 'ANALYZE ';
+		if ($driverName == 'mysql') {
+			$this->msgSection->writeln('<info>Info:</info> MySql/MariaDB detected.');
+			$sql = 'ANALYZE TABLE ';
+		}
+
+		foreach ($tables as $table) {
+			try {
+				DB::statement($sql . $table);
+				$this->msgSection->writeln('<info>Info:</info> ' . $table . ' optimized.');
+			} catch (\Throwable $th) {
+				$this->msgSection->writeln('<error>Error:</error> could not optimize ' . $table . '.');
+				$this->msgSection->writeln('<error>Error:</error> ' . $th->getMessage());
+			}
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Nothing do to here.
+	}
+}
