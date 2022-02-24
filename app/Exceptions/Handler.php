6402906807d5f3d3b285dbadcd885e5f936f7c79
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-02-24 20:25:37 +0100

    Added report function

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,8 @@ use App\Exceptions\Handlers\AccessDBDenied;
 use App\Exceptions\Handlers\InstallationHandler;
 use App\Exceptions\Handlers\MigrationHandler;
 use App\Exceptions\Handlers\NoEncryptionKey;
+use App\Models\Logs;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Arr;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
@@ -14,6 +16,32 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Handler extends ExceptionHandler
 {
+	/**
+	 * Maps class names of exceptions to their severity.
+	 *
+	 * By default, exceptions are logged with severity
+	 * {@link Logs::SEVERITY_ERROR} by {@link Handler::report()}.
+	 * This array overwrites the default severity per exception.
+	 *
+	 * @var array<class-string, int>
+	 */
+	public const EXCEPTION2SEVERITY = [
+		PhotoSkippedException::class => Logs::SEVERITY_WARNING,
+		ImportCancelledException::class => Logs::SEVERITY_NOTICE,
+	];
+
+	protected $dontReport = [];
+	protected $internalDontReport = [];
+	protected string $appPath;
+
+	public function __construct(Container $container)
+	{
+		parent::__construct($container);
+		// Cache the application path to avoid multiple function calls
+		// and potential exceptions in `report()`
+		$this->appPath = app_path();
+	}
+
 	/**
 	 * Renders the given HttpException.
 	 *
@@ -94,5 +122,84 @@ class Handler extends ExceptionHandler
 		} catch (\Throwable) {
 			return [];
 		}
+	}
+
+	/**
+	 * Called by the framework if an exception occurs for logging purposes.
+	 *
+	 * As we have our own home-brewed logging mechanism via {@link Logs}
+	 * which does not implement {@link \Psr\Log\LoggerInterface} and does
+	 * not register with the service container, we override the method.
+	 *
+	 * @param \Throwable $e the exception to log
+	 */
+	public function report(\Throwable $e): void
+	{
+		do {
+			$e = $this->mapException($e);
+
+			if ($this->shouldntReport($e)) {
+				return;
+			}
+
+			$severity = array_key_exists(get_class($e), self::EXCEPTION2SEVERITY) ?
+				self::EXCEPTION2SEVERITY[get_class($e)] :
+				Logs::SEVERITY_ERROR;
+
+			$backtrace = self::normalizeBacktrace($e);
+
+			// Find the first (i.e. deepest) entry that refers to our own code base.
+			// Methods inside the framework don't help us.
+			$btEntry = Arr::first($backtrace, fn (array $bt) => str_contains($bt['file'], $this->appPath));
+			$line = $btEntry['line'];
+			$method = ($btEntry['class'] ? $btEntry['class'] . '::' : '') . ($btEntry['function'] ?: '<unknown>');
+
+			if ($e->getPrevious() !== null) {
+				Logs::log($severity, $method, $line, $e->getMessage() . '; caused by');
+			} else {
+				Logs::log($severity, $method, $line, $e->getMessage());
+			}
+		} while ($e = $e->getPrevious());
+	}
+
+	/**
+	 * Returns the re-organized backtrace of an exception.
+	 *
+	 * The backtrace reported by PHP is oddly strange.
+	 * The attribute pair file/line on the one hand-side and class/function
+	 * on the other hand-side of a standard PHP backtrace are off-by-one.
+	 * The reported file/line of an entry of the backtrace don't refer to
+	 * the position *inside* the reported class/function, but where
+	 * class/method has been invoked.
+	 * In particular, if one wants to know the position where the
+	 * exception has been thrown, then one must not look up
+	 * `backtrace[0]['file']` and `backtrace[0]['line']`, resp., but
+	 * use `getFile` and `getLine()` of the exception.
+	 *
+	 * @param \Throwable $e
+	 *
+	 * @return array{file: string, line: int, class: ?string, function: ?string}
+	 */
+	private static function normalizeBacktrace(\Throwable $e): array
+	{
+		$backtrace = $e->getTrace();
+		$file = $e->getFile();
+		$line = $e->getLine();
+		foreach ($backtrace as &$bt) {
+			$f = $bt['file'];
+			$l = $bt['line'];
+			$bt['file'] = $file;
+			$bt['line'] = $line;
+			$file = $f;
+			$line = $l;
+		}
+		$backtrace[] = [
+			'file' => $file,
+			'line' => $line,
+			'class' => null,    // the top most call always happens in global
+			'function' => null, // namespace outside a function
+		];
+
+		return $backtrace;
 	}
 }
