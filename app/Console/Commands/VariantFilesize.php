4b4f7f7f125f0ea6cfc46b49c6ad55214ce03d30
Quentin Duchemin <quentinduchemin@tuta.io>
2022-03-04 22:17:10 +0100

    Use limits and offsets for size variants filesize command

--- app/Console/Commands/VariantFilesize.php
+++ app/Console/Commands/VariantFilesize.php
@@ -12,7 +12,7 @@ class VariantFilesize extends Command
 	 *
 	 * @var string
 	 */
-	protected $signature = 'lychee:variant_filesize';
+	protected $signature = 'lychee:variant_filesize {limit=50 : number of photos to process at once (0 means all)}';
 
 	/**
 	 * The console command description.
@@ -28,29 +28,45 @@ class VariantFilesize extends Command
 	 */
 	public function handle()
 	{
-		if ($this->confirm('This command can take tens of minutes for large instances. Do you really want to run it now ?')) {
-			$variants = SizeVariant::query()
-				->where('filesize', '=', 0)
-				->get();
+		$limit = intval($this->argument('limit'));
 
-			if (count($variants) == 0) {
+		if ($this->confirm('This command can take a long time for large instances. Do you really want to run it now ?')) {
+			$variants_query = SizeVariant::query()
+				->where('filesize', '=', -1)->orderBy('id');
+
+			$count = $variants_query->count();
+			if ($count == 0) {
 				$this->line('All filesize variants already set in database.');
 
 				return false;
 			}
 
-			/* @var SizeVariant $variant */
-			$this->withProgressBar($variants, function ($variant) {
-				$fullPath = $variant->full_path;
-				if (file_exists($fullPath)) {
-					$variant->filesize = filesize($fullPath);
-					if (!$variant->save()) {
-						$this->line('Failed to update filesize for ' . $fullPath . '.');
+			// Number of queries
+			$pages = ceil($count / $limit);
+
+			$bar = $this->output->createProgressBar($count);
+			$bar->start();
+
+			for ($i = 0; $i < $pages; $i++) {
+				// No need to offset, because previous variants have no -1 filesize anymore
+				$variants = $variants_query->limit($limit)->get();
+
+				/* @var SizeVariant $variant */
+				foreach ($variants as $variant) {
+					$fullPath = $variant->full_path;
+					if (file_exists($fullPath)) {
+						$variant->filesize = filesize($fullPath);
+						if (!$variant->save()) {
+							$this->line('Failed to update filesize for ' . $fullPath . '.');
+						}
+					} else {
+						$this->line('File does not exist for ' . $fullPath . '.');
 					}
-				} else {
-					$this->line('File does not exist for ' . $fullPath . '.');
+					$bar->advance();
 				}
-			});
+			}
+
+			$bar->finish();
 		}
 	}
 }
