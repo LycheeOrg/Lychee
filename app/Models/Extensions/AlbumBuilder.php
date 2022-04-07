558ddebffc70ea63c262908e6c01fb24b629486a
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-04-07 20:25:03 +0200

    Use aggregate function MIN/MAX to not mislead MySQL optimizer.

--- app/Models/Extensions/AlbumBuilder.php
+++ app/Models/Extensions/AlbumBuilder.php
@@ -3,8 +3,8 @@
 namespace App\Models\Extensions;
 
 use App\Models\Album;
-use App\Models\Photo;
-use Kalnoy\Nestedset\QueryBuilder;
+use Illuminate\Support\Facades\DB;
+use Kalnoy\Nestedset\QueryBuilder as NSQueryBuilder;
 
 /**
  * Specialized query builder for {@link \App\Models\Album}.
@@ -17,10 +17,14 @@ use Kalnoy\Nestedset\QueryBuilder;
  * used as a sub-query which will not hydrate actual models.
  * Thus, a global scope unnecessarily complicates queries in many cases.
  */
-class AlbumBuilder extends QueryBuilder
+class AlbumBuilder extends NSQueryBuilder
 {
 	/**
 	 * Get the hydrated models without eager loading.
+	 *
+	 * Adds the "virtual" columns min_taken_at and max_taken_at to the query,
+	 * if a "full" model is requested, i.e. if the selected columns are
+	 * `*` or not given at all.
 	 *
 	 * @param array|string $columns
 	 *
@@ -33,23 +37,52 @@ class AlbumBuilder extends QueryBuilder
 			($columns == ['*'] || $columns == ['albums.*']) &&
 			($baseQuery->columns == ['*'] || $baseQuery->columns == ['albums.*'] || $baseQuery->columns == null)
 		) {
+			// Note:
+			//  1. The order of JOINS is important.
+			//     Although `JOIN` is cumulative, i.e.
+			//     `photos JOIN albums` and `albums JOIN photos`
+			//     should be identical, it is not with respect to the
+			//     MySQL query optimizer.
+			//     For an efficient query it is paramount, that the
+			//     query first filters out all child albums and then
+			//     selects the most/least recent photo within the child
+			//     albums.
+			//     If the JOIN starts with photos, MySQL first selects
+			//     all photos of the entire gallery.
+			//  2. The query must use the aggregation functions
+			//     `MIN`/`MAX`, we must not use `ORDER BY ... LIMIT 1`.
+			//     Otherwise, the MySQL optimizer first selects the
+			//     photos and then joins with albums (i.e. the same
+			//     effect as above).
+			//     The background is rather difficult to explain, but is
+			//     due to MySQL's "Limit Query Optimization"
+			//     (https://dev.mysql.com/doc/refman/8.0/en/limit-optimization.html).
+			//     Basically, if MySQL sees an `ORDER BY ... LIMIT ...`
+			//     construction and has an applicable index for that,
+			//     MySQL's built-in heuristic chooses that index with high
+			//     priority and does not consider any alternatives.
+			//     In this specific case, this heuristic fails splendidly.
+			//
+			// Further note, that PostgreSQL's optimizer is not affected
+			// by any of these tricks.
+			// The optimized query plan for PostgreSQL is always the same.
+			// Good PosgreSQL :-)
+			//
+			// We must not use `Album::query->` to start the query, but
+			// use a non-Eloquent query here to avoid an infinite loop
+			// with this query builder.
+			$minTsSelect = DB::table('albums', 'a')
+				->select(DB::raw('MIN(taken_at)'))
+				->join('photos', 'album_id', '=', 'a.id')
+				->whereColumn('a._lft', '>=', 'albums._lft')
+				->whereColumn('a._rgt', '<=', 'albums._rgt')
+				->whereNotNull('taken_at');
+
+			$maxTsSelect = $minTsSelect->clone()->select(DB::raw('MAX(taken_at)'));
+
 			$this->addSelect([
-				'min_taken_at' => Photo::query()
-					->select('taken_at')
-					->join('albums as a', 'a.id', '=', 'album_id')
-					->whereColumn('a._lft', '>=', 'albums._lft')
-					->whereColumn('a._rgt', '<=', 'albums._rgt')
-					->whereNotNull('taken_at')
-					->orderBy('taken_at', 'asc')
-					->limit(1),
-				'max_taken_at' => Photo::query()
-					->select('taken_at')
-					->join('albums as a', 'a.id', '=', 'album_id')
-					->whereColumn('a._lft', '>=', 'albums._lft')
-					->whereColumn('a._rgt', '<=', 'albums._rgt')
-					->whereNotNull('taken_at')
-					->orderBy('taken_at', 'desc')
-					->limit(1),
+				'min_taken_at' => $minTsSelect,
+				'max_taken_at' => $maxTsSelect,
 			]);
 		}
 
