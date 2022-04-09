128c9b13570e578842853c40833aa01684839e13
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-04-09 20:17:59 +0200

    Added forgotten file

---
+++ app/Exceptions/PhotoResyncedException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exceptions;
+
+/**
+ * PhotoReSyncedException.
+ *
+ * Returns status code 409 (Conflict) to an HTTP client.
+ */
+class PhotoResyncedException extends PhotoSkippedException
+{
+	public const DEFAULT_MESSAGE = 'The photo has been skipped, but its metadata has been updated';
+
+	public function __construct(string $message = self::DEFAULT_MESSAGE, \Throwable $previous = null)
+	{
+		parent::__construct($message, $previous);
+	}
+}
