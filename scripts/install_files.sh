390540532a7d11acf48609058a4ac896914c8143
qwerty287 <ndev@web.de>
2022-05-06 12:46:20 +0200

    Fix coloring in scripts

--- scripts/install_files.sh
+++ scripts/install_files.sh
@@ -4,19 +4,19 @@ GREEN="\033[38;5;010m"
 YELLOW="\033[38;5;011m"
 ORANGE="\033[38;5;214m"
 
-echo "\n${YELLOW}creating file for css personalization${NO_COLOR}"
+printf "\n${YELLOW}creating file for css personalization${NO_COLOR}\n"
 echo "touch public/dist/user.css"
 touch public/dist/user.css
 
-echo "\n${YELLOW}creating default sqlite database${NO_COLOR}"
+printf "\n${YELLOW}creating default sqlite database${NO_COLOR}\n"
 echo "touch database/database.sqlite"
 touch database/database.sqlite
 
-echo "\n${YELLOW}setting up hooks for git pull and git commits${NO_COLOR}"
+printf "\n${YELLOW}setting up hooks for git pull and git commits${NO_COLOR}\n"
 echo "cp scripts/pre-commit .git/hooks/"
 cp scripts/pre-commit .git/hooks/
 echo "cp scripts/post-merge .git/hooks/"
 cp scripts/post-merge .git/hooks/
 
-echo "\n${ORANGE}To disable the call of composer and migration on pull add${NO_COLOR}"
-echo "${ORANGE}a file named '.NO_AUTO_COMPOSER_MIGRATE' in this directory.${NO_COLOR}\n"
+printf "\n${ORANGE}To disable the call of composer and migration on pull add${NO_COLOR}\n"
+printf "${ORANGE}a file named '.NO_AUTO_COMPOSER_MIGRATE' in this directory.${NO_COLOR}\n"
