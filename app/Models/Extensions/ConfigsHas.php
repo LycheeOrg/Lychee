19016e1842ef866cfa0973d1cf1389764a144ca1
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-05-08 18:30:18 +0200

    Fixed Config and ConfigHas

--- app/Models/Extensions/ConfigsHas.php
+++ app/Models/Extensions/ConfigsHas.php
@@ -2,8 +2,10 @@
 
 namespace App\Models\Extensions;
 
-use App\Models\Logs;
-use Exception;
+use App\Exceptions\ExternalComponentMissingException;
+use App\Exceptions\Handler;
+use App\Exceptions\Internal\InvalidConfigOption;
+use App\Exceptions\Internal\QueryBuilderException;
 
 trait ConfigsHas
 {
@@ -27,31 +29,26 @@ trait ConfigsHas
 		// 1: Exiftool is available
 		// 2: Not yet tested if exiftool is available
 
-		$has_exiftool = intval(self::get_value('has_exiftool'));
+		$has_exiftool = intval(self::get_value('has_exiftool', 2));
 
 		// value not yet set -> let's see if exiftool is available
-		if ($has_exiftool == 2) {
+		if ($has_exiftool === 2) {
 			try {
 				$path = exec('command -v exiftool');
-				if ($path == '') {
-					self::set('has_exiftool', 0);
-					$has_exiftool = false;
-				} else {
-					self::set('has_exiftool', 1);
-					$has_exiftool = true;
-				}
-			} catch (Exception $e) {
-				self::set('has_exiftool', 0);
-				$has_exiftool = false;
-				Logs::warning(__METHOD__, __LINE__, 'exec is disabled, has_exiftool has been set to 0.');
+			} catch (\Exception $e) {
+				$path = '';
+				Handler::reportSafely(new ExternalComponentMissingException('could not find exiftool, `has_exiftool` will be set to 0', $e));
 			}
-		} elseif ($has_exiftool == 1) {
-			$has_exiftool = true;
-		} else {
-			$has_exiftool = false;
+			$has_exiftool = empty($path) ? 0 : 1;
+			try {
+				self::set('has_exiftool', $has_exiftool);
+			} catch (InvalidConfigOption|QueryBuilderException $e) {
+				// If we could not save the detected setting, still proceed
+				Handler::reportSafely($e);
+			}
 		}
 
-		return $has_exiftool;
+		return $has_exiftool === 1;
 	}
 
 	/**
@@ -64,30 +61,25 @@ trait ConfigsHas
 		// 1: ffmpeg is available
 		// 2: Not yet tested if ffmpeg is available
 
-		$has_ffmpeg = intval(self::get_value('has_ffmpeg'));
+		$has_ffmpeg = intval(self::get_value('has_ffmpeg', 2));
 
 		// value not yet set -> let's see if ffmpeg is available
-		if ($has_ffmpeg == 2) {
+		if ($has_ffmpeg === 2) {
 			try {
 				$path = exec('command -v ffmpeg');
-				if ($path == '') {
-					self::set('has_ffmpeg', 0);
-					$has_ffmpeg = false;
-				} else {
-					self::set('has_ffmpeg', 1);
-					$has_ffmpeg = true;
-				}
-			} catch (Exception $e) {
-				self::set('has_ffmpeg', 0);
-				$has_ffmpeg = false;
-				Logs::warning(__METHOD__, __LINE__, 'exec is disabled, set_ffmpeg has been set to 0.');
+			} catch (\Exception $e) {
+				$path = '';
+				Handler::reportSafely(new ExternalComponentMissingException('could not find ffmpeg, `has_ffmpeg` will be set to 0', $e));
 			}
-		} elseif ($has_ffmpeg == 1) {
-			$has_ffmpeg = true;
-		} else {
-			$has_ffmpeg = false;
+			$has_ffmpeg = empty($path) ? 0 : 1;
+			try {
+				self::set('has_ffmpeg', $has_ffmpeg);
+			} catch (InvalidConfigOption|QueryBuilderException $e) {
+				// If we could not save the detected setting, still proceed
+				Handler::reportSafely($e);
+			}
 		}
 
-		return $has_ffmpeg;
+		return $has_ffmpeg === 1;
 	}
 }
