0e13483e2d8ca617b3572a552bf69344d5d1a1a3
Ruediger Kessel <ruediger.kessel@gmail.com>
2022-05-16 11:16:46 +0200

    Verifying the usage of the LDAP test interface

---
+++ tests/Feature/VerifyTestInterfaceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class VerifyTestInterfaceTest extends TestCase
+{
+	protected function _debug($myDebugVar, $label = '')
+	{
+		$msg = str_replace(PHP_EOL, ' ', print_r($myDebugVar, true));
+		while (str_contains($msg, '  ')) {
+			$msg = str_replace('  ', ' ', $msg);
+		}
+		error_log($label . "'" . trim($msg) . "'");
+	}
+
+	public function testVerifyTestInterface()
+	{
+		exec('grep test_LDAP_ * -r -l', $ret);
+		$ret = implode(' ', $ret);
+		$this->assertEquals($ret,'app/LDAP/LDAPFunctions.php tests/Feature/VerifyTestInterfaceTest.php tests/Feature/LDAPTest.php',
+			'LDAP test interface is missused!');
+	}
+}
