af9dcc4bcaa4fb5150e2d4d881fa89c9bc8507d8
Ruediger Kessel <ruediger.kessel@gmail.com>
2022-05-17 21:41:42 +0200

    Allow for null passwords

--- app/Actions/User/Create.php
+++ app/Actions/User/Create.php
@@ -13,7 +13,7 @@ class Create
 	 * @throws InvalidPropertyException
 	 * @throws ModelDBException
 	 */
-	public function do(string $username, string $password, bool $mayUpload, bool $isLocked, string $email = null, string $displayName = null): User
+	public function do(string $username, ?string $password, bool $mayUpload, bool $isLocked, string $email = null, string $displayName = null): User
 	{
 		if (User::query()->where('username', '=', $username)->count()) {
 			throw new ConflictingPropertyException('Username already exists');
@@ -23,7 +23,10 @@ class Create
 			$user->may_upload = $mayUpload;
 			$user->is_locked = $isLocked;
 			$user->username = $username;
-			$user->password = bcrypt($password);
+			// null passwords should not be hashed
+			if (!is_null($password)) {
+				$user->password = bcrypt($password);
+			}
 			$user->email = $email;
 			$user->display_name = $displayName;
 		} catch (\InvalidArgumentException $e) {
