6a2d43ecb1b8442549478edb0e6ce1f33a5b7415
Ruediger Kessel <ruediger.kessel@gmail.com>
2022-05-18 13:44:39 +0200

    cleanup

--- app/Console/Commands/LDAPUpdateAllUsers.php
+++ app/Console/Commands/LDAPUpdateAllUsers.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use App\LDAP\LDAPActions;
 use App\Models\Configs;
 use Illuminate\Console\Command;
-use Symfony\Component\Console\Exception\ExceptionInterface as SymfonyConsoleException;
 
 class LDAPUpdateAllUsers extends Command
 {
@@ -22,16 +21,6 @@ class LDAPUpdateAllUsers extends Command
 	 * @var string
 	 */
 	protected $description = 'Updates the users table from the LDAP server if LDAP is enabled';
-
-	/**
-	 * Create a new command instance.
-	 *
-	 * @throws SymfonyConsoleException
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
 
 	/**
 	 * Execute the console command.
