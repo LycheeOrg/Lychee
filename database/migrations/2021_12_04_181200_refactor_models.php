f07287711d93d27ecdf245c0119775eda40e6f1e
Ruediger Kessel <ruediger.kessel@gmail.com>
2022-05-18 19:29:18 +0200

    drop requirement for email being unique. see comment.

--- database/migrations/2021_12_04_181200_refactor_models.php
+++ database/migrations/2021_12_04_181200_refactor_models.php
@@ -467,6 +467,20 @@ class RefactorModels extends Migration
 	 *
 	 * @return void
 	 */
+	// The requirement for email adresses being unique has to be dropped in case
+	// the users are managed externally e.g. by an ldap-server. The reason is an inherent
+	// update problem.
+	// Since the external server needs to take care of the uniqueness of the e-mail address,
+	// nothing can be done in lychee in case if it is not unique.
+	// If the uid of a user (his login name) gets changed, but his e-mail address does not then
+	// the user cannot login any more if lychee uses LDAP. The reason is that lychee cannot update
+	// the users table since the e-mail already exists and without a new entry in the users table
+	// a login is not possible.
+	// The only solution would be to develop a clever purge strategy that deletes non-existing users
+	// first and call this strategy in case the users account cannot be created during login.
+	// But if the external login provider delivers non unique email addresses, there will be no way out.
+	// Since the uniqueness of the email address is not a data requirement, but just a measure to prevent
+	// that people not get registered twice, I suggest to drop the unique requirement in the database.
 	private function createUsersTableUp(): void
 	{
 		Schema::create('users', function (Blueprint $table) {
@@ -475,7 +489,7 @@ class RefactorModels extends Migration
 			$table->dateTime('updated_at', 6)->nullable(false);
 			$table->string('username', 100)->nullable(false)->unique();
 			$table->string('password', 100)->nullable(true);
-			$table->string('email', 100)->nullable()->unique();
+			$table->string('email', 100)->nullable();
 			$table->boolean('may_upload')->nullable(false)->default(false);
 			$table->boolean('is_locked')->nullable(false)->default(false);
 			$table->rememberToken();
@@ -495,7 +509,7 @@ class RefactorModels extends Migration
 			$table->dateTime('updated_at')->nullable(false);
 			$table->string('username', 100)->nullable(false)->unique();
 			$table->string('password', 100)->nullable(true);
-			$table->string('email', 100)->nullable()->unique();
+			$table->string('email', 100)->nullable();
 			$table->boolean('upload')->nullable(false)->default(false);
 			$table->boolean('lock')->nullable(false)->default(false);
 			$table->rememberToken();
