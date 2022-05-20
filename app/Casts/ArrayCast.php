e7b3a84c135eb6257365d8820317f165afe073e7
ildyria <beviguier@gmail.com>
2022-05-20 22:09:54 +0200

    182 - app/Casts/*.php

--- app/Casts/ArrayCast.php
+++ app/Casts/ArrayCast.php
@@ -10,20 +10,20 @@ class ArrayCast implements CastsAttributes
 	/**
 	 * @param Model  $model      the associated model class
 	 * @param string $key        the name of the SQL column holding the stringified array
-	 * @param string $value      the stringified array
+	 * @param mixed  $value      the stringified array
 	 * @param array  $attributes all SQL attributes of the entity
 	 *
 	 * @return array the array
 	 */
 	public function get($model, string $key, $value, array $attributes): array
 	{
-		return empty($value) ? [] : explode(',', $value);
+		return empty($value) ? [] : explode(',', strval($value));
 	}
 
 	/**
 	 * @param Model  $model      the associated model class
 	 * @param string $key        the name of the SQL column holding the stringified array
-	 * @param array  $value      the array
+	 * @param mixed  $value      the array
 	 * @param array  $attributes
 	 *
 	 * @return array An associative map of SQL columns and their values
@@ -33,6 +33,7 @@ class ArrayCast implements CastsAttributes
 		// Normalize the input value
 		// The array must not contain empty tags and tags which contain a comma
 		// TODO: Either use a separate table to store the tags or another encoding (e.g. JSON) which also allows commas in tags
+
 		$arr = empty($value) ? [] : array_values(array_filter(
 			$value,
 			fn ($elem) => ($elem !== null && $elem !== '' && !str_contains($elem, ','))
