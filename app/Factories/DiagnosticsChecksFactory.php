7fe6a9b53baf39fe379902d2be97f60a08142817
ildyria <beviguier@gmail.com>
2022-05-20 14:19:34 +0200

    334 - app/Factories/DiagnosticsChecksFactory.php

--- app/Factories/DiagnosticsChecksFactory.php
+++ app/Factories/DiagnosticsChecksFactory.php
@@ -12,10 +12,10 @@ class DiagnosticsChecksFactory
 	public function __construct()
 	{
 		$checks_namespace = 'App\Actions\Diagnostics\Checks';
-		$list_checks = scandir(__DIR__ . '/../Actions/Diagnostics/Checks');
+		$list_checks = \Safe\scandir(__DIR__ . '/../Actions/Diagnostics/Checks');
 
 		for ($i = 0; $i < count($list_checks); $i++) {
-			$class_candidate = $checks_namespace . '\\' . substr($list_checks[$i], 0, -4);
+			$class_candidate = $checks_namespace . '\\' . \Safe\substr($list_checks[$i], 0, -4);
 
 			if (is_subclass_of($class_candidate, DiagnosticCheckInterface::class)) {
 				$this->checks[] = $class_candidate;
