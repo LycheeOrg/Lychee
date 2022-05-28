980d0e3e2a7e1197c844571610688652545f506a
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-05-28 15:19:41 +0200

    Added test for GD handler

---
+++ tests/Feature/PhotosAddGDTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * We don't care for unhandled exceptions in tests.
+ * It is the nature of a test to throw an exception.
+ * Without this suppression we had 100+ Linter warning in this file which
+ * don't help anything.
+ *
+ * @noinspection PhpDocMissingThrowsInspection
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+
+namespace Tests\Feature;
+
+use App\Models\Configs;
+
+/**
+ * Runs the tests of {@link PhotosAddTestAbstract} with GD as image handler.
+ */
+class PhotosAddGDTest extends PhotosAddTestAbstract
+{
+	protected int $hasImagickInit;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		$this->hasImagickInit = (int) Configs::get_value(self::CONFIG_HAS_IMAGICK, 0);
+		Configs::set(self::CONFIG_HAS_IMAGICK, 0);
+
+		if (Configs::hasImagick()) {
+			static::markTestSkipped('Imagick still enabled although it shouldn\'t. Test Skipped.');
+		}
+	}
+
+	public function tearDown(): void
+	{
+		Configs::set(self::CONFIG_HAS_IMAGICK, $this->hasImagickInit);
+		parent::tearDown();
+	}
+}
