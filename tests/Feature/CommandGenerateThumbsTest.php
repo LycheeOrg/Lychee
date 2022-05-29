634d2f50004c9726cb02fa6fd05dd05f280d1fcf
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-05-29 20:21:15 +0200

    First positive test for command `lychee:generate_thumbs`.

--- tests/Feature/CommandGenerateThumbsTest.php
+++ tests/Feature/CommandGenerateThumbsTest.php
@@ -1,10 +1,23 @@
 <?php
 
+/**
+ * We don't care for unhandled exceptions in tests.
+ * It is the nature of a test to throw an exception.
+ * Without this suppression we had 100+ Linter warning in this file which
+ * don't help anything.
+ *
+ * @noinspection PhpDocMissingThrowsInspection
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+
 namespace Tests\Feature;
 
+use App\Models\SizeVariant;
+use Illuminate\Support\Facades\DB;
+use Tests\Feature\Base\PhotoTestBase;
 use Tests\TestCase;
 
-class CommandGenerateThumbsTest extends TestCase
+class CommandGenerateThumbsTest extends PhotoTestBase
 {
 	public const COMMAND = 'lychee:generate_thumbs';
 
@@ -26,5 +39,30 @@ class CommandGenerateThumbsTest extends TestCase
 		$this->artisan(self::COMMAND, ['type' => 'small'])
 			->expectsOutput('No picture requires small.')
 			->assertExitCode(0);
+	}
+
+	public function testThumbRecreation(): void
+	{
+		$photo1 = static::convertJsonToObject($this->photos_tests->upload(
+			static::createUploadedFile(TestCase::SAMPLE_FILE_NIGHT_IMAGE)
+		));
+
+		// Remove the size variant "small" from disk and from DB
+		\Safe\unlink(public_path($photo1->size_variants->small->url));
+		DB::table('size_variants')
+			->where('photo_id', '=', $photo1->id)
+			->where('type', '=', SizeVariant::SMALL)
+			->delete();
+
+		// Re-create it
+		$this->artisan(self::COMMAND, ['type' => 'small'])
+			->assertExitCode(0);
+
+		// Get updated photo and check if size variants has been re-created
+		$photo2 = static::convertJsonToObject($this->photos_tests->get($photo1->id));
+		static::assertNotNull($photo2->size_variants->small);
+		static::assertEquals($photo1->size_variants->small->width, $photo2->size_variants->small->width);
+		static::assertEquals($photo1->size_variants->small->height, $photo2->size_variants->small->height);
+		static::assertFileExists(public_path($photo2->size_variants->small->url));
 	}
 }
