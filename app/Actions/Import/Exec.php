6a776317885201b1af72b049b6ec35847eb98bdf
ildyria <beviguier@gmail.com>
2022-06-01 23:24:23 +0200

    26 ?

--- app/Actions/Import/Exec.php
+++ app/Actions/Import/Exec.php
@@ -90,7 +90,7 @@ class Exec
 			echo $report->toCLIString() . PHP_EOL;
 		}
 
-		if ($report instanceof ImportEventReport && $report->getException()) {
+		if ($report instanceof ImportEventReport && $report->getException() != null) {
 			Handler::reportSafely($report->getException());
 		}
 	}
