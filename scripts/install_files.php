1739588ca872152557892e48828f8db474dfe885
qwerty287 <ndev@web.de>
2022-06-05 09:18:25 +0200

    Use PHP_EOL

--- scripts/install_files.php
+++ scripts/install_files.php
@@ -7,15 +7,15 @@ $GREEN = "\033[38;5;010m";
 $YELLOW = "\033[38;5;011m";
 $ORANGE = "\033[38;5;214m";
 
-echo "\n${YELLOW}creating file for CSS personalization$NO_COLOR\n";
+echo "\n${YELLOW}creating file for CSS personalization$NO_COLOR" . PHP_EOL;
 \Safe\touch('public/dist/user.css');
 
-echo "\n${YELLOW}creating default SQLite database$NO_COLOR\n";
+echo "\n${YELLOW}creating default SQLite database$NO_COLOR" . PHP_EOL;
 \Safe\touch('database/database.sqlite');
 
-echo "\n${YELLOW}setting up hooks for git pull and git commits$NO_COLOR\n";
+echo "\n${YELLOW}setting up hooks for git pull and git commits$NO_COLOR" . PHP_EOL;
 \Safe\copy('scripts/pre-commit', '.git/hooks/pre-commit');
 \Safe\copy('scripts/post-merge', '.git/hooks/post-merge');
 
-echo "\n${ORANGE}To disable the call of composer and migration on pull add$NO_COLOR\n";
-echo "${ORANGE}a file named '.NO_AUTO_COMPOSER_MIGRATE' in this directory.$NO_COLOR\n";
+echo "\n${ORANGE}To disable the call of composer and migration on pull add$NO_COLOR" . PHP_EOL;
+echo "${ORANGE}a file named '.NO_AUTO_COMPOSER_MIGRATE' in this directory.$NO_COLOR" . PHP_EOL;
