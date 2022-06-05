efce82c6770a4815a2be3d9c20c13370a891976f
qwerty287 <ndev@web.de>
2022-06-05 12:16:15 +0200

    Add argon opts back

--- config/hashing.php
+++ config/hashing.php
@@ -30,4 +30,21 @@ return [
 	'bcrypt' => [
 		'rounds' => env('BCRYPT_ROUNDS', 10),
 	],
+
+	/*
+	|--------------------------------------------------------------------------
+	| Argon Options
+	|--------------------------------------------------------------------------
+	|
+	| Here you may specify the configuration options that should be used when
+	| passwords are hashed using the Argon algorithm. These will allow you
+	| to control the amount of time it takes to hash the given password.
+	|
+	*/
+
+	'argon' => [
+		'memory' => 1024,
+		'threads' => 2,
+		'time' => 2,
+	],
 ];
