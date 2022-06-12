fdd1521197016ab57273a64e8397e6852178b6b8
qwerty287 <ndev@web.de>
2022-06-12 08:40:01 +0200

    Add DB options back

--- config/database.php
+++ config/database.php
@@ -38,6 +38,7 @@ return [
 			'url' => env('DATABASE_URL'),
 			'database' => env('DB_DATABASE', database_path('database.sqlite')),
 			'prefix' => '',
+			'foreign_key_constraints' => true,
 		],
 
 		'mysql' => [
@@ -48,6 +49,7 @@ return [
 			'database' => env('DB_DATABASE', 'forge'),
 			'username' => env('DB_USERNAME', 'forge'),
 			'password' => env('DB_PASSWORD', ''),
+			'unix_socket' => env('DB_SOCKET', ''),
 			'charset' => 'utf8mb4',
 			'collation' => 'utf8mb4_unicode_ci',
 			// The timezone of the DB connection should always be set in order
