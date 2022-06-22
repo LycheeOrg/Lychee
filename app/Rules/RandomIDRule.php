5ad3be26d55bc305cb66c835f724bbe8c74b01f0
ildyria <beviguier@gmail.com>
2022-06-22 11:45:13 +0200

    use function instead of hardcoded /Safe/

--- app/Rules/RandomIDRule.php
+++ app/Rules/RandomIDRule.php
@@ -4,6 +4,7 @@ namespace App\Rules;
 
 use App\Contracts\HasRandomID;
 use Illuminate\Contracts\Validation\Rule;
+use function Safe\preg_match;
 
 class RandomIDRule implements Rule
 {
@@ -23,7 +24,7 @@ class RandomIDRule implements Rule
 			(
 				$value === null &&
 				$this->isNullable
-			) || \Safe\preg_match('/^[-_a-zA-Z0-9]{' . HasRandomID::ID_LENGTH . '}$/', $value) === 1;
+			) || preg_match('/^[-_a-zA-Z0-9]{' . HasRandomID::ID_LENGTH . '}$/', $value) === 1;
 	}
 
 	/**
