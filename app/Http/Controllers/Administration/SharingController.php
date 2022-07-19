b87d010f2b09ec6f40aa413a9a167013d54629eb
ildyria <beviguier@gmail.com>
2022-07-19 14:40:54 +0200

    fixes

--- app/Http/Controllers/Administration/SharingController.php
+++ app/Http/Controllers/Administration/SharingController.php
@@ -31,7 +31,7 @@ class SharingController extends Controller
 	{
 		// Note: This test is part of the request validation for the other
 		// methods of this class.
-		if (!Gate::check('can-upload')) {
+		if (!Gate::any('admin', 'can-upload')) {
 			throw new UnauthorizedException('Upload privilege required');
 		}
 
