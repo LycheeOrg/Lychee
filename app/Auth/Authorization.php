113b0313bafaecbc878c01c7cdd062bc07973433
ildyria <beviguier@gmail.com>
2022-07-21 11:45:00 +0200

    fix comment and missing return

--- app/Auth/Authorization.php
+++ app/Auth/Authorization.php
@@ -44,14 +44,15 @@ class Authorization
 			/** @var User|null $adminUser */
 			$adminUser = User::query()->find(0);
 			Auth::login($adminUser);
+
+			return true;
 		}
 
 		return false;
 	}
 
 	/**
-	 * Given a username and password, create an admin user in the database.
-	 * Do note that the password is set NOT HASHED.
+	 * Reset admin user: set username and password to empty string ''.
 	 *
 	 * @return bool actually always true
 	 *
