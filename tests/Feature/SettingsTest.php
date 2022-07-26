3215ae90395b991b5ab644ed12dd7beba88f43e8
ildyria <beviguier@gmail.com>
2022-07-26 17:22:42 +0200

    fix merge from master

--- tests/Feature/SettingsTest.php
+++ tests/Feature/SettingsTest.php
@@ -13,30 +13,32 @@
 namespace Tests\Feature;
 
 use App\DTO\SortingCriterion;
-use App\Facades\AccessControl;
 use App\Http\Requests\Settings\SetSortingRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class SettingsTest extends TestCase
 {
 	public function testSetSorting(): void
 	{
-		AccessControl::log_as_id(0);
+		Auth::loginUsingId(0);
 
 		$this->postJson('/api/Settings::setSorting',
-		[
-			SetSortingRequest::ALBUM_SORTING_COLUMN_ATTRIBUTE => SortingCriterion::COLUMN_CREATED_AT,
-			SetSortingRequest::PHOTO_SORTING_COLUMN_ATTRIBUTE => SortingCriterion::COLUMN_CREATED_AT,
-			SetSortingRequest::ALBUM_SORTING_ORDER_ATTRIBUTE => SortingCriterion::ASC,
-			SetSortingRequest::PHOTO_SORTING_ORDER_ATTRIBUTE => SortingCriterion::ASC,
-		])->assertStatus(204);
+			[
+				SetSortingRequest::ALBUM_SORTING_COLUMN_ATTRIBUTE => SortingCriterion::COLUMN_CREATED_AT,
+				SetSortingRequest::PHOTO_SORTING_COLUMN_ATTRIBUTE => SortingCriterion::COLUMN_CREATED_AT,
+				SetSortingRequest::ALBUM_SORTING_ORDER_ATTRIBUTE => SortingCriterion::ASC,
+				SetSortingRequest::PHOTO_SORTING_ORDER_ATTRIBUTE => SortingCriterion::ASC,
+			])->assertStatus(204);
 
-		AccessControl::logout();
+		Auth::logout();
+		Session::flush();
 	}
 
 	public function testSetSortingWithIllegalAlbumAttribute(): void
 	{
-		AccessControl::log_as_id(0);
+		Auth::loginUsingId(0);
 
 		$response = $this->postJson('/api/Settings::setSorting',
 			[
@@ -49,12 +51,13 @@ class SettingsTest extends TestCase
 		$response->assertStatus(422);
 		$response->assertSee('sorting albums column must be null or one out of');
 
-		AccessControl::logout();
+		Auth::logout();
+		Session::flush();
 	}
 
 	public function testSetSortingWithIllegalPhotoAttribute(): void
 	{
-		AccessControl::log_as_id(0);
+		Auth::loginUsingId(0);
 
 		$response = $this->postJson('/api/Settings::setSorting',
 			[
@@ -67,12 +70,13 @@ class SettingsTest extends TestCase
 		$response->assertStatus(422);
 		$response->assertSee('sorting photos column must be null or one out of');
 
-		AccessControl::logout();
+		Auth::logout();
+		Session::flush();
 	}
 
 	public function testSetSortingWithUnknownOrder(): void
 	{
-		AccessControl::log_as_id(0);
+		Auth::loginUsingId(0);
 
 		$response = $this->postJson('/api/Settings::setSorting',
 			[
@@ -85,6 +89,7 @@ class SettingsTest extends TestCase
 		$response->assertStatus(422);
 		$response->assertSee('order must be either');
 
-		AccessControl::logout();
+		Auth::logout();
+		Session::flush();
 	}
 }
