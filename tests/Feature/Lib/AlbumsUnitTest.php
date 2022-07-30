3e5a127314362c5070b9130c0c26e53a890fd144
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-07-30 18:18:20 +0200

    Add password parameter

--- tests/Feature/Lib/AlbumsUnitTest.php
+++ tests/Feature/Lib/AlbumsUnitTest.php
@@ -259,6 +259,12 @@ class AlbumsUnitTest
 	 * @param bool        $nsfw
 	 * @param bool        $downloadable
 	 * @param bool        $share_button_visible
+	 * @param string|null $password             `null` does not change password
+	 *                                          settings;
+	 *                                          the empty string `''` removes
+	 *                                          a (potentially set) password;
+	 *                                          a non-empty string sets the
+	 *                                          password accordingly
 	 * @param int         $expectedStatusCode
 	 * @param string|null $assertSee
 	 */
@@ -270,10 +276,11 @@ class AlbumsUnitTest
 		bool $nsfw = false,
 		bool $downloadable = true,
 		bool $share_button_visible = true,
+		?string $password = null,
 		int $expectedStatusCode = 204,
 		?string $assertSee = null
 	): void {
-		$response = $this->testCase->postJson('/api/Album::setProtectionPolicy', [
+		$params = [
 			'grants_full_photo' => $full_photo,
 			'albumID' => $id,
 			'is_public' => $public,
@@ -281,7 +288,13 @@ class AlbumsUnitTest
 			'is_nsfw' => $nsfw,
 			'is_downloadable' => $downloadable,
 			'is_share_button_visible' => $share_button_visible,
-		]);
+		];
+
+		if ($password !== null) {
+			$params['password'] = $password;
+		}
+
+		$response = $this->testCase->postJson('/api/Album::setProtectionPolicy', $params);
 		$response->assertStatus($expectedStatusCode);
 		if ($assertSee) {
 			$response->assertSee($assertSee, false);
