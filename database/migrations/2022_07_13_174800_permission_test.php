72e5efd6bc38c6668607f7136bebc1ce55b1f517
ildyria <beviguier@gmail.com>
2022-08-29 18:57:00 +0200

    migration fix for public

--- database/migrations/2022_07_13_174800_permission_test.php
+++ database/migrations/2022_07_13_174800_permission_test.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 
 class PermissionTest extends Migration
 {
@@ -12,7 +13,14 @@ class PermissionTest extends Migration
 	 */
 	public function up(): void
 	{
+		$is_public = Str::endsWith(getcwd(), 'public');
+		if ($is_public) {
+			chdir('..');
+		}
 		Artisan::call('lychee:fix-permissions', ['--dry-run' => 1]);
+		if ($is_public) {
+			chdir('public');
+		}
 	}
 
 	/**
