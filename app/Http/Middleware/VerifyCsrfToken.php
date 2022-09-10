c5d2ff62ccfa44319f06b7ad228a2e4f8046d0a1
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-09-10 18:24:21 +0200

    Added custom guard to handle session or token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use App\Exceptions\Internal\QueryBuilderException;
-use App\Models\User;
+use App\Services\Auth\SessionOrTokenGuard;
 use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Session\TokenMismatchException;
-use Illuminate\Support\Facades\Auth;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -24,9 +22,11 @@ class VerifyCsrfToken extends Middleware
 	];
 
 	/**
-	 * The goal of this function is to allow to bypass the CSRF token requirement
-	 * if an Authorization value is provided in the header and matches the
-	 * token of a user.
+	 * Attempts to verify the CSRF token unless an API token is provided.
+	 *
+	 * Note, if the API token is given but invalid (i.e. refers to a
+	 * non-existing user), then {@link \App\Services\Auth\SessionOrTokenGuard}
+	 * bails out.
 	 *
 	 * @param Request $request
 	 * @param Closure $next
@@ -34,21 +34,12 @@ class VerifyCsrfToken extends Middleware
 	 * @return mixed
 	 *
 	 * @throws TokenMismatchException
-	 * @throws QueryBuilderException
 	 */
 	public function handle($request, Closure $next): mixed
 	{
-		$token = $request->header('Authorization');
-		if ($request->is('api/*') && is_string($token)) {
-			/** @var User|null $user */
-			$user = User::query()
-				->where('token', '=', hash('SHA512', $token))
-				->first();
-			if ($user instanceof User) {
-				Auth::loginUsingId($user->id);
-
-				return $next($request);
-			}
+		$token = $request->get(SessionOrTokenGuard::TOKEN_COLUMN_NAME);
+		if (is_string($token) && $token !== '') {
+			return $next($request);
 		}
 
 		return parent::handle($request, $next);
---
+++ app/Services/Auth/SessionOrTokenGuard.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Exceptions\BadRequestHeaderException;
+use Illuminate\Auth\SessionGuard;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * A custom Guard which combines the default Laravel Session Guard with
+ * a token Guard in an OR-like fashion.
+ *
+ * In Laravel terms a Guard is a service class which defines _how_ the user
+ * of a request is determined
+ * (see https://laravel.com/docs/9.x/authentication#introduction).
+ * This guard behaves as follows:
+ *
+ *  _ If a request has neither a session nor sends an API token, the request
+ *    counts as unauthenticated.
+ *    In other words, the returned user equals `null`.
+ *  - If a request has no session, but the request sends an API token, the
+ *    API token is used for user authentication.
+ *  - If a request has a session and sends no API token, the previously
+ *    stored user is loaded from the session.
+ *    If such a user does not exist, the request is unauthenticated.
+ *  - If a request has both a session _and_ sends an API token, this guard
+ *    ensures consistency between the both; i.e. the user stored inside the
+ *    session and indicated by the token must be the same.
+ *    Normally, a client is expected to either use session-based login or
+ *    use token authentication, not both simultaneously.
+ *    If a client is crazy enough to use both but fails to keep it consistent,
+ *    this counts as a client-side error.
+ *    Lychee also uses a session for other things like storing the list of
+ *    unlocked albums and a "jumping" user might lead to leaking undesired
+ *    information.
+ *
+ * Laravel ships with three guards out-of-the-box:
+ *
+ *  - {@link \Illuminate\Auth\SessionGuard}
+ *  - {@link \Illuminate\Auth\TokenGuard}
+ *  - {@link \Illuminate\Auth\RequestGuard}
+ *
+ * which behave in the obvious way:
+ *
+ *  - {@link \Illuminate\Auth\SessionGuard} is an stateful guard and works
+ *    together with {@link \Illuminate\Contracts\Session\Session}.
+ *    This guard expects that the user is once explicitly set via `loginAs`
+ *    (or similar methods). Then the guard stores the user inside the session
+ *    and fetches the previously stored user from the session during
+ *    subsequent request.
+ *  - {@link \Illuminate\Auth\TokenGuard} is a stateless guard and uses a
+ *    proprietary HTTP header to transmit an "API token".
+ *    The token must be sent by the client for each request.
+ *  - {@link \Illuminate\Auth\RequestGuard} is a stateless guard and uses a
+ *    standard HTTP authentication techniques (such as HTTP digest) via the
+ *    HTTP authentication header.
+ *
+ * The available guards are configured in `config/auth.php`.
+ * Note that the configuration file uses the term "guard" in a different way
+ * than above (Laravel I love you soooo much).
+ * In `config/auth.php` several "presets" can be configured below the array
+ * key `'guards'`, the actual guard to be used (as listed above) is configured
+ * via `'driver'`.
+ * The default preset to be used for all request unless stated otherwise on a
+ * route, is set in `default`.
+ * Laravel is built around the idea that a particular route uses either one
+ * of the guards above, i.e. _either_ uses `SessionGuard`, `TokenGuard` or
+ * `RequestGuard`.
+ *
+ * Lychee is special in this case.
+ * Lychee allows to use a session-based login _or_ a token authentication
+ * for the same route.
+ * Hence, we use a special guard.
+ *
+ * Laravel uses {@link \Illuminate\Auth\AuthManager::resolve()} to resolve
+ * the guard at runtime.
+ * The built-in guards {@link \Illuminate\Auth\SessionGuard} and
+ * {@link \Illuminate\Auth\TokenGuard} are created via
+ * the magic line
+ * `$driverMethod = 'create'.ucfirst($config['driver']).'Driver';`
+ * which is why the "special" identifiers `"session"` and `"token"` work
+ * inside the file `config/auth.php`.
+ * Custom identifiers can be registered `Auth::extend` and become a
+ * "custom creator" inside {@link \Illuminate\Auth\AuthManager::resolve()}.
+ */
+class SessionOrTokenGuard extends SessionGuard
+{
+	public const HTTP_TOKEN_HEADER = 'Authorization';
+	public const TOKEN_COLUMN_NAME = 'token';
+	public const TOKEN_HASH_METHOD = 'SHA512';
+
+	/**
+	 * Creates an instance of this guard.
+	 *
+	 * As this class inherits the Laravel Session Guard, this method is
+	 * copy-&-paste from
+	 * {@link \Illuminate\Auth\AuthManager::createSessionDriver()} which
+	 * creates the Laravel Session guard.
+	 *
+	 * @throws BindingResolutionException
+	 */
+	public static function createGuard(Application $app, string $name, array $config): self
+	{
+		$userProvider = Auth::createUserProvider($config['provider']);
+		$guard = new self($name, $userProvider, $app->make('session.store'));
+		$guard->setCookieJar($app->make('cookie'));
+		$guard->setDispatcher($app->make('events'));
+		$guard->setRequest($app->refresh('request', $guard, 'setRequest'));
+		if (isset($config['remember'])) {
+			$guard->setRememberDuration($config['remember']);
+		}
+
+		return $guard;
+	}
+
+	/**
+	 * Returns the user of the current request.
+	 *
+	 * This method is a merger of
+	 * {@link SessionGuard::user} and {@link \Illuminate\Auth\TokenGuard::user}.
+	 * This method is the main "working horse" and behaves as described in
+	 * the class comment.
+	 *
+	 * @return ?Authenticatable
+	 *
+	 * @throws BadRequestHeaderException
+	 * @throws \RuntimeException
+	 */
+	public function user(): Authenticatable|null
+	{
+		// If we've already retrieved the user for the current request we can just
+		// return it back immediately. We do not want to fetch the user data on
+		// every call to this method because that would be tremendously slow.
+		if ($this->user !== null) {
+			return $this->user;
+		}
+
+		// First, try to get a user by token.
+		$userByToken = $this->getUserByToken();
+
+		// Second, try to get a user by stored user ID on the session.
+		$userIdBySession = $this->session->get($this->getName());
+		$userBySession = $userIdBySession !== null ? $this->provider->retrieveById($userIdBySession) : null;
+
+		// Third, if `$userBySession` is null, but we decrypt a "recaller"
+		// cookie we attempt to pull the user data from that cookie which
+		// serves as a remember-me cookie
+		$recaller = $userBySession === null ? $this->recaller() : null;
+		$userByRecaller = $recaller !== null ? $this->userFromRecaller($recaller) : null;
+
+		// We step through the different combinations which may happen,
+		// because we use a combination of token and session.
+		if ($userBySession !== null) {
+			if ($userByToken === null || $userBySession->getAuthIdentifier() === $userByToken->getAuthIdentifier()) {
+				// We are good, no contradiction!
+				$this->setUser($userBySession);
+			} else {
+				throw new BadRequestHeaderException('Token- and session-based user mismatch');
+			}
+		} elseif ($userByToken !== null) {
+			// We handle a token-based user like {@link SessionGuard::user}
+			// handles a recalled user from a remember-me cookie except
+			// that we don't set the remember-me cookie.
+			$this->login($userByToken, false, true);
+		} elseif ($userByRecaller !== null) {
+			$this->login($userByRecaller, true, true);
+		} else {
+			// In the other cases, `$this->user` has implicitly been set by
+			// `$this->setUser` or `$this->login`.
+			$this->user = null; /* @phpstan-ignore-line; in {@link \Illuminate\Auth\GuardHelpers} this property is erroneously annotated as non-nullable */
+		}
+
+		return $this->user;
+	}
+
+	/**
+	 * Get the ID for the currently authenticated user.
+	 *
+	 * This is a fixed variant of {@link \Illuminate\Auth\TokenGuard::id}
+	 * which uses PHP 8 syntax and ensures that a value is always returned.
+	 * We don't use the complicated variant of {@link SessionGuard::id},
+	 * because {@link SessionOrTokenGuard::user()} ensures that
+	 * {@link SessionOrTokenGuard::$loggedOut} and
+	 * {@link SessionOrTokenGuard::$user} are always consistent.
+	 *
+	 * @return int|string|null
+	 *
+	 * @throws BadRequestHeaderException
+	 * @throws \RuntimeException
+	 */
+	public function id(): int|string|null
+	{
+		return $this->user()?->getAuthIdentifier();
+	}
+
+	/**
+	 * Logins the given user.
+	 *
+	 * If an API token is given, logging in another user than the user
+	 * given by the API token is considered an error.
+	 *
+	 * @param AuthenticatableContract $user
+	 * @param bool                    $remember
+	 * @param bool                    $skipTokenCheck
+	 *
+	 * @return void
+	 *
+	 * @throws BadRequestHeaderException
+	 * @throws \RuntimeException
+	 */
+	public function login(AuthenticatableContract $user, $remember = false, bool $skipTokenCheck = false): void
+	{
+		if (!$skipTokenCheck) {
+			$userByToken = $this->getUserByToken();
+			if ($userByToken !== null && $user->getAuthIdentifier() !== $userByToken->getAuthIdentifier()) {
+				throw new BadRequestHeaderException('Cannot login as another user than the one provided by the API token');
+			}
+		}
+
+		parent::login($user, $remember);
+	}
+
+	/**
+	 * Logs out the current user.
+	 *
+	 * An attempt to logout while an API token is provided is considered an
+	 * error.
+	 *
+	 * @return void
+	 *
+	 * @throws BadRequestHeaderException
+	 * @throws \RuntimeException
+	 */
+	public function logout(): void
+	{
+		if ($this->getUserByToken() !== null) {
+			throw new BadRequestHeaderException('Cannot logout if user is provided by API token');
+		}
+
+		parent::logout();
+	}
+
+	/**
+	 * Returns the user denoted by the token in the HTTP header.
+	 *
+	 * @return ?Authenticatable The user denoted by the HTTP header or `null`
+	 *                          if HTTP header is not set
+	 *
+	 * @throws BadRequestHeaderException thrown if the HTTP header with a
+	 *                                   token is set, but no corresponding
+	 *                                   user can be found
+	 */
+	protected function getUserByToken(): ?Authenticatable
+	{
+		$token = $this->request->headers->get(self::HTTP_TOKEN_HEADER);
+
+		return is_string($token) && $token !== '' ? $this->provider->retrieveByCredentials([
+			self::TOKEN_COLUMN_NAME => hash(self::TOKEN_HASH_METHOD, $token),
+		]) ?? throw new BadRequestHeaderException('Invalid token') : null;
+	}
+}
