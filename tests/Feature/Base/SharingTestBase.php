292068a9dfbcc552bd19d897102d3a481362ff47
ildyria <beviguier@gmail.com>
2022-09-17 21:41:28 +0200

    remove public check - add later

--- tests/Feature/Base/SharingTestBase.php
+++ tests/Feature/Base/SharingTestBase.php
@@ -235,7 +235,7 @@ class SharingTestBase extends PhotoTestBase
 		return [
 			'thumb' => $this->generateExpectedThumbJson($thumbID),
 			'photos' => $expectedPhotos,
-			'policies' => ['is_public' => $isPublic], // TODO: Verified if always needed in test.
+			// 'policies' => ['is_public' => $isPublic], // TODO: Verified if always needed in test.
 		];
 	}
 }
