b758e39246f9fdf300c5940cc8a60155c654c8d3
ildyria <beviguier@gmail.com>
2022-09-25 16:19:47 +0200

    add comment

--- app/DTO/ArrayableDTO.php
+++ app/DTO/ArrayableDTO.php
@@ -5,6 +5,11 @@ namespace App\DTO;
 use ReflectionClass;
 use ReflectionProperty;
 
+/**
+ * In some cases, when a DTO does not need to apply casts on attributes
+ * we can directly make use of a reflection which returns an array containing
+ * all the PUBLIC attributes of the DTO.
+ */
 class ArrayableDTO extends DTO
 {
 	/**
