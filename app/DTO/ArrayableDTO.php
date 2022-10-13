f4702d7b71e9b1b826a4fde78d8f2b8859f3e7e6
Benoît Viguier <ildyria@users.noreply.github.com>
2022-10-13 12:04:56 +0200

    Arrayable DTO using reflection (#1529)

---
+++ app/DTO/ArrayableDTO.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DTO;
+
+use App\Exceptions\Internal\LycheeLogicException;
+use Illuminate\Contracts\Support\Arrayable;
+use ReflectionClass;
+use ReflectionProperty;
+use function Safe\sprintf;
+
+/**
+ * In some cases, when a DTO does not need to apply casts on attributes
+ * we can directly make use of a reflection which returns an array containing
+ * all the PUBLIC attributes of the DTO.
+ */
+class ArrayableDTO extends DTO
+{
+	/**
+	 * By default, we return an array containing the PUBLIC attributes of the DTO.
+	 *
+	 * @return array the serialized properties of this object
+	 */
+	public function toArray(): array
+	{
+		$result = [];
+		$cls = new ReflectionClass($this);
+		$props = $cls->getProperties(ReflectionProperty::IS_PUBLIC);
+		foreach ($props as $prop) {
+			$value = $prop->getValue($this);
+			if (is_object($value)) {
+				if ($value instanceof Arrayable) {
+					$value = $value->toArray();
+				} else {
+					throw new LycheeLogicException(sprintf('Unable to convert %s into an array', get_class($value)));
+				}
+			}
+			$result[$prop->getName()] = $value;
+		}
+
+		return $result;
+	}
+}
--- app/DTO/Shares.php
+++ app/DTO/Shares.php
@@ -7,25 +7,19 @@ use Illuminate\Support\Collection;
 /**
  * Data Transfer Object (DTO) to transmit the list of shares to the client.
  */
-class Shares extends DTO
+class Shares extends ArrayableDTO
 {
-	public Collection $shared;
-	public Collection $albums;
-	public Collection $users;
-
-	public function __construct(Collection $shared, Collection $albums, Collection $users)
+	/**
+	 * @param Collection $shared List of shared albums
+	 * @param Collection $albums List of albums
+	 * @param Collection $users  List of users
+	 *
+	 * @return void
+	 */
+	public function __construct(
+		public Collection $shared,
+		public Collection $albums,
+		public Collection $users)
 	{
-		$this->shared = $shared;
-		$this->albums = $albums;
-		$this->users = $users;
-	}
-
-	public function toArray(): array
-	{
-		return [
-			'shared' => $this->shared->toArray(),
-			'albums' => $this->albums->toArray(),
-			'users' => $this->users->toArray(),
-		];
 	}
 }
