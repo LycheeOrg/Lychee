1b1067d1a2bf71c9ed955467bd7f2a3477971b6a
ildyria <beviguier@gmail.com>
2022-11-06 14:39:14 +0100

    add documentation on the CatchFailures trait in the tests

--- tests/Feature/Traits/CatchFailures.php
+++ tests/Feature/Traits/CatchFailures.php
@@ -4,6 +4,10 @@ namespace Tests\Feature\Traits;
 
 use Illuminate\Testing\TestResponse;
 
+/**
+ * This trait allows to retrieve the message returned by the back-end in case of unexpected results.
+ * This provides more readable results than: "status code 500 does match expected status code 200".
+ */
 trait CatchFailures
 {
 	protected function assertStatus(TestResponse $response, int $expectedStatusCode): void
@@ -16,6 +20,21 @@ trait CatchFailures
 		$response->assertStatus($expectedStatusCode);
 	}
 
+	/**
+	 * An exception is an array of the shape:
+	 * array{message:string, exception:string, file:string, line:int, trace:array{}, previous_exception: obj }
+	 * Unfortunately the trace contains the full call stack and dumping it completely does not add significant
+	 * information. Most of the time only the first 3 values of the trace are of interest.
+	 *
+	 * For this reason this function only keeps the first 3 values of the trace of the exception returned.
+	 *
+	 * Additionally, this transformation is applied recursively on the previous_exception in the case of
+	 * exception encapsulation.
+	 *
+	 * @param array $exception
+	 *
+	 * @return void
+	 */
 	private function trimException(array &$exception): void
 	{
 		$exception['trace'] = array_slice($exception['trace'], 0, 3);
