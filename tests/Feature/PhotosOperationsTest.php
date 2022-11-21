742008d34d1515617f33be549c217800e3b4ba59
ildyria <beviguier@gmail.com>
2022-11-21 21:27:31 +0100

    see if hardcoded works

--- tests/Feature/PhotosOperationsTest.php
+++ tests/Feature/PhotosOperationsTest.php
@@ -131,6 +131,7 @@ class PhotosOperationsTest extends PhotoTestBase
 			25,
 			'+02:00'
 		);
+
 		$updated_taken_at = $taken_at->addYear();
 
 		$this->photos_tests->set_upload_date($id, $updated_taken_at->toIso8601String());
@@ -190,8 +191,8 @@ class PhotosOperationsTest extends PhotoTestBase
 			'shutter' => '30 s',
 			'is_starred' => true,
 			'tags' => [],
-			'taken_at' => $updated_taken_at->setTimezone('UTC')->format('Y-m-d\TH:i:s.uP'),
-			'taken_at_orig_tz' => $updated_taken_at->getTimezone()->getName(),
+			'taken_at' => '2019-06-01T01:28:25.000000+02:00',
+			'taken_at_orig_tz' => '+02:00',
 			'title' => "Night in Ploumanac'h",
 			'type' => TestCase::MIME_TYPE_IMG_JPEG,
 			'size_variants' => [
