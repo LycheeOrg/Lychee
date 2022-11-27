5e43d1f2a40aa8ca7046cfa4c2aa071c24c3bf9e
qwerty287 <ndev@web.de>
2022-11-27 08:30:21 +0100

    Prefer direct table

--- database/migrations/2022_11_26_183251_increment_user_i_ds.php
+++ database/migrations/2022_11_26_183251_increment_user_i_ds.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Models\BaseAlbumImpl;
-use App\Models\Photo;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
@@ -15,23 +12,23 @@ class IncrementUserIDs extends Migration
 	 */
 	public function up(): void
 	{
-		/** @var User $user */
-		$user = User::find(0);
+		/** @var App\Models\User $user */
+		$user = DB::table('users')::find(0);
 		if ($user !== null && ($user->username === '' || $user->password === '')) {
 			// The admin user (id 0) has never set a username and password, so we remove it.
 			// This should only happen on a completely new installation where the admin user is created by the
 			// MigrateAdminUser migration and the user has never logged in.
 			$user->delete();
 		}
-		foreach (User::query()->orderByDesc('id')->get() as $user) {
+		foreach (DB::table('users')->orderByDesc('id')->get() as $user) {
 			$oldID = $user->id;
 			$newID = $oldID + 1;
 			$user->id = $newID;
 			$user->incrementing = false;
 			$user->save();
 			// update other columns referencing user ID
-			BaseAlbumImpl::query()->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
-			Photo::query()->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
+			DB::table('base_albums')->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
+			DB::table('photos')->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
 			DB::table('user_base_album')->where('user_id', '=', $oldID)->update(['user_id' => $newID]);
 			DB::table('webauthn_credentials')->where('authenticatable_id', '=', $oldID)->update(['authenticatable_id' => $newID]);
 			DB::table('users')->where('id', '=', $oldID)->delete();
@@ -45,19 +42,19 @@ class IncrementUserIDs extends Migration
 	 */
 	public function down(): void
 	{
-		/** @var User $user */
-		foreach (User::all() as $user) {
+		/** @var App\Models\User $user */
+		foreach (DB::table('users')->get() as $user) {
 			$oldID = $user->id;
 			$newID = $oldID - 1;
 			$user->id = $newID;
 			$user->incrementing = false;
 			$user->save();
 			// update other columns referencing user ID
-			BaseAlbumImpl::query()->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
-			Photo::query()->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
+			DB::table('base_albums')->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
+			DB::table('photos')->where('owner_id', '=', $oldID)->update(['owner_id' => $newID]);
 			DB::table('user_base_album')->where('user_id', '=', $oldID)->update(['user_id' => $newID]);
 			DB::table('webauthn_credentials')->where('authenticatable_id', '=', $oldID)->update(['authenticatable_id' => $newID]);
-			User::query()->where('id', '=', $oldID)->delete();
+			DB::table('users')->where('id', '=', $oldID)->delete();
 		}
 	}
 }
