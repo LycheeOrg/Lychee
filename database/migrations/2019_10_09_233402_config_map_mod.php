14e826dd84c3fa26828c178a122ecb033a4708ce
ildyria <beviguier@gmail.com>
2022-12-09 18:20:45 +0100

    forgotten those

--- database/migrations/2019_10_09_233402_config_map_mod.php
+++ database/migrations/2019_10_09_233402_config_map_mod.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class ConfigMapMod extends Migration
 {
--- database/migrations/2020_12_18_162100_bump_version040009.php
+++ database/migrations/2020_12_18_162100_bump_version040009.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class BumpVersion040009 extends Migration
 {
