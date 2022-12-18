6fd5f745fc3e086b9e4112be30b065221ee945a8
qwerty287 <ndev@web.de>
2022-12-18 11:18:21 +0100

    fix cast

--- tests/Boot.php
+++ tests/Boot.php
@@ -34,7 +34,7 @@ class Boot implements BeforeFirstTestHook
 				// Thus, we need to reset the index to the greatest ID + 1
 				/** @var User $lastUser */
 				$lastUser = User::query()->orderByDesc('id')->first();
-				DB::statement('ALTER SEQUENCE users_id_seq1 RESTART WITH ' . $lastUser->id + 1);
+				DB::statement('ALTER SEQUENCE users_id_seq1 RESTART WITH ' . strval($lastUser->id + 1));
 			}
 		} elseif (!$admin->may_administrate) {
 			$admin->may_administrate = true;
