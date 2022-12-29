8af2f353d820fb6a16be0a83cc125f421957626e
qwerty287 <ndev@web.de>
2022-12-29 10:01:07 +0100

    fix installer script

--- scripts/install_files.php
+++ scripts/install_files.php
@@ -11,10 +11,10 @@ $YELLOW = "\033[38;5;011m";
 $ORANGE = "\033[38;5;214m";
 
 echo "\n{$YELLOW}creating file for CSS personalization$NO_COLOR" . PHP_EOL;
-touch('storage/app/public/user.css');
+touch('public/dist/user.css');
 
 echo "\n{$YELLOW}creating default SQLite database$NO_COLOR" . PHP_EOL;
-touch('storage/app/database.sqlite');
+touch('database/database.sqlite');
 
 if (is_dir('.git')) {
 	echo "\n{$YELLOW}setting up hooks for git pull and git commits$NO_COLOR" . PHP_EOL;
