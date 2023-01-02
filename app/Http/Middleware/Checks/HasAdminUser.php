3f74ea88c097585030a8d8088a4c114559bbd89b
Benoît Viguier <ildyria@users.noreply.github.com>
2023-01-02 17:32:21 +0100

    Apply suggestions from code review
    Co-authored-by: Martin Stone <1611702+d7415@users.noreply.github.com>

--- app/Http/Middleware/Checks/HasAdminUser.php
+++ app/Http/Middleware/Checks/HasAdminUser.php
@@ -19,8 +19,8 @@ class HasAdminUser implements MiddlewareCheck
 	public function assert(): bool
 	{
 		// This is necessary in the case that someone is using the web-updater.
-		// In such case the tables already exists so the IsInstaled will return true.
-		// However this middleware will throw an error because may_administrate does not exists yet.
+		// In such case the tables already exist so the IsInstalled will return true.
+		// However this middleware will throw an error because may_administrate does not exist yet.
 		if (Schema::hasColumn('users', 'may_administrate')) {
 			try {
 				return User::query()->where('may_administrate', '=', true)->count() > 0;
@@ -28,7 +28,7 @@ class HasAdminUser implements MiddlewareCheck
 				throw new FrameworkException('Laravel\'s container component', $e);
 			}
 		} else {
-			// If the column does not exists yet but we are executing this scripts
+			// If the column does not exist yet but we are executing this script
 			// it means that there exists already an admin user (with ID = 0).
 			return true;
 		}
