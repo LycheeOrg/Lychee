b095d2324b3ead75d47e0ff6435d66e6d57c04e0
ildyria <beviguier@gmail.com>
2023-01-02 17:14:16 +0100

    improve HasAdminUser

--- app/Http/Middleware/Checks/HasAdminUser.php
+++ app/Http/Middleware/Checks/HasAdminUser.php
@@ -7,8 +7,7 @@ use App\Contracts\Http\MiddlewareCheck;
 use App\Exceptions\Internal\FrameworkException;
 use App\Models\User;
 use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Database\QueryException;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Schema;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 
@@ -19,20 +18,15 @@ class HasAdminUser implements MiddlewareCheck
 	 */
 	public function assert(): bool
 	{
-		try {
-			return User::query()->where('may_administrate', '=', true)->count() > 0;
-		} catch (QueryException $e) {
-			if (
-				Str::contains($e->getMessage(), 'column "may_administrate" does not exist') // PGSql
-				|| (Str::contains($e->getMessage(), 'SQLSTATE[42S22]') &&
-					Str::contains($e->getMessage(), "Unknown column 'may_administrate'")) // MySQL
-			) {
-				// We are doing an upgrade, therefore we can assume there exists an admin.
-				return true;
+		// This is necessary in the case that someone is using the web-updater.
+		if (Schema::hasColumn('users', 'may_administrate')) {
+			try {
+				return User::query()->where('may_administrate', '=', true)->count() > 0;
+			} catch (BindingResolutionException|NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+				throw new FrameworkException('Laravel\'s container component', $e);
 			}
-			throw $e;
-		} catch (BindingResolutionException|NotFoundExceptionInterface|ContainerExceptionInterface $e) {
-			throw new FrameworkException('Laravel\'s container component', $e);
+		} else {
+			return true;
 		}
 	}
 }
