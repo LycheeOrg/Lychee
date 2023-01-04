2cc842f1e43f2fc024122002af056b24864e9da2
Benoît Viguier <ildyria@users.noreply.github.com>
2023-01-04 16:20:17 +0100

    More robust fix on HasAdminUser (#1684)

--- app/Http/Middleware/Checks/HasAdminUser.php
+++ app/Http/Middleware/Checks/HasAdminUser.php
@@ -7,8 +7,7 @@ use App\Contracts\Http\MiddlewareCheck;
 use App\Exceptions\Internal\FrameworkException;
 use App\Models\User;
 use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Database\QueryException;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Schema;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 
@@ -19,16 +18,19 @@ class HasAdminUser implements MiddlewareCheck
 	 */
 	public function assert(): bool
 	{
-		try {
-			return User::query()->where('may_administrate', '=', true)->count() > 0;
-		} catch (QueryException $e) {
-			if (Str::contains($e->getMessage(), 'column "may_administrate" does not exist')) {
-				// We are doing an upgrade, therefore we can assume there exists an admin.
-				return true;
+		// This is necessary in the case that someone is using the web-updater.
+		// In such case the tables already exist so the IsInstalled will return true.
+		// However this middleware will throw an error because may_administrate does not exist yet.
+		if (Schema::hasColumn('users', 'may_administrate')) {
+			try {
+				return User::query()->where('may_administrate', '=', true)->count() > 0;
+			} catch (BindingResolutionException|NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+				throw new FrameworkException('Laravel\'s container component', $e);
 			}
-			throw $e;
-		} catch (BindingResolutionException|NotFoundExceptionInterface|ContainerExceptionInterface $e) {
-			throw new FrameworkException('Laravel\'s container component', $e);
+		} else {
+			// If the column does not exist yet but we are executing this script
+			// it means that there exists already an admin user (with ID = 0).
+			return true;
 		}
 	}
 }
