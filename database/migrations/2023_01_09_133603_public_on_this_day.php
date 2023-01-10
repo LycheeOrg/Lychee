aec1e1cc44208e62f8a143eb03b15c47ee51e5f7
Đorđe Aleksić <aldjordje@gmail.com>
2023-01-10 12:11:39 +0100

    Setting to make On This Day smart album public

---
+++ database/migrations/2023_01_09_133603_public_on_this_day.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class() extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(): void
+	{
+		DB::table('configs')->insert([
+			[
+				'key' => 'public_on_this_day',
+				'value' => '0',
+				'cat' => 'Smart Albums',
+				'type_range' => '0|1',
+				'confidentiality' => '0',
+				'description' => 'Make On This Day smart album accessible to anonymous users',
+			],
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(): void
+	{
+		DB::table('configs')->where('key', '=', 'public_on_this_day')->delete();
+	}
+};
