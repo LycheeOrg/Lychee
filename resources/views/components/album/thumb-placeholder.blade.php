1dd61c1cf65c6529dfb6d584e21710ee73232db8
Benoît Viguier <ildyria@users.noreply.github.com>
2023-01-16 18:02:07 +0100

    rm -fr livewire (#1712)
    * rm -fr livewire
    * dusting

--- resources/views/components/album/thumb-placeholder.blade.php
+++
@@ -1,8 +0,0 @@
-<span class="thumbimg">
-	<img class='lazyload'
-	src='{{ URL::asset('img/placeholder.png') }}'
-	data-src='img/no_images.svg'
-	alt='Photo thumbnail'
-	data-overlay='false'
-	draggable='false'>
-</span>
