db1a21aa3064f99cdfd0003bb2912bdde8bb191b
ildyria <beviguier@gmail.com>
2023-02-02 20:34:27 +0100

    fix security hotspots

--- resources/views/livewire/components/about.blade.php
+++ resources/views/livewire/components/about.blade.php
@@ -5,11 +5,17 @@
 	</h1>
 	@if($is_new_release_available)
 		<span class="update-status up-to-date-release">
-			– <a target="_blank" href="https://github.com/LycheeOrg/Lychee/releases" data-tabindex="-1">{{ __('lychee.UPDATE_AVAILABLE') }}</a>
+			– <a target="_blank"
+					rel="noopener"
+					href="https://github.com/LycheeOrg/Lychee/releases"
+					data-tabindex="-1">{{ __('lychee.UPDATE_AVAILABLE') }}</a>
 		</span>
 	@elseif($is_git_update_available)
 		<span class="update-status up-to-date-git">
-			– <a target="_blank" href="https://github.com/LycheeOrg/Lychee" data-tabindex="-1">{{ __('lychee.UPDATE_AVAILABLE') }}</a>
+			– <a target="_blank"
+					rel="noopener"
+					href="https://github.com/LycheeOrg/Lychee"
+					data-tabindex="-1">{{ __('lychee.UPDATE_AVAILABLE') }}</a>
 		</span>
 	@endif
 	<h2>{{__("lychee.ABOUT_SUBTITLE") }}</h2>
