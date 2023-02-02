902be063dd134e31ae14bd6885e13204e70a84d6
ildyria <beviguier@gmail.com>
2023-02-02 17:18:37 +0100

    fix redirection if landing is disabled

--- app/Http/Livewire/Pages/Landing.php
+++ app/Http/Livewire/Pages/Landing.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Livewire\Pages;
 
-use App\Enum\Livewire\PageMode;
 use App\Models\Configs;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class Landing extends Component
 {
-	public PageMode $mode = PageMode::LANDING;
-
 	public string $title;
 	public string $subtitle;
 	public string $background;
@@ -20,10 +17,6 @@ class Landing extends Component
 	 */
 	public function mount(): void
 	{
-		if (!Configs::getValueAsBool('landing_page_enable')) {
-			// TODO: redirect to route('livewire_index', ['page' => PageMode::GALLERY->value]);
-		}
-
 		$this->title = Configs::getValueAsString('landing_title');
 		$this->subtitle = Configs::getValueAsString('landing_subtitle');
 		$this->background = Configs::getValueAsString('landing_background');
