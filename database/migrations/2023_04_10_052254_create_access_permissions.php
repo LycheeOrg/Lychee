b47c74bf833453a70f0f2a1f237bf70dabbc5865
ildyria <beviguier@gmail.com>
2023-04-10 08:30:38 +0200

    create migration to access_permissions

---
+++ database/migrations/2023_04_10_052254_create_access_permissions.php
@@ -0,0 +1,129 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+	private const OWNER_ID = 'owner_id';
+	private const USER_ID = 'user_id';
+	private const BASE_ALBUM_ID = 'base_album_id';
+	private const IS_LINK_REQUIRED = 'is_link_required';
+	private const PASSWORD = 'password';
+	private const GRANTS_FULL_PHOTO_ACCESS = 'grants_full_photo_access';
+	private const GRANTS_DONWLOAD = 'grants_download';
+	private const GRANTS_UPLOAD = 'grants_upload';
+	private const GRANTS_EDIT = 'grants_edit';
+	private const GRANTS_DELETE = 'grants_delete';
+
+	private const CREATED_AT_COL_NAME = 'created_at';
+	private const UPDATED_AT_COL_NAME = 'updated_at';
+	private const DATETIME_PRECISION = 0;
+	public const RANDOM_ID_LENGTH = 24;
+
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		$this->createAccesPersmissionTable();
+		DB::transaction(fn () => $this->populateAccessPermissionTable());
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('access_permissions');
+	}
+
+	/**
+	 * Generate the table for access_permissions.
+	 */
+	private function createAccesPersmissionTable(): void
+	{
+		// Any old data is not relevant.
+		Schema::dropIfExists('access_permissions');
+
+		Schema::create('access_permissions', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			// Owner of the access capabilities
+			// (technically we could infer that from the album)
+			$table->unsignedInteger(self::OWNER_ID)->nullable(false)->default(0);
+
+			// User associated with the access capabilities
+			// If null we consider the album public
+			$table->unsignedInteger('user_id')->nullable()->default(null);
+
+			// parentId = album ID
+			$table->char(self::BASE_ALBUM_ID, self::RANDOM_ID_LENGTH)->nullable(true);
+
+			// basic access rights for anonymous users.
+			$table->boolean(self::IS_LINK_REQUIRED)->nullable(false)->default(false);
+			$table->string(self::PASSWORD, 100)->nullable()->default(null);
+
+			// Grants capabilities
+			$table->boolean(self::GRANTS_FULL_PHOTO_ACCESS)->nullable(false)->default(false);
+			$table->boolean(self::GRANTS_DONWLOAD)->nullable(false)->default(false);
+			$table->boolean(self::GRANTS_UPLOAD)->nullable(false)->default(false);
+			$table->boolean(self::GRANTS_EDIT)->nullable(false)->default(false);
+			$table->boolean(self::GRANTS_DELETE)->nullable(false)->default(false);
+
+			$table->dateTime(self::CREATED_AT_COL_NAME, self::DATETIME_PRECISION)->nullable();
+			$table->dateTime(self::UPDATED_AT_COL_NAME, self::DATETIME_PRECISION)->nullable();
+
+			$table->index([self::OWNER_ID]); // for albums which are own by the currently authenticated user
+			$table->index([self::BASE_ALBUM_ID]); // for albums which are own by the currently authenticated user
+		});
+	}
+
+	private function populateAccessPermissionTable(): void
+	{
+		$baseAlbums = DB::table('base_albums')->get();
+		foreach ($baseAlbums as $baseAlbum) {
+			if ($baseAlbum->is_public) {
+				DB::table('access_permissions')->
+				insert([
+					self::OWNER_ID => $baseAlbum->owner_id,
+					self::USER_ID => null,
+					self::BASE_ALBUM_ID => $baseAlbum->id,
+					self::IS_LINK_REQUIRED => $baseAlbum->is_link_required,
+					self::PASSWORD => $baseAlbum->password,
+					self::GRANTS_FULL_PHOTO_ACCESS => $baseAlbum->grants_full_photo_access,
+					self::GRANTS_DONWLOAD => $baseAlbum->grants_download,
+					self::GRANTS_UPLOAD => false,
+					self::GRANTS_EDIT => false,
+					self::GRANTS_DELETE => false,
+				]);
+			}
+		}
+
+		// Loop over every base album
+		$currentShares = DB::table('user_base_album')->join('base_albums', 'base_album_id', '=', 'user_base_album.id')->select([
+			'base_album_id',
+			self::USER_ID,
+			self::OWNER_ID,
+			self::GRANTS_DONWLOAD,
+			self::GRANTS_FULL_PHOTO_ACCESS,
+		])
+		->get();
+
+		foreach ($currentShares as $share) {
+			DB::table('access_permissions')->
+			insert([
+				self::OWNER_ID => $share->owner_id,
+				self::USER_ID => $share->user_id,
+				self::BASE_ALBUM_ID => $share->base_album_id,
+				self::IS_LINK_REQUIRED => false,
+				self::PASSWORD => null,
+				self::GRANTS_FULL_PHOTO_ACCESS => $share->grants_full_photo_access,
+				self::GRANTS_DONWLOAD => $share->grants_download,
+				self::GRANTS_UPLOAD => false,
+				self::GRANTS_EDIT => false,
+				self::GRANTS_DELETE => false,
+			]);
+		}
+	}
+};
