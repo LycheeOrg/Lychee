066011bb7a2811b84ce909e885f2c21c78c56ca6
ildyria <beviguier@gmail.com>
2023-04-15 18:53:09 +0200

    stub

---
+++ database/migrations/2023_04_15_165249_create_access_permissions_for_smart_albums.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('access_permissions_for_smart_albums', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('access_permissions_for_smart_albums');
+	}
+};
