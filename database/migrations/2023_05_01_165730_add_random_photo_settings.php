09990c3b8a59ca721b2e29a0669be8142f1ee1a5
mingan666 <11653241+mingan666@users.noreply.github.com>
2023-05-04 07:54:35 +0200

    [Enhancement] Make it possible to fetch random image without needing to be public & starred (#1819)

---
+++ database/migrations/2023_05_01_165730_add_random_photo_settings.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class() extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		DB::table('configs')->insert([
+			'key' => 'random_album_id',
+			'value' => 'starred',
+			'cat' => 'Mod Frame',
+			'type_range' => 'string',
+			'confidentiality' => '0',
+			'description' => 'Album id to be used by for random function.',
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		DB::table('configs')->where('key', '=', 'random_album_id')->delete();
+	}
+};
