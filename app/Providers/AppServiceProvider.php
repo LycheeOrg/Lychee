d92cd6dd7f3d79d91f32abf9e32f7960384fc257
ildyria <beviguier@gmail.com>
2023-05-17 00:11:34 +0200

    fix secure headers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -114,6 +114,11 @@ class AppServiceProvider extends ServiceProvider
 		 * Set up the Authorization layer for accessing Logs in LogViewer.
 		 */
 		LogViewer::auth(function ($request) {
+
+			// We must disable unsafe-eval because vue3 used by log-viewer requires it.
+			// We only do that in that specific case. It is disabled by default otherwise.
+			config(['secure-headers.csp.script-src.unsafe-eval' => true]);
+
 			// return true to allow viewing the Log Viewer.
 			return Auth::authenticate() !== null && Gate::check(SettingsPolicy::CAN_SEE_LOGS, Configs::class);
 		});
