e87e426f178361d4d4553eb2ae96d494972896b6
Benoît Viguier <ildyria@users.noreply.github.com>
2023-05-19 22:48:35 +0200

    Fix version 4.9 log-viewer paths hard coding (#1857)

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -45,7 +45,7 @@ return [
 	|
 	*/
 
-	'back_to_system_url' => config('app.url', null),
+	'back_to_system_url' => '../', // config('app.url', null),
 
 	'back_to_system_label' => null, // Displayed by default: "Back to {{ app.name }}"
 
