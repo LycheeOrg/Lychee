839b9fa98ece7e496b44727b7571026d2af35ebb
wladif <wladif@users.noreply.github>
2023-05-26 14:57:58 +0300

    Fix phpstan

--- app/Actions/Import/FromUrl.php
+++ app/Actions/Import/FromUrl.php
@@ -55,7 +55,7 @@ class FromUrl
 				$path = parse_url($url, PHP_URL_PATH);
 				$extension = '.' . pathinfo($path, PATHINFO_EXTENSION);
 
-				if ($extension != '.') {
+				if ($extension !== '.') {
 					// Validate photo extension even when `$create->add()` will do later.
 					// This prevents us from downloading unsupported files.
 					BaseMediaFile::assertIsSupportedOrAcceptedFileExtension($extension);
