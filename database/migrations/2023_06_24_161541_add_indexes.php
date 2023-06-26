1d109b77df212a1332089a455333e0db33532da0
ildyria <beviguier@gmail.com>
2023-06-26 19:04:41 +0200

    remove commented code

--- database/migrations/2023_06_24_161541_add_indexes.php
+++ database/migrations/2023_06_24_161541_add_indexes.php
@@ -17,11 +17,6 @@ return new class() extends Migration {
 
 	// Attributes name
 	public const IS_LINK_REQUIRED = 'is_link_required';
-	// public const GRANTS_FULL_PHOTO_ACCESS = 'grants_full_photo_access';
-	// public const GRANTS_DOWNLOAD = 'grants_download';
-	// public const GRANTS_UPLOAD = 'grants_upload';
-	// public const GRANTS_EDIT = 'grants_edit';
-	// public const GRANTS_DELETE = 'grants_delete';
 	public const PASSWORD = 'password';
 
 	private AbstractSchemaManager $schemaManager;
