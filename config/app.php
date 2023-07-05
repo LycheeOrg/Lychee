b10c8d9610e95adb93263f294891e0771421bc79
ildyria <beviguier@gmail.com>
2023-07-05 15:01:58 +0200

    remove already existing config

--- config/app.php
+++ config/app.php
@@ -70,8 +70,6 @@ return [
 
 	'asset_url' => null,
 
-	'force_https' => (bool) env('APP_FORCE_HTTPS', false),
-
 	/*
 	|--------------------------------------------------------------------------
 	| Application URL
