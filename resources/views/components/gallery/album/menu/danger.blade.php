f5dd9c215eb09d12001ff9cf96227baae21d3589
ildyria <beviguier@gmail.com>
2023-07-12 01:02:47 +0200

    details

--- resources/views/components/gallery/album/menu/danger.blade.php
+++ resources/views/components/gallery/album/menu/danger.blade.php
@@ -1,6 +1,6 @@
 @props(['class' => 'w-full', 'mode', 'current'])
 <li @class([
-    "my-2 px-2 pt-3 pb-4 cursor-pointer font-bold text-center transition-colors ease-in-out select-none
+    "px-2 pt-3 pb-4 cursor-pointer font-bold text-center transition-colors ease-in-out select-none
     xl:border-l-2 max-xl:border-b-2 border-solid border-red-800
     hover:bg-red-800 hover:text-white {{ $class }}",
     'text-red-800' => $mode !== $current,
