bcc4b542195978b0152b6c0a8ad9b35793adac4d
ildyria <beviguier@gmail.com>
2023-07-16 16:11:09 +0200

    fixes some access rights

--- resources/views/livewire/pages/profile.blade.php
+++ resources/views/livewire/pages/profile.blade.php
@@ -7,14 +7,16 @@
 		<div class="settings_view max-w-xl text-neutral-400 text-sm mx-auto">
 			<livewire:forms.profile.set-login />
 			@if($are_notification_active)
-			<div class="setting_line">
+			<div>
 				<p>
 					{{ __('lychee.USER_EMAIL_INSTRUCTION') }}
 				</p>
 			</div>
 			<livewire:forms.profile.set-email />
 			@endif
+			@can(App\Policies\UserPolicy::CAN_USE_2FA, [App\Models\User::class, null])
 			<livewire:modules.profile.second-factor />
+			@endcan
 		</div>
 	</div>
 </div>
