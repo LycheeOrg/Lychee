c6e4d8821efb5ea9f6b25e7846d05146ae14f77b
ildyria <beviguier@gmail.com>
2023-08-21 23:23:29 +0200

    add error thrown if APP_URL does not match current url

---
+++ app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Actions\Diagnostics\Pipes\Checks;
+
+use App\Contracts\DiagnosticPipe;
+
+class AppUrlMatchCheck implements DiagnosticPipe
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function handle(array &$data, \Closure $next): array
+	{
+		if (config('app.url') !== request()->httpHost())
+		{
+			$data[] = 'Error: APP_URL does not match the current url. This will break U2F authentication. Please update APP_URL to reflect this change.';	
+		}
+
+		return $next($data);
+	}
+}
