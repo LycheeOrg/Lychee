4fc3c7f6a465a724a8a42a9375c79e2851e48119
ildyria <beviguier@gmail.com>
2023-08-21 23:27:12 +0200

    formatting + better check

--- app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
+++ app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
@@ -11,7 +11,8 @@ class AppUrlMatchCheck implements DiagnosticPipe
 	 */
 	public function handle(array &$data, \Closure $next): array
 	{
-		if (config('app.url') !== request()->httpHost()) {
+		if (config('app.url') !== request()->httpHost() &&
+			config('app.url') !== request()->schemeAndHttpHost()) {
 			$data[] = 'Error: APP_URL does not match the current url. This will break U2F authentication. Please update APP_URL to reflect this change.';
 		}
 
