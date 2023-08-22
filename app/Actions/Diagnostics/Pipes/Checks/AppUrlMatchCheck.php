1149192d0999371cc060200905e43296785af482
ildyria <beviguier@gmail.com>
2023-08-22 10:26:23 +0200

    WRTC

--- app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
+++ app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
@@ -15,7 +15,7 @@ class AppUrlMatchCheck implements DiagnosticPipe
 		// http:// is 7 characters.
 		if (strpos($config_url, '/', 8) !== false) {
 			$data[] = 'Warning: APP_URL contains a sub-path. This may impact your WebAuthn authentication.';
-		} else if ($config_url !== request()->httpHost() && $config_url !== request()->schemeAndHttpHost()) {
+		} elseif ($config_url !== request()->httpHost() && $config_url !== request()->schemeAndHttpHost()) {
 			$data[] = 'Error: APP_URL does not match the current url. This will break WebAuthn authentication. Please update APP_URL to reflect this change.';
 		}
 
