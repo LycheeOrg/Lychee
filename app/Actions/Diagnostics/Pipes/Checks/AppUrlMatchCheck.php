8320d168436cf830789994205b55b2e3aa7ce5e6
ildyria <beviguier@gmail.com>
2023-08-22 10:22:11 +0200

    fix warning

--- app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
+++ app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
@@ -11,9 +11,12 @@ class AppUrlMatchCheck implements DiagnosticPipe
 	 */
 	public function handle(array &$data, \Closure $next): array
 	{
-		if (config('app.url') !== request()->httpHost() &&
-			config('app.url') !== request()->schemeAndHttpHost()) {
-			$data[] = 'Error: APP_URL does not match the current url. This will break U2F authentication. Please update APP_URL to reflect this change.';
+		$config_url = config('app.url');
+		// http:// is 7 characters.
+		if (strpos($config_url, '/', 8) !== false) {
+			$data[] = 'Warning: APP_URL contains a sub-path. This may impact your WebAuthn authentication.';
+		} else if ($config_url !== request()->httpHost() && $config_url !== request()->schemeAndHttpHost()) {
+			$data[] = 'Error: APP_URL does not match the current url. This will break WebAuthn authentication. Please update APP_URL to reflect this change.';
 		}
 
 		return $next($data);
