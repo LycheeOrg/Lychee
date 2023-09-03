126724b061b170d8e14501c534c3239827cb98f4
ildyria <beviguier@gmail.com>
2023-09-03 17:29:02 +0200

    fix phpstan complaints

--- app/View/Components/Gallery/Photo/Download.php
+++ app/View/Components/Gallery/Photo/Download.php
@@ -28,29 +28,32 @@ class Download extends Component
 	{
 		$this->photoId = $photo->id;
 
-		$medium = $photo->size_variants?->getSizeVariant(SizeVariantType::MEDIUM);
-		$medium2x = $photo->size_variants?->getSizeVariant(SizeVariantType::MEDIUM2X);
-		$original = $photo->size_variants?->getSizeVariant(SizeVariantType::ORIGINAL);
-		$small = $photo->size_variants?->getSizeVariant(SizeVariantType::SMALL);
-		$small2x = $photo->size_variants?->getSizeVariant(SizeVariantType::SMALL2X);
-		$thumb = $photo->size_variants?->getSizeVariant(SizeVariantType::THUMB);
-		$thumb2x = $photo->size_variants?->getSizeVariant(SizeVariantType::THUMB2X);
+		$medium = $photo->size_variants->getSizeVariant(SizeVariantType::MEDIUM);
+		$medium2x = $photo->size_variants->getSizeVariant(SizeVariantType::MEDIUM2X);
+		$original = $photo->size_variants->getSizeVariant(SizeVariantType::ORIGINAL);
+		$small = $photo->size_variants->getSizeVariant(SizeVariantType::SMALL);
+		$small2x = $photo->size_variants->getSizeVariant(SizeVariantType::SMALL2X);
+		$thumb = $photo->size_variants->getSizeVariant(SizeVariantType::THUMB);
+		$thumb2x = $photo->size_variants->getSizeVariant(SizeVariantType::THUMB2X);
 
 		$downgrade = !Gate::check(PhotoPolicy::CAN_ACCESS_FULL_PHOTO, [Photo::class, $photo]) &&
 		!$photo->isVideo() &&
-		$photo->size_variants?->hasMedium() === true;
+		$photo->size_variants->hasMedium() === true;
+
+		$rq = request();
 
 		$this->size_variants = collect([
-			'original' => $original === null ? null : SizeVariantResource::make($original)->noUrl($downgrade)->toArray(null),
-			'medium2x' => $medium2x === null ? null : SizeVariantResource::make($medium2x)->toArray(null),
-			'medium' => $medium === null ? null : SizeVariantResource::make($medium)->toArray(null),
-			'small2x' => $small2x === null ? null : SizeVariantResource::make($small2x)->toArray(null),
-			'small' => $small === null ? null : SizeVariantResource::make($small)->toArray(null),
-			'thumb2x' => $thumb2x === null ? null : SizeVariantResource::make($thumb2x)->toArray(null),
-			'thumb' => $thumb === null ? null : SizeVariantResource::make($thumb)->toArray(null),
+			'original' => $original === null ? null : SizeVariantResource::make($original)->noUrl($downgrade)->toArray($rq),
+			'medium2x' => $medium2x === null ? null : SizeVariantResource::make($medium2x)->toArray($rq),
+			'medium' => $medium === null ? null : SizeVariantResource::make($medium)->toArray($rq),
+			'small2x' => $small2x === null ? null : SizeVariantResource::make($small2x)->toArray($rq),
+			'small' => $small === null ? null : SizeVariantResource::make($small)->toArray($rq),
+			'thumb2x' => $thumb2x === null ? null : SizeVariantResource::make($thumb2x)->toArray($rq),
+			'thumb' => $thumb === null ? null : SizeVariantResource::make($thumb)->toArray($rq),
 		])
 		->filter(fn ($e) => $e !== null)
 		->map(function ($v, $k) {
+			/** @var array{filesize:int} $v */
 			$v['filesize'] = Helpers::getSymbolByQuantity(intval($v['filesize']));
 
 			return $v;
