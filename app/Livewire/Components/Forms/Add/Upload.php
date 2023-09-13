6ce55f8a26eb4e65779703187b3a028e990e6f56
ildyria <beviguier@gmail.com>
2023-09-13 02:49:23 +0200

    continue working on upload

--- app/Livewire/Components/Forms/Add/Upload.php
+++ app/Livewire/Components/Forms/Add/Upload.php
@@ -5,8 +5,8 @@ namespace App\Livewire\Components\Forms\Add;
 use App\Enum\Livewire\FileStatus;
 use App\Exceptions\PhotoSkippedException;
 use App\Facades\Helpers;
+use App\Image\Files\NativeLocalFile;
 use App\Image\Files\ProcessableJobFile;
-use App\Image\Files\UploadedFile;
 use App\Jobs\ProcessImageJob;
 use App\Models\Configs;
 use Illuminate\Contracts\View\View;
@@ -14,16 +14,10 @@ use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 use Livewire\WithFileUploads;
-
-use function Safe\fclose;
-use function Safe\fopen;
-use function Safe\fread;
-use function Safe\fwrite;
 use function Safe\ini_get;
-use function Safe\unlink;
 
 /**
- * This defines the Login Form used in modals.
+ * This defines the Upload Form used in modals.
  */
 class Upload extends Component
 {
@@ -34,7 +28,7 @@ class Upload extends Component
 	 */
 	public ?string $albumId = null;
 
-	/** @var array<int,array{fileName:string,fileChunk:mixed,lastModified:int,progress:int,fileRef:TemporaryUploadedFile,uuid:string,fileSize:int,stage:FileStatus}> */
+	/** @var array<int,array{fileName:string,fileChunk:mixed,lastModified:int,progress:int,uuidName:string,extension:string,fileSize:int,stage:FileStatus}> */
 	public array $uploads = [];
 
 	public int $chunkSize;
@@ -78,31 +72,23 @@ class Upload extends Component
 			$fileDetails = $this->uploads[$index];
 
 			// Initialize data if not existing.
-			$fileDetails['uuid'] ??= strtr(base64_encode(random_bytes(12)), '+/', '-_');
+			$fileDetails['extension'] ??= '.' . pathinfo($fileDetails['fileName'], PATHINFO_EXTENSION);
+			$fileDetails['uuidName'] ??= strtr(base64_encode(random_bytes(12)), '+/', '-_') . $fileDetails['extension'];
 			$fileDetails['stage'] ??= FileStatus::UPLOADING;
 
 			// Refresh uploads
-			$this->uploads[$index]['uuid'] = $fileDetails['uuid'];
+			$this->uploads[$index]['extension'] = $fileDetails['extension'];
+			$this->uploads[$index]['uuidName'] = $fileDetails['uuidName'];
 			$this->uploads[$index]['stage'] = $fileDetails['stage'];
 
-			// $extension = '.' . pathinfo($fileDetails['fileName'], PATHINFO_EXTENSION);
-			$finalPath = Storage::path('/livewire-tmp/' . $fileDetails['fileName']);
+			/** @var TemporaryUploadedFile $chunkFile */
+			$chunkFile = $fileDetails['fileChunk'];
+			$final = new NativeLocalFile(Storage::path('/livewire-tmp/' . $fileDetails['uuidName']));
+			$final->append($chunkFile->readStream());
+			$chunkFile->delete();
 
-			// Chunk File
-			$chunkName = $fileDetails['fileChunk']->getFileName();
-			$chunkPath = Storage::path('/livewire-tmp/' . $chunkName);
-			$chunk = fopen($chunkPath, 'rb');
-			$buff = fread($chunk, $this->chunkSize);
-			fclose($chunk);
+			$curSize = $final->getFilesize();
 
-			// Merge Together
-			$final = fopen($finalPath, 'ab');
-			fwrite($final, $buff);
-			fclose($final);
-			unlink($chunkPath);
-
-			// Progress
-			$curSize = Storage::size('/livewire-tmp/' . $fileDetails['fileName']);
 			$this->uploads[$index]['progress'] = intval($curSize / $fileDetails['fileSize'] * 100);
 			if ($this->uploads[$index]['progress'] === 100) {
 				$this->uploads[$index]['fileRef'] =
@@ -119,27 +105,10 @@ class Upload extends Component
 	{
 		foreach ($this->uploads as $idx => $fileData) {
 			if ($fileData['stage'] === FileStatus::READY) {
-				// This code is a nasty work-around which should not exist.
-				// PHP stores a temporary copy of the uploaded file without a file
-				// extension.
-				// Unfortunately, most of our methods pass around absolute file paths
-				// instead of proper `File` object.
-				// During the process we have a lot of code which tries to
-				// re-determine the MIME type of the file based on the file path.
-				// This is not only inefficient, but the original MIME type (of the
-				// uploaded file) gets lost on the way.
-				// As a work-around we store the uploaded file with a file extension.
-				// Unfortunately, we cannot simply re-name the file, because this
-				// might break due to permission problems for certain installation
-				// if the temporarily uploaded file is stored in the system-global
-				// temporary directory below another mount point or another Docker
-				// image than the Lychee installation.
-				// Hence, we must make a deep copy.
-				// TODO: Remove this code again, if all other TODOs regarding MIME and file handling are properly refactored and we have stopped using absolute file paths as the least common denominator to pass around files.
-				$uploadedFile = new UploadedFile($fileData['fileRef']);
+				$uploadedFile = new NativeLocalFile(Storage::path('/livewire-tmp/' . $fileData['uuidName']));
 				$processableFile = new ProcessableJobFile(
-					$uploadedFile->getOriginalExtension(),
-					$uploadedFile->getOriginalBasename()
+					$fileData['extension'],
+					$fileData['fileName']
 				);
 				$processableFile->write($uploadedFile->read());
 				$uploadedFile->close();
@@ -151,11 +120,9 @@ class Upload extends Component
 					$this->uploads[$idx]['stage'] = FileStatus::PROCESSING;
 
 					if (Configs::getValueAsBool('use_job_queues')) {
-						// TODO: replace 0 correct time/date
-						ProcessImageJob::dispatch($processableFile, $this->albumId, $fileData['lastModified'] ?? 0);
+						ProcessImageJob::dispatch($processableFile, $this->albumId, $fileData['lastModified']);
 					} else {
-						// TODO: replace 0 correct time/date
-						ProcessImageJob::dispatchSync($processableFile, $this->albumId, $fileData['lastModified'] ?? 0);
+						ProcessImageJob::dispatchSync($processableFile, $this->albumId, $fileData['lastModified']);
 					}
 					$this->uploads[$idx]['stage'] = FileStatus::DONE;
 				} catch (PhotoSkippedException $e) {
--- resources/views/livewire/forms/photo/delete.blade.php
+++ resources/views/livewire/forms/photo/delete.blade.php
@@ -4,6 +4,7 @@
 	</div>
 	<div class="flex w-full box-border">
 		<x-forms.buttons.cancel class="border-t border-t-dark-800 rounded-bl-md w-full" wire:click="close">{{ __('lychee.PHOTO_KEEP' ) }}</x-forms.buttons.cancel>
-		<x-forms.buttons.danger class="border-t border-t-dark-800 rounded-br-md w-full" wire:click="submit">{{ __('lychee.PHOTO_DELETE') }}</x-forms.buttons.action>
+		<x-forms.buttons.danger class="border-t border-t-dark-800 rounded-br-md w-full"
+			@keydown.enter.window="$wire.submit()" wire:click="submit">{{ __('lychee.PHOTO_DELETE') }}</x-forms.buttons.action>
 	</div>
 </div>
