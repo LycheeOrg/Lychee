113ac00f255d347c7d2d909900249d89bc96b734
ildyria <beviguier@gmail.com>
2023-12-23 23:25:51 +0100

    fix 8.

--- resources/views/components/gallery/album/menu/menu.blade.php
+++ resources/views/components/gallery/album/menu/menu.blade.php
@@ -1,7 +1,7 @@
 @props(['album', 'userCount' => 0])
-<div class="w-full sm:m-7 mb-4 flex justify-center flex-wrap flex-row-reverse"
-    x-cloak x-show="albumFlags.isDetailsOpen" x-collapse.duration.300ms >
+<div class="w-full flex justify-center flex-wrap flex-row-reverse" x-cloak x-show="albumFlags.isDetailsOpen" x-collapse.duration.300ms >
     <ul class="
+        sm:mt-7 sm:px-7 mb-4
         text-neutral-200 text-sm w-full xl:w-1/6 xl:px-9
         max-xl:w-full max-xl:flex max-xl:justify-center
         ">
@@ -14,21 +14,21 @@
         @endif
         <x-gallery.album.menu.danger tab='3' >{{ "DANGER ZONE" }}</x-gallery.album.menu.danger>
     </ul>
-    <div class="w-full xl:w-5/6 flex justify-center flex-wrap" x-cloak x-show="albumFlags.activeTab === 0">
+    <div class="w-full xl:w-5/6 flex justify-center flex-wrap mb-4 sm:mt-7 pl-7" x-cloak x-show="albumFlags.activeTab === 0">
         <livewire:forms.album.properties :album="$album" />
         <livewire:forms.album.visibility :album="$album" />
     </div>
     @if ($userCount > 1 && ($album instanceof \App\Models\Album))
-    <div class="w-full xl:w-5/6 flex justify-center flex-wrap" x-cloak x-show="albumFlags.activeTab === 1">
+    <div class="w-full xl:w-5/6 flex justify-center flex-wrap mb-4 sm:mt-7 pl-7" x-cloak x-show="albumFlags.activeTab === 1">
         <livewire:forms.album.share-with :album="$album" />
     </div>
     @endif
     @if($album instanceof \App\Models\Album)
-    <div class="w-full xl:w-5/6 flex justify-center flex-wrap" x-cloak x-show="albumFlags.activeTab === 2">
+    <div class="w-full xl:w-5/6 flex justify-center flex-wrap mb-4 sm:mt-7 pl-7" x-cloak x-show="albumFlags.activeTab === 2">
         <livewire:forms.album.move-panel :album="$album" />
     </div>
     @endif
-    <div class="w-full xl:w-5/6 flex justify-center flex-wrap" x-cloak x-show="albumFlags.activeTab === 3">
+    <div class="w-full xl:w-5/6 flex justify-center flex-wrap mb-4 sm:mt-7 pl-7" x-cloak x-show="albumFlags.activeTab === 3">
             {{-- We only display this menu if there are more than 1 user, it does not make sense otherwise --}}
             @if ($userCount > 1)
             <livewire:forms.album.transfer :album="$album"  lazy />
