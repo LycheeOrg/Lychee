61050722ddcdfe944ee3b7a4a8a31e661af83b3f
ildyria <beviguier@gmail.com>
2023-12-23 21:09:16 +0100

    fix 1.

--- resources/views/components/gallery/view/photo.blade.php
+++ resources/views/components/gallery/view/photo.blade.php
@@ -93,7 +93,8 @@
 			<template x-if="photo.rights.can_edit">
 			<div x-cloak
 				class="absolute top-0 h-1/4 w-3/4 xl:w-1/2 left-1/2 -translate-x-1/2 opacity-10 group hover:opacity-100 transition-opacity duration-500 ease-in-out 
-					z-20">
+					z-20"
+					x-show="!photoFlags.isEditOpen">
 				<span class="absolute left-1/2 -translate-x-1/2 p-1 min-w-[25%] filter-shadow text-center">
 					<x-gallery.photo.button icon="star" class=''
 						x-bind:class="photo.is_starred ? 'fill-yellow-500 hover:fill-yellow-100' : 'fill-white hover:fill-yellow-500'"
