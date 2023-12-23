b72602453e28cf48ce860e98cc5846e4d7a8f618
ildyria <beviguier@gmail.com>
2023-12-23 17:05:30 +0100

    version 5.0.0

---
+++ database/migrations/2023_12_23_160356_bump_version050000.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class() extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		DB::table('configs')->where('key', 'version')->update(['value' => '050000']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		DB::table('configs')->where('key', 'version')->update(['value' => '041300']);
+	}
+};
