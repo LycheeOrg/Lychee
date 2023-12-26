a4cf55b934d1d37235a8fc6809bb45c1fb193704
Benoît Viguier <ildyria@users.noreply.github.com>
2023-12-26 19:38:36 +0100

    Better diagnostics for APP_URL and LYCHEE_UPLOAD_URL (#2105)

--- app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
+++ app/Actions/Diagnostics/Pipes/Checks/AppUrlMatchCheck.php
@@ -7,6 +7,11 @@ use App\Contracts\DiagnosticPipe;
 class AppUrlMatchCheck implements DiagnosticPipe
 {
 	/**
+	 * We check:
+	 * 1. that APP_URL correctly match the url of the request.
+	 * 2. That APP_URL correctly match the url of the request.
+	 * 3. That LYCHEE_UPLOADS_URL is not provided by null
+	 * 4. that if APP_URL is default, that the config is also using localhost. (Additional Error in the diagnostics).
 	 * {@inheritDoc}
 	 */
 	public function handle(array &$data, \Closure $next): array
@@ -15,8 +20,19 @@ class AppUrlMatchCheck implements DiagnosticPipe
 		// https:// is 8 characters.
 		if (strpos($config_url, '/', 8) !== false) {
 			$data[] = 'Warning: APP_URL contains a sub-path. This may impact your WebAuthn authentication.';
-		} elseif ($config_url !== request()->httpHost() && $config_url !== request()->schemeAndHttpHost()) {
-			$data[] = 'Error: APP_URL does not match the current url. This will break WebAuthn authentication. Please update APP_URL to reflect this change.';
+		}
+
+		if ($config_url !== request()->httpHost() && $config_url !== request()->schemeAndHttpHost()) {
+			$data[] = 'Error: APP_URL does not match the current url. This will break WebAuthn authentication.';
+		}
+
+		$config_url_imgage = config('filesystems.disks.images.url');
+		if ($config_url_imgage === '') {
+			$data[] = 'Error: LYCHEE_UPLOADS_URL is set and empty. This will prevent images to be displayed. Remove the line from your .env';
+		}
+
+		if (($config_url . '/uploads/') === $config_url_imgage && $config_url !== request()->schemeAndHttpHost() && $config_url !== request()->httpHost()) {
+			$data[] = 'Error: APP_URL does not match the current url. This will prevent images to be properly displayed.';
 		}
 
 		return $next($data);
