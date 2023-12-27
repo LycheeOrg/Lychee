736a6cbc53650d5dd47785079d3d4a801b5a1aed
Manuel Schmid <9307310+mashb1t@users.noreply.github.com>
2023-12-27 14:43:19 +0100

    Remove path from query string via middleware (#2112)
    Co-authored-by: ildyria <beviguier@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,7 @@ class Kernel extends HttpKernel
 			\App\Http\Middleware\VerifyCsrfToken::class,
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
 			\App\Http\Middleware\DisableCSP::class,
+			\App\Http\Middleware\QueryStringFixer::class,
 		],
 
 		'web-admin' => [
