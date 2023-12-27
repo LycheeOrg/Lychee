5308c82c79833270bdae272b7d8c82ea1c283809
ildyria <beviguier@gmail.com>
2023-12-27 15:03:56 +0100

    improved

--- .fly/scripts/caches.sh
+++ .fly/scripts/caches.sh
@@ -3,3 +3,6 @@
 /usr/bin/php /var/www/html/artisan config:cache --no-ansi -q
 /usr/bin/php /var/www/html/artisan route:cache --no-ansi -q
 /usr/bin/php /var/www/html/artisan view:cache --no-ansi -q
+/usr/bin/php /var/www/html/artisan migrate:fresh --force
+/usr/bin/php /var/www/html/artisan lychee:create_user admin admin --may-administrate
+/usr/bin/php /var/www/html/artisan lychee:create_user user password
--- fly-deploy.sh
+++
@@ -1,3 +0,0 @@
-php /var/www/html/artisan migrate --force
-php /var/www/html/artisan lychee:update_user admin admin || php /var/www/html/artisan lychee:create_user admin admin --may-administrate
-php /var/www/html/artisan lychee:create_user user password
