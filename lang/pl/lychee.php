cba3849efb6514c55b72d865a60d1961525506de
ildyria <beviguier@gmail.com>
2024-01-04 18:26:00 +0100

    Login => Log in

--- lang/pl/lychee.php
+++ lang/pl/lychee.php
@@ -315,7 +315,7 @@ return [
 	'U2F_AUTHENTIFICATION_SUCCESS' => 'Autoryzacja pomyślna!',
 	'U2F_CREDENTIALS' => 'Dane uwierzytelniające',
 	'U2F_CREDENTIALS_DELETED' => 'Usunięto dane uwierzytelniające!',
-	'U2F_LOGIN' => 'Login with WebAuthn',
+	'U2F_LOGIN' => 'Log in with WebAuthn',
 
 	'NEW_PHOTOS_NOTIFICATION' => 'Send new photos notification emails.',
 	'SETTINGS_SUCCESS_NEW_PHOTOS_NOTIFICATION' => 'New photos notification updated',
