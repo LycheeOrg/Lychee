6b7a9c3c1386a08fe25b6450a5bde1397204753b
ildyria <beviguier@gmail.com>
2024-01-07 22:20:45 +0100

    fix locale

--- lang/vi/aspect_ratio.php
+++ lang/vi/aspect_ratio.php
@@ -6,8 +6,8 @@ return [
 	| Aspect Ratio lines.
 	|--------------------------------------------------------------------------
 	*/
-	'5by4' => '5/4 (instagram portrait)',
-	'4by5' => '4/5 (instagram landscape)',
+	'5by4' => '5/4 (instagram landscape)',
+	'4by5' => '5/4 (instagram portrait)',
 	'2by3' => '2/3 (portrait)',
 	'3by2' => '3/2 (landscape)',
 	'1by1' => 'square',
