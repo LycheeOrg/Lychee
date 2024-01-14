d8abc7a66f9f3ecdc3a083efe38bf14da98794ec
ildyria <beviguier@gmail.com>
2024-01-14 15:46:50 +0100

    improve display [skip-ci]

--- resources/views/livewire/forms/profile/oauth.blade.php
+++ resources/views/livewire/forms/profile/oauth.blade.php
@@ -11,9 +11,9 @@
 		<h1 class="font-bold text-text-main-100 mb-2 border-b border-solid border-neutral-600 pb-3">{{ __('oauth.SET_UP_OAUTH') }}</h1>
 	</div>
 	@foreach ($this->oauthData as $oauthData) 
-		@if($oauthData->isEnabled)
-		<div class=" text-text-main-100">
-			<i @class(["align-middle w-4 text-create-600",
+		<div class=" text-text-main-100 {{ $oauthData->isEnabled ? '' : 'hover:text-primary-400'}}">
+			<i @class(["align-middle w-4",
+			"text-create-600" => $oauthData->isEnabled,
 			"fa-brands fa-apple" => $oauthData->providerType === 'apple',
 			"fa-brands fa-amazon" => $oauthData->providerType === 'amazon',
 			"fa-brands fa-facebook" => $oauthData->providerType === 'facebook',
@@ -23,27 +23,17 @@
 			"fa-brands fa-microsoft" => $oauthData->providerType === 'microsoft',
 			"fa-solid fa-cloud" => $oauthData->providerType === 'nextcloud',
 			])></i>
+			@if($oauthData->isEnabled)
 			<span class="ml-2">
-			{{ sprintf(__('oauth.TOKEN_REGISTERED'), ucfirst($oauthData->providerType)) }}<a wire:click='clear("{{ $oauthData->providerType }}")' class='ml-2 cursor-pointer italic text-text-main-400 hover:text-danger-800'>({{ __('oauth.RESET') }})</a>
+				{{ sprintf(__('oauth.TOKEN_REGISTERED'), ucfirst($oauthData->providerType)) }}<a wire:click='clear("{{ $oauthData->providerType }}")' class='ml-2 cursor-pointer italic text-text-main-400 hover:text-danger-800'>({{ __('oauth.RESET') }})</a>
 			</span>
+			@else
+				<a href="{{ $oauthData->registrationRoute }}"
+					class = 'ml-2 cursor-pointer'>
+					{{ sprintf(__('oauth.SET_UP'), ucfirst($oauthData->providerType)) }}
+				</a>
+			@endif
 		</div>
-		@else
-		<div class='text-text-main-200 my-1 hover:text-primary-400'>
-			<i @class(["align-middle w-4",
-			 "fa-brands fa-apple" => $oauthData->providerType === 'apple',
-			 "fa-brands fa-amazon" => $oauthData->providerType === 'amazon',
-			 "fa-brands fa-facebook" => $oauthData->providerType === 'facebook',
-			 "fa-brands fa-github" => $oauthData->providerType === 'github',
-			 "fa-brands fa-google" => $oauthData->providerType === 'google',
-			 "fa-brands fa-mastodon" => $oauthData->providerType === 'mastodon',
-			 "fa-brands fa-microsoft" => $oauthData->providerType === 'microsoft',
-			 "fa-solid fa-cloud" => $oauthData->providerType === 'nextcloud',
-			 ])></i>
-			<a href="{{ $oauthData->registrationRoute }}"
-				class = 'ml-2 cursor-pointer'
-			>{{ sprintf(__('oauth.SET_UP'), ucfirst($oauthData->providerType)) }}</a>
-		</div>
-		@endif
 	@endforeach
 	@endif
 </div>
