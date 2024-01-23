16d658cba49a40f6439861e1d8785ca34c2f821b
ildyria <beviguier@gmail.com>
2024-01-23 20:04:09 +0100

    make JS invariant

--- resources/views/hook-redirection.blade.php
+++ resources/views/hook-redirection.blade.php
@@ -1,13 +1,22 @@
+<div 
+	id="redirectData"
+	data-gallery="{{ $gallery }}"
+	data-redirect="{{ $base }}"
+></div>
 <script>
 	// Dirty work around.
 	const hashMatch = document.location.hash.replace("#", "").split("/");
 	const albumID = hashMatch[0] ?? '';
 	const photoID = hashMatch[1] ?? '';
+	const elem = document.getElementById('redirectData');
+	const gallery = elem.dataset.gallery;
+	const base = elem.dataset.redirect;
+
 	if (photoID !== '') {
-		window.location = '{{ $gallery }}/' + albumID + '/' + photoID;
+		window.location = gallery + '/' + albumID + '/' + photoID;
 	} else if (albumID !== '') {
-		window.location = '{{ $gallery }}/' + albumID;
+		window.location = gallery + '/' + albumID;
 	} else {
-		window.location = '{{ $base }}';
+		window.location = base;
 	}
 </script>
