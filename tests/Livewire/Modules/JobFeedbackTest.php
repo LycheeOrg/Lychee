4e38e309117c38b7c72531518b48bbcbe7dcade3
ildyria <beviguier@gmail.com>
2024-01-23 18:53:23 +0100

    add 2 tests

---
+++ tests/Livewire/Modules/JobFeedbackTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * We don't care for unhandled exceptions in tests.
+ * It is the nature of a test to throw an exception.
+ * Without this suppression we had 100+ Linter warning in this file which
+ * don't help anything.
+ *
+ * @noinspection PhpDocMissingThrowsInspection
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+
+namespace Tests\Livewire\Modules;
+
+use App\Livewire\Components\Modules\Jobs\Feedback;
+use Livewire\Livewire;
+use Tests\Livewire\Base\BaseLivewireTest;
+
+class JobFeedbackTest extends BaseLivewireTest
+{
+	private string $component = Feedback::class;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+	}
+
+	public function tearDown(): void
+	{
+		parent::tearDown();
+	}
+
+	public function testLoggedOut(): void
+	{
+		Livewire::test($this->component)
+			->assertViewIs('livewire.modules.jobs.feedback')
+			->assertSet('display', false)
+			->assertOk();
+	}
+
+	public function testLoggedIn(): void
+	{
+		Livewire::actingAs($this->admin)->test($this->component)
+			->assertViewIs('livewire.modules.jobs.feedback')
+			->assertSet('display', true)
+			->assertOk();
+	}
+}
