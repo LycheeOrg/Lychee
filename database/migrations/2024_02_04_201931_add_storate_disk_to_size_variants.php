20da9f41b7ffeca82bf4814164f321ed68d6349a
Kovah <mail@kovah.de>
2024-02-07 15:41:26 +0100

    Remove deprecated migration steps for new storage_disk column

--- database/migrations/2024_02_04_201931_add_storate_disk_to_size_variants.php
+++ database/migrations/2024_02_04_201931_add_storate_disk_to_size_variants.php
@@ -9,27 +9,8 @@ return new class() extends Migration {
 	public const COLUMN = 'storage_disk';
 	public const DEFAULT = 'images';
 
-	// ! remove me in final PR
-	public const TABLE_PHOTO = 'photos';
-	public const COLUMN_REMOVED = 'external_storage';
-
 	public function up(): void
 	{
-		// ! remove me in final PR
-		if (Schema::hasColumn(self::TABLE_SV, self::COLUMN_REMOVED)) {
-			Schema::table(self::TABLE_SV, function (Blueprint $table) {
-				$table->dropColumn(self::COLUMN_REMOVED);
-			});
-		}
-		// ! remove me in final PR
-		Schema::disableForeignKeyConstraints();
-		if (Schema::hasColumn(self::TABLE_PHOTO, self::COLUMN_REMOVED)) {
-			Schema::table(self::TABLE_PHOTO, function (Blueprint $table) {
-				$table->dropColumn(self::COLUMN_REMOVED);
-			});
-		}
-		Schema::enableForeignKeyConstraints();
-
 		Schema::table(self::TABLE_SV, function (Blueprint $table) {
 			$table->string(self::COLUMN)->default(self::DEFAULT);
 		});
