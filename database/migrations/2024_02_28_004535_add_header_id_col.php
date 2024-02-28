c75f06f817a62254dfa936c1e283d22f14bb5dd3
aSouchereau <alex@souch.ca>
2024-02-28 15:26:02 -0500

    Added migration for header_id col

---
+++ database/migrations/2024_02_28_004535_add_header_id_col.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+	private const RANDOM_ID_LENGTH = 24;
+
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::table('albums', function (Blueprint $table) {
+			$table->char('header_id', self::RANDOM_ID_LENGTH)->after('cover_id')->nullable()->default(null);
+
+			$table->foreign('header_id')
+				->references('id')->on('photos')
+				->onUpdate('CASCADE')
+				->onDelete('SET NULL');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::table('albums', function (Blueprint $table) {
+			$table->dropColumn('header_id');
+
+			$table->dropForeign('header_id');
+		});
+	}
+};
