0a7305d373c778000208b30bf43bc16adcecd975
ildyria <beviguier@gmail.com>
2024-04-14 21:43:51 +0200

    add drop select for the header image

---
+++ app/Livewire/Components/Forms/Album/SetHeader.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Livewire\Components\Forms\Album;
+
+use App\Contracts\Models\AbstractAlbum;
+use App\Livewire\Traits\Notify;
+use App\Livewire\Traits\UseValidator;
+use App\Models\Album;
+use App\Models\Photo;
+use App\Models\User;
+use App\Policies\AlbumPolicy;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Locked;
+use Livewire\Component;
+
+class SetHeader extends Component
+{
+	use AuthorizesRequests;
+	use UseValidator;
+	use Notify;
+
+	#[Locked] public string $albumID;
+	public ?string $search = null; // ! wired
+	/** @var array<int,array{id:string,title:string,thumb:string}> */
+	#[Locked] public array $photoListSaved;
+	public ?string $header_id;
+	public ?string $title;
+
+	/**
+	 * This is the equivalent of the constructor for Livewire Components.
+	 *
+	 * @param string $album_id to update the attributes of
+	 *
+	 * @return void
+	 */
+	public function mount(string $album_id): void
+	{
+		/** @var Album $album */
+		$album = Album::with(['header'])->findOrFail($album_id);
+
+		Gate::authorize(AlbumPolicy::CAN_EDIT, [AbstractAlbum::class, $album]);
+
+		$this->albumID = $album_id;
+
+		$this->photoListSaved = Photo::with('size_variants')->where('album_id', '=', $album_id)->get()->map(fn (Photo $photo) => [
+			'id' => $photo->id,
+			'title' => $photo->title,
+			'thumb' => $photo->size_variants->getThumb()?->url ?? URL::asset('img/no_images.svg'),
+		])->all();
+
+		$this->header_id = $album->header_id;
+		$this->title = $album->header?->title;
+	}
+
+	/**
+	 * Simply render the form.
+	 *
+	 * @return View
+	 */
+	public function render(): View
+	{
+		return view('livewire.forms.album.set-header');
+	}
+
+	/**
+	 * Give the tree of albums owned by the user.
+	 *
+	 * @return array<int,array{id:string,title:string,thumb:string}>
+	 */
+	public function getPhotoListProperty(): array
+	{
+		$filtered = collect($this->photoListSaved);
+		if ($this->search !== null && trim($this->search) !== '') {
+			return $filtered->filter(function (array $photo) {
+				return Str::contains($photo['title'], ltrim($this->search), true);
+			})->all();
+		}
+
+		return $filtered->all();
+	}
+
+	public function select(?string $photoId, ?string $title): void
+	{
+		Gate::authorize(AlbumPolicy::CAN_EDIT_ID, [AbstractAlbum::class, [$this->albumID]]);
+
+		Album::where('id', '=', $this->albumID)->update(['header_id' => $photoId]);
+		$this->header_id = $photoId;
+		$this->title = $title;
+	}
+
+	public function clearHeaderId(): void
+	{
+		$this->select(null, null);
+	}
+}
---
+++ resources/views/livewire/forms/album/set-header.blade.php
@@ -0,0 +1,48 @@
+<div class="mt-4 h-10" x-data="{ isSearchPhotoOpen: false }" x-on:click.away="isSearchPhotoOpen = false">
+	<span class="font-bold">{{ __('lychee.SET_HEADER') }}</span>
+	@if ($header_id !== null)
+		<span class="h-4 inline-block relative px-2 cursor-pointer text-2xs">{{ $title }}
+			<a class="block absolute right-0 top-0" wire:click="clearHeaderId()">
+			<x-icons.iconic class=" fill-red-800 w-4 h-4 mb-2 translate-x-full" icon="x" /></a>
+		</span>
+	@else
+		<span class="mx-2 w-full">
+			<form>
+				<input wire:model.live.debounce.500ms="search" type="text"
+					class="my-[1px] text-text-main-0 rounded bg-black/30 inline-block text-2xs align-middle
+					px-2 py-1 focus:outline-none focus:shadow-outline
+					placeholder:text-text-main-400"
+					placeholder="Search (Press '/' to focus)" x-ref="search"
+					@keydown.window="
+		if (event.key === '/') {
+			event.preventDefault();
+			$refs.search.focus();
+		}"
+					@focus="isSearchPhotoOpen = true" @keydown="isSearchPhotoOpen = true"
+					@keydown.escape.window="isSearchPhotoOpen = false"
+					@keydown.shift.tab="isSearchPhotoOpen = false">
+			</form>
+		</span>
+		<div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
+		<div class="z-50 absolute bg-bg-900 text-xs rounded mt-4"
+			x-show.transition.opacity="isSearchPhotoOpen">
+			@if (count($this->photoList) > 0)
+				<ul class=" max-h-[50vh] overflow-y-auto">
+					@foreach ($this->photoList as $result)
+						<li class="border-b border-bg-800 cursor-pointer transition-all ease-in-out duration-300
+									hover:bg-gradient-to-b hover:from-primary-500 hover:to-primary-600 hover:text-text-main-0"
+							wire:click="select('{{ $result['id'] }}',@js($result['title']))">
+							<a class="px-3 py-1 flex items-center"
+								@if ($loop->last) @keydown.tab="isSearchPhotoOpen = false" @endif>
+								<img src="{{ $result['thumb'] }}" alt="poster" class=" w-4 rounded-sm">
+								<span class="ml-4 text-left">{{ ($result['title']) }}</span>
+							</a>
+						</li>
+					@endforeach
+				</ul>
+			@else
+				<div class="px-3 py-3">No results for "{{ $search }}"</div>
+			@endif
+		</div>
+	@endif
+</div>
