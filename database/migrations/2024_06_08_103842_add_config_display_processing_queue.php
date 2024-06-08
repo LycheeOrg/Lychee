59293f6df077a409ef35600498dbe5b83f04fcf1
Benoît Viguier <ildyria@users.noreply.github.com>
2024-06-08 20:31:19 +0200

    Add missing primary key in Jobs_history + option to disable feedback completely. (#2457)
    * Add missing primary key in Jobs_history + option to disable feedback completely. (#2457)
    * Wrap collections to avoid double queries (#2458)

---
+++ database/migrations/2024_06_08_103842_add_config_display_processing_queue.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Extensions\BaseConfigMigration;
+
+return new class() extends BaseConfigMigration {
+	public const PROCESSING = 'Image Processing';
+	public const BOOL = '0|1';
+
+	public function getConfigs(): array
+	{
+		return [
+			[
+				'key' => 'job_processing_queue_visible',
+				'value' => '1',
+				'is_secret' => false,
+				'cat' => self::PROCESSING,
+				'type_range' => self::BOOL,
+				'description' => 'Enable the processing queue in the bottom left corner.',
+			],
+		];
+	}
+};
