eca086d39797402f24e44eefcc7d5c76fee8534c
ildyria <beviguier@gmail.com>
2024-06-08 12:30:16 +0200

    remove commented code

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,8 +64,6 @@ class Kernel extends HttpKernel
 		],
 
 		'api' => [
-			// 'installation:complete',
-			// 'admin_user:set',
 			'accept_content_type:json',
 			'content_type:json',
 			\Illuminate\Cookie\Middleware\EncryptCookies::class,
