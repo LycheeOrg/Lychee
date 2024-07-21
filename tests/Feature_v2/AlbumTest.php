df5fdf95f87c57ceab3db530611cf2878b5dccaf
ildyria <beviguier@gmail.com>
2024-07-21 22:01:27 +0200

    more tests

--- tests/Feature_v2/AlbumTest.php
+++ tests/Feature_v2/AlbumTest.php
@@ -16,11 +16,6 @@ use Tests\Feature_v2\Base\BaseApiV2Test;
 
 class AlbumTest extends BaseApiV2Test
 {
-	/**
-	 * Test album functions.
-	 *
-	 * @return void
-	 */
 	public function testGet(): void
 	{
 		$response = $this->getJson('Album::get');
@@ -28,5 +23,50 @@ class AlbumTest extends BaseApiV2Test
 		$response->assertJson([
 			'message' => 'The album id field is required.',
 		]);
+	}
+
+	public function testGetAnon(): void
+	{
+		$response = $this->getJsonWithData('Album::get', ['album_id' => $this->album4->id]);
+		$response->assertOk();
+		$response->assertJson([
+			'config' => [
+				'is_base_album' => true,
+				'is_model_album' => true,
+				'is_accessible' => true,
+				'is_password_protected' => false,
+				'is_search_accessible' => false,
+			],
+			'resource' => [
+				'id' => $this->album4->id,
+				'title' => $this->album4->title,
+				'albums' => [
+					[
+						'id' => $this->subAlbum4->id,
+						'title' => $this->subAlbum4->title,
+						'is_public' => true,
+						'thumb' => [
+							'id' => $this->subPhoto4->id,
+						],
+					],
+				],
+				'photos' => [
+					[
+						'id' => $this->photo4->id,
+					],
+				],
+			],
+		]);
+	}
+
+	public function testGetUnauthorizedOrForbidden(): void
+	{
+		// Unauthorized if not logged in.
+		$response = $this->getJsonWithData('Album::get', ['album_id' => $this->album1->id]);
+		$response->assertUnauthorized();
+
+		// Forbidden if logged in.
+		$response = $this->actingAs($this->userLocked)->getJsonWithData('Album::get', ['album_id' => $this->album1->id]);
+		$response->assertForbidden();
 	}
 }
