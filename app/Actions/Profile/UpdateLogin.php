305c333925e72a4e0c09bea8682ffb045c8b4461
ildyria <beviguier@gmail.com>
2024-08-04 20:59:35 +0200

    oups

--- app/Actions/Profile/UpdateLogin.php
+++ app/Actions/Profile/UpdateLogin.php
@@ -5,48 +5,12 @@ namespace App\Actions\Profile;
 use App\Exceptions\ConfigurationKeyMissingException;
 use App\Exceptions\ConflictingPropertyException;
 use App\Exceptions\Internal\QueryBuilderException;
-use App\Exceptions\ModelDBException;
-use App\Exceptions\UnauthenticatedException;
-use App\Models\Configs;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 class UpdateLogin
 {
-	/**
-	 * Changes and modifies login parameters of CURRENT user (may be admin).
-	 *
-	 * @param string|null $username
-	 * @param string      $password
-	 * @param string      $oldPassword
-	 * @param string      $ip
-	 *
-	 * @return User updated user
-	 *
-	 * @throws UnauthenticatedException
-	 * @throws QueryBuilderException
-	 * @throws ConflictingPropertyException
-	 * @throws ModelDBException
-	 */
-	public function do(?string $username, string $password, string $oldPassword, string $ip): User
-	{
-		/** @var User $user */
-		$user = Auth::user() ?? throw new UnauthenticatedException();
-
-		if ($username !== null &&
-			$username !== '' &&
-			Configs::getValueAsBool('allow_username_change')) {
-			$this->updateUsername($user, $username, $ip);
-		}
-
-		$user->password = Hash::make($password);
-		$user->save();
-
-		return $user;
-	}
-
 	/**
 	 * Update Username if it does not already exists.
 	 *
--- app/Legacy/V1/RuleSets/User/ChangeLoginRuleSet.php
+++ app/Legacy/V1/RuleSets/User/ChangeLoginRuleSet.php
@@ -4,7 +4,6 @@ namespace App\Legacy\V1\RuleSets\User;
 
 use App\Contracts\Http\RuleSet;
 use App\Legacy\V1\Contracts\Http\Requests\RequestAttribute;
-use App\Rules\CurrentPasswordRule;
 use App\Rules\PasswordRule;
 use App\Rules\UsernameRule;
 
@@ -18,7 +17,7 @@ class ChangeLoginRuleSet implements RuleSet
 		return [
 			RequestAttribute::USERNAME_ATTRIBUTE => ['sometimes', new UsernameRule(true)],
 			RequestAttribute::PASSWORD_ATTRIBUTE => ['required', new PasswordRule(false)],
-			RequestAttribute::OLD_PASSWORD_ATTRIBUTE => ['required', new PasswordRule(false), new CurrentPasswordRule()],
+			RequestAttribute::OLD_PASSWORD_ATTRIBUTE => ['required', new PasswordRule(false)],
 		];
 	}
 }
