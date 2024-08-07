04b76b4e0f8df064536dc385d4bcb0ae8dd16a9d
Keinsleif <mountaindull@gmail.com>
2024-08-08 03:08:52 +0900

    [add] ja localization files

---
+++ lang/ja/oauth.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @noinspection SpellCheckingInspection */
+
+return [
+	'NOT_AVAILABLE' => 'Oauth は利用可能ではありません。',
+	'SET_UP_CREDENTIALS' => '.env で資格情報を設定してください',
+	'SET_UP_OAUTH' => 'Oauth 認証を設定する',
+	'SET_UP' => '%s を設定する',
+	'TOKEN_REGISTERED' => '%s トークンが登録されました。',
+	'RESET' => 'リセット',
+];
