929f695fc7dc05ebc4be05651958c8ac07b10833
ildyria <beviguier@gmail.com>
2024-08-07 21:31:51 +0200

    add integrity check

--- app/Actions/Diagnostics/Pipes/Infos/InstallTypeInfo.php
+++ app/Actions/Diagnostics/Pipes/Infos/InstallTypeInfo.php
@@ -6,6 +6,7 @@ use App\Actions\Diagnostics\Diagnostics;
 use App\Assets\Features;
 use App\Contracts\DiagnosticPipe;
 use App\Metadata\Versions\InstalledVersion;
+use LycheeVerify\Verify;
 
 /**
  * What kind of Lychee install we are looking at?
@@ -13,11 +14,10 @@ use App\Metadata\Versions\InstalledVersion;
  */
 class InstallTypeInfo implements DiagnosticPipe
 {
-	private InstalledVersion $installedVersion;
-
-	public function __construct(InstalledVersion $installedVersion)
+	public function __construct(
+		private InstalledVersion $installedVersion,
+		private Verify $verify)
 	{
-		$this->installedVersion = $installedVersion;
 	}
 
 	/**
@@ -25,7 +25,7 @@ class InstallTypeInfo implements DiagnosticPipe
 	 */
 	public function handle(array &$data, \Closure $next): array
 	{
-		$data[] = Diagnostics::line('composer install:', $this->installedVersion->isDev() ? 'dev' : '--no-dev');
+		$data[] = Diagnostics::line('composer install:', ($this->installedVersion->isDev() ? 'dev' : '--no-dev') . ($this->verify->validate() ? '' : '*'));
 		$data[] = Diagnostics::line('APP_ENV:', config('app.env')); // check if production
 		$data[] = Diagnostics::line('APP_DEBUG:', config('app.debug') === true ? 'true' : 'false'); // check if debug is on (will help in case of error 500)
 		$data[] = Diagnostics::line('APP_URL:', config('app.url') !== 'http://localhost' ? 'set' : 'default'); // Some people leave that value by default... It is now breaking their visual.
