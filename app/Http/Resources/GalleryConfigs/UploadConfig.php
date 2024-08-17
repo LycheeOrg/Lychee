7074c845a704551fb1c2d18615e2eea3204142d6
ildyria <beviguier@gmail.com>
2024-08-17 17:00:18 +0200

    fix phpstan

--- app/Http/Resources/GalleryConfigs/UploadConfig.php
+++ app/Http/Resources/GalleryConfigs/UploadConfig.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources\GalleryConfigs;
 
 use App\Facades\Helpers;
 use App\Models\Configs;
+use Safe\Exceptions\InfoException;
+use function Safe\ini_get;
 use Spatie\LaravelData\Data;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
@@ -17,8 +19,13 @@ class UploadConfig extends Data
 	{
 		$this->upload_processing_limit = max(1, Configs::getValueAsInt('upload_processing_limit'));
 		$chunk_size = Configs::getValueAsInt('upload_chunk_size');
-		$upload_max_filesize = Helpers::convertSize(ini_get('upload_max_filesize'));
-		$post_max_size = Helpers::convertSize(ini_get('post_max_size'));
+		try {
+			$upload_max_filesize = Helpers::convertSize(ini_get('upload_max_filesize'));
+			$post_max_size = Helpers::convertSize(ini_get('post_max_size'));
+		} catch (InfoException $e) {
+			$upload_max_filesize = 1024 * 1024;
+			$post_max_size = 1024 * 1024;
+		}
 		$this->upload_chunk_size = $chunk_size > 0 ? $chunk_size : min($upload_max_filesize, $post_max_size);
 	}
 }
