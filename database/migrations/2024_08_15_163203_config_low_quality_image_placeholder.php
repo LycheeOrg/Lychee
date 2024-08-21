c7bc30ee4e2d397a0a5848dfff499a8516e6c203
aSouchereau <dev@souch.ca>
2024-08-21 11:27:37 -0400

    Use correct BaseConfigMigration class

--- database/migrations/2024_08_15_163203_config_low_quality_image_placeholder.php
+++ database/migrations/2024_08_15_163203_config_low_quality_image_placeholder.php
@@ -1,17 +1,20 @@
 <?php
 
-use App\Legacy\BaseConfigMigration;
+use App\Models\Extensions\BaseConfigMigration;
 
 return new class() extends BaseConfigMigration {
+	public const PROCESSING = 'Image Processing';
+
 	public function getConfigs(): array
 	{
 		return [
 			[
 				'key' => 'low_quality_image_placeholder',
 				'value' => '1',
-				'cat' => 'Image Processing',
+				'cat' => self::PROCESSING,
 				'type_range' => self::BOOL,
 				'description' => 'Enable low quality image placeholders',
+				'is_secret' => false,
 			],
 		];
 	}
