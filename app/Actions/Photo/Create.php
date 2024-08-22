5915bfb569606808cac81b29df6d61af9c645566
aSouchereau <dev@souch.ca>
2024-08-22 16:16:40 -0400

    Add pipe to other set of standalone pipes

--- app/Actions/Photo/Create.php
+++ app/Actions/Photo/Create.php
@@ -246,6 +246,7 @@ class Create
 			Shared\Save::class,
 			Standalone\CreateOriginalSizeVariant::class,
 			Standalone\CreateSizeVariants::class,
+			Standalone\EncodePlaceholder::class,
 			Shared\UploadSizeVariantsToS3::class,
 		];
 		$standAloneDto = $this->executePipeOnDTO($standAlonePipes, $standAloneDto);
