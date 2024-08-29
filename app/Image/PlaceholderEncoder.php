0d9d938dcffbfdf892a6a1294a76f16ea4f643b2
aSouchereau <dev@souch.ca>
2024-08-29 12:17:34 -0400

    Separate compression step into its own function

--- app/Image/PlaceholderEncoder.php
+++ app/Image/PlaceholderEncoder.php
@@ -18,17 +18,40 @@ use function Safe\stream_get_contents;
 
 class PlaceholderEncoder
 {
-	private const IMAGE_QUALITY = 30;
+	private const IMAGE_QUALITY = 50;
 
+	/** Character length that the encoded base64 image cannot exceed.
+	 *	The initial value 255 is determined by the max characters in size_variants.short_path DB column.
+	 *  Since base64 encodes into groups of 4 characters, with padding to fill the final
+	 *  group if it is less than 3 bytes, the actual limit can be calculated by:
+	 * 	255 - (255 % 4) = 252 characters.
+	 */
+	private const BASE64_SIZE_LIMIT = 252;
+
+	/** Filesize in bytes that the compressed image cannot exceed.
+	 * The limit is calculated using the BASE64_SIZE_LIMIT.
+	 * Base64 encodes every 3 bytes into 4 characters.
+	 *    - 252 / 4 = 63 groups of four characters
+	 *    - Since each group of 4 chars corresponds to 3 bytes of data,
+	 *      63 * 3 = 189 bytes of unencoded data.
+	 */
+	private const FILESIZE_LIMIT = 189;
+	private const MAX_COMPRESSION_RETRIES = 3;
+
+	/**
+	 * Performs the steps to encode a usable placeholder image and saves it in
+	 * the size variant's short_path column.
+	 *
+	 * @param SizeVariant $sizeVariant unencoded placeholder size variant
+	 *
+	 * @return void
+	 */
 	public function do(SizeVariant $sizeVariant): void
 	{
 		try {
 			$inMemoryBuffer = new InMemoryBuffer();
-			// Compress webp image to acceptable size for DB,
-			// size_variants.short_path column allows max 255 characters.
-			// We need a bit of breathing room because the file
-			// may expand up to 33% when we convert to base64.
-			imagewebp($this->createGdImage($sizeVariant->getFile()), $inMemoryBuffer->stream(), self::IMAGE_QUALITY);
+			$gdImage = $this->createGdImage($sizeVariant->getFile());
+			$this->compressImage($gdImage, $inMemoryBuffer);
 
 			$base64 = new TemporaryLocalFile('');
 
@@ -36,9 +59,14 @@ class PlaceholderEncoder
 			$base64->write($inMemoryBuffer->read());
 			$base64->close();
 
-			$sizeVariant->filesize = $base64->getFilesize();
-			$sizeVariant->short_path = stream_get_contents($base64->read());
-			$sizeVariant->save();
+			$base64Length = $base64->getFilesize();
+			if ($base64Length <= self::BASE64_SIZE_LIMIT) {
+				$sizeVariant->filesize = $base64->getFilesize();
+				$sizeVariant->short_path = stream_get_contents($base64->read());
+				$sizeVariant->save();
+			} else {
+				throw new MediaFileOperationException('Encoded image is too large.');
+			}
 		} catch (\ErrorException $e) {
 			throw new MediaFileOperationException('Failed to encode placeholder to base64', $e);
 		}
@@ -77,5 +105,37 @@ class PlaceholderEncoder
 		$referenceImage = imagecreatefromstring($imgBinary);
 
 		return $referenceImage;
+	}
+
+	/**
+	 * Compress webp image to acceptable size for DB.
+	 *
+	 * @param \GdImage       $source source Image
+	 * @param InMemoryBuffer $output the file to write to
+	 *
+	 * @return void
+	 *
+	 * @throws ImageException
+	 * @throws FilesystemException
+	 */
+	private function compressImage(\GdImage $source, InMemoryBuffer $output): void
+	{
+		$quality = self::IMAGE_QUALITY;
+		$retries = 0;
+		// Given a proper placeholder source image (16px x 16px) it should
+		// almost always be sufficiently compressed on the first attempt.
+		// But just in case it isn't we try to compress again.
+		do {
+			// ensure buffer is empty before trying to compress again
+			$emptyStream = \Safe\fopen('php://temp', 'w+');
+			$output->write($emptyStream);
+			\Safe\fclose($emptyStream);
+
+			imagewebp($source, $output->stream(), $quality);
+			$filesize = \Safe\fstat($output->read())['size'];
+
+			$quality -= 5;
+			$retries++;
+		} while ($filesize > self::FILESIZE_LIMIT && $retries <= self::MAX_COMPRESSION_RETRIES);
 	}
 }
