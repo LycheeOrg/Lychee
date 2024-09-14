48f675dba96fc56661d166cc43fea6a14d277be7
ildyria <beviguier@gmail.com>
2024-09-14 15:40:42 +0200

    fixes

--- app/Http/Resources/GalleryConfigs/InitConfig.php
+++ app/Http/Resources/GalleryConfigs/InitConfig.php
@@ -5,6 +5,9 @@ namespace App\Http\Resources\GalleryConfigs;
 use App\Enum\ImageOverlayType;
 use App\Enum\ThumbOverlayVisibilityType;
 use App\Models\Configs;
+use App\Policies\SettingsPolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Spatie\LaravelData\Data;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
@@ -22,6 +25,7 @@ class InitConfig extends Data
 	public ImageOverlayType $image_overlay_type;
 	public ThumbOverlayVisibilityType $display_thumb_album_overlay;
 	public ThumbOverlayVisibilityType $display_thumb_photo_overlay;
+	public ?string $clockwork_url;
 
 	public function __construct()
 	{
@@ -35,5 +39,25 @@ class InitConfig extends Data
 		$this->display_thumb_album_overlay = Configs::getValueAsEnum('display_thumb_album_overlay', ThumbOverlayVisibilityType::class);
 		$this->display_thumb_photo_overlay = Configs::getValueAsEnum('display_thumb_photo_overlay', ThumbOverlayVisibilityType::class);
 		$this->show_keybinding_help_popup = Configs::getValueAsBool('show_keybinding_help_popup');
+		$this->clockwork_url = $this->has_clockwork_in_menu();
+	}
+
+	private function has_clockwork_in_menu(): string|null
+	{
+		if (!Gate::check(SettingsPolicy::CAN_ACCESS_DEV_TOOLS, [Configs::class])) {
+			return null;
+		}
+
+		// Defining clockwork URL
+		$clockWorkEnabled = config('clockwork.enable') === true || (config('app.debug') === true && config('clockwork.enable') === null);
+		$clockWorkWeb = config('clockwork.web');
+		if ($clockWorkEnabled && $clockWorkWeb === true) {
+			return URL::asset('clockwork/app');
+		}
+		if (is_string($clockWorkWeb)) {
+			return $clockWorkWeb . '/app';
+		}
+
+		return null;
 	}
 }
