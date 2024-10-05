5b44018d30bfd1bc7461875bbedb56c74e208c8c
ildyria <beviguier@gmail.com>
2024-10-05 10:00:10 +0200

    fix Unsorted Album being empty when public. Note that nobody owns pictures in Unsorted anymore!

--- app/SmartAlbums/UnsortedAlbum.php
+++ app/SmartAlbums/UnsortedAlbum.php
@@ -5,6 +5,7 @@ namespace App\SmartAlbums;
 use App\Enum\SmartAlbumType;
 use App\Exceptions\ConfigurationKeyMissingException;
 use App\Exceptions\Internal\FrameworkException;
+use App\Models\Photo;
 use Illuminate\Database\Eloquent\Builder;
 
 class UnsortedAlbum extends BaseSmartAlbum
@@ -27,5 +28,23 @@ class UnsortedAlbum extends BaseSmartAlbum
 	public static function getInstance(): self
 	{
 		return self::$instance ??= new self();
+	}
+
+	/**
+	 * In the case of unsorted, we cannot determine whether the photo is visible or not from its parent.
+	 * If the Unsorted album is made public, then all the pictures in it are visible (including pictures which are not owned by the current user).
+	 *
+	 * @return \App\Eloquent\FixedQueryBuilder<Photo>
+	 *
+	 * @throws InternalLycheeException
+	 */
+	public function photos(): Builder
+	{
+		if ($this->publicPermissions !== null) {
+			return Photo::query()->with(['album', 'size_variants', 'size_variants.sym_links'])
+			->where($this->smartPhotoCondition);
+		}
+
+		return parent::photos();
 	}
 }
