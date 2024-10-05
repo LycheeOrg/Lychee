8bdbde2d6bed6a78227609d1dfcfa7b2c400458e
ildyria <beviguier@gmail.com>
2024-10-05 19:54:25 +0200

    avoid flash

--- resources/js/composables/album/albumRefresher.ts
+++ resources/js/composables/album/albumRefresher.ts
@@ -2,7 +2,7 @@ import AlbumService from "@/services/album-service";
 import { AuthStore } from "@/stores/Auth";
 import { computed, Ref, ref } from "vue";
 
-export function useAlbumRefresher(albumId: Ref<string>, auth: AuthStore, isLoginOpen: Ref<boolean>) {
+export function useAlbumRefresher(albumId: Ref<string>, auth: AuthStore, isLoginOpen: Ref<boolean>, nsfw_consented: Ref<string[]>) {
 	const isPasswordProtected = ref(false);
 	const user = ref(undefined) as Ref<undefined | App.Http.Resources.Models.UserResource>;
 	const modelAlbum = ref(undefined as undefined | App.Http.Resources.Models.AlbumResource);
@@ -10,6 +10,7 @@ export function useAlbumRefresher(albumId: Ref<string>, auth: AuthStore, isLogin
 	const smartAlbum = ref(undefined as undefined | App.Http.Resources.Models.SmartAlbumResource);
 	const album = computed(() => modelAlbum.value || tagAlbum.value || smartAlbum.value);
 	const layout = ref(null) as Ref<null | App.Http.Resources.GalleryConfigs.PhotoLayoutConfig>;
+	const isAlbumConsented = ref(false);
 
 	const photos = ref([]) as Ref<App.Http.Resources.Models.PhotoResource[]>;
 	const config = ref(undefined) as Ref<undefined | App.Http.Resources.GalleryConfigs.AlbumConfig>;
@@ -37,6 +38,8 @@ export function useAlbumRefresher(albumId: Ref<string>, auth: AuthStore, isLogin
 					smartAlbum.value = data.data.resource as App.Http.Resources.Models.SmartAlbumResource;
 				}
 				photos.value = album.value?.photos ?? [];
+
+				isAlbumConsented.value = nsfw_consented.value.find((e) => e === albumId.value) !== undefined;
 			})
 			.catch((error) => {
 				if (error.response.status === 401 && error.response.data.message === "Password required") {
@@ -62,6 +65,7 @@ export function useAlbumRefresher(albumId: Ref<string>, auth: AuthStore, isLogin
 	}
 
 	return {
+		isAlbumConsented,
 		isPasswordProtected,
 		albumId,
 		user,
