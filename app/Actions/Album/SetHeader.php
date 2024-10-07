45bb7bfdefc6d0412d058e0e2c0f133e36558e2e
ildyria <beviguier@gmail.com>
2024-10-07 09:33:42 +0200

    fix bug where updating settings would remove the header image

--- app/Actions/Album/SetHeader.php
+++ app/Actions/Album/SetHeader.php
@@ -14,15 +14,16 @@ class SetHeader extends Action
 	 * @param Album  $album
 	 * @param bool   $is_compact
 	 * @param ?Photo $photo
+	 * @param bool   $shall_override
 	 *
 	 * @return Album
 	 */
-	public function do(Album $album, bool $is_compact, ?Photo $photo): Album
+	public function do(Album $album, bool $is_compact, ?Photo $photo, bool $shall_override = false): Album
 	{
 		if ($is_compact) {
 			$album->header_id = AlbumController::COMPACT_HEADER;
 		} else {
-			$album->header_id = ($album->header_id === $photo?->id) ? null : $photo?->id;
+			$album->header_id = ($album->header_id !== $photo?->id || $shall_override) ? $photo?->id : null;
 		}
 		$album->save();
 
