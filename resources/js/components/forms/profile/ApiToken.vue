8a3a117072fa0b122d362552eb28e3595c436685
ildyria <beviguier@gmail.com>
2024-10-10 19:33:52 +0200

    fix the constant loading + improved token UX

--- resources/js/components/forms/profile/ApiToken.vue
+++ resources/js/components/forms/profile/ApiToken.vue
@@ -12,37 +12,27 @@
 		<template #container="{ closeCallback }">
 			<form>
 				<div v-focustrap class="flex flex-col gap-4 relative w-[500px] text-sm rounded-md pt-9">
-					<div class="flex flex-row justify-between gap-2 px-9 w-full items-center">
-						<label for="token-dialog-token" class="flex-shrink">Token</label>
-						<InputText
-							class="flex-grow[4] bg-transparent w-full pt-1 pb-0 px-0.5 h-7 border-b border-b-solid focus:border-b-primary-500 disabled:italic disabled:text-center inline-block"
-							:disabled="isDisabled"
-							v-model="token"
-							:readonly="true"
-						/>
-						<Button text severity="secondary" class="group" :title="$t('lychee.RESET')" @click="resetToken">
-							<MiniIcon class="w-4 h-4 ionicons group-hover:fill-primary-500" icon="reload" />
-						</Button>
-						<Button
-							text
-							severity="secondary"
-							class="group"
-							:tile="$t('lychee.DISABLE_TOKEN_TOOLTIP')"
-							@click="unsetToken"
-							v-if="!isDisabled"
-						>
-							<MiniIcon class="w-4 h-4 ionicons group-hover:fill-red-700" icon="ban" />
-						</Button>
-					</div>
+					<div class="flex flex-col justify-center gap-2 px-9 w-full items-center">
+						<template v-if="token === undefined">
+						<span>{{ tokenText }}</span>
+						<a v-if="isDisabled" severity="contrast" class="cursor-pointer font-bold text-primary-500 underline" @click="resetToken">Create a new token</a>
+						<a v-else severity="contrast" class="cursor-pointer font-bold text-primary-500 underline" @click="resetToken">Reset the token</a>
+						</template>
+						<template v-else>
+							<span><i class="text-danger-600 pi pi-exclamation-triangle mr-2" />This token will not be displayed again. Copy it and keep it in a safe place.</span>
+							<InputText 
+								class="flex-grow[4] bg-transparent w-full pt-1 pb-0 px-0.5 h-7 border-b border-b-solid focus:border-b-primary-500 disabled:italic disabled:text-center inline-block"
+								v-model="token"
+								:readonly="true"
+							/>
+						</template>
+				</div>
 				</div>
 				<div class="flex justify-center mt-9">
-					<Button
-						@click="closeCallback"
-						severity="secondary"
-						class="w-full border-none font-bold border-1 border-white-alpha-30 hover:bg-white-alpha-10"
-					>
+					<Button @click="close" severity="secondary" class="w-full border-none font-bold rounded-none rounded-bl-lg">
 						{{ $t("lychee.CLOSE") }}
 					</Button>
+					<Button @click="disable" v-if="!isDisabled && token === undefined" severity="danger" class="w-full border-none font-bold rounded-none rounded-br-lg"> Disable </Button>
 				</div>
 			</form>
 		</template>
@@ -55,13 +45,15 @@ import Button from "primevue/button";
 import { trans } from "laravel-vue-i18n";
 import AuthService from "@/services/auth-service";
 import InputText from "@/components/forms/basic/InputText.vue";
-import MiniIcon from "@/components/icons/MiniIcon.vue";
 import ProfileService from "@/services/profile-service";
+import { useToast } from "primevue/usetoast";
 
 const visible = defineModel();
 
 const isDisabled = ref(true);
 const token = ref(undefined as undefined | string);
+const tokenText = ref("");
+const toast = useToast();
 
 function resetToken() {
 	ProfileService.resetToken().then((response) => {
@@ -69,16 +61,27 @@ function resetToken() {
 		isDisabled.value = false;
 	});
 }
-function unsetToken() {
+
+function close() {
+	// ! We hide this value to prevent the token from being displayed again.
+	token.value = undefined;
+	visible.value = false;
+}
+
+function disable() {
 	ProfileService.unsetToken().then(() => {
-		token.value = trans("lychee.DISABLED_TOKEN_STATUS_MSG");
-		isDisabled.value = true;
+		toast.add({
+			severity: "success",
+			summary: "Token disabled",
+			life: 3000,
+		});
+		visible.value = false;
 	});
 }
 
 watch(visible, (_value) => {
 	AuthService.user().then((response) => {
-		token.value = response.data.has_token ? trans("lychee.TOKEN_NOT_AVAILABLE") : trans("lychee.DISABLED_TOKEN_STATUS_MSG");
+		tokenText.value = response.data.has_token ? trans("lychee.TOKEN_NOT_AVAILABLE") : "No token API have been generated.";
 		isDisabled.value = !response.data.has_token;
 	});
 });
