342eb56f4fe12fef80d36b0f44e0a967d2a49674
ildyria <beviguier@gmail.com>
2024-12-03 23:47:03 +0100

    useless stuff

--- database/migrations/2024_12_03_221445_set_number_icons_mobile.php
+++ database/migrations/2024_12_03_221445_set_number_icons_mobile.php
@@ -3,11 +3,6 @@
 use App\Models\Extensions\BaseConfigMigration;
 
 return new class() extends BaseConfigMigration {
-	public const SQUARE = 'square';
-	public const JUSTIFIED = 'justified';
-	public const MASONRY = 'masonry';
-	public const GRID = 'grid';
-
 	public function getConfigs(): array
 	{
 		return [
