e0c277f2ebc94ebf155b934801235e66ca61a53e
Tino Hager <tino.hager@nager.at>
2024-12-21 20:26:44 +0100

    Update AboutLychee.vue

--- resources/js/components/modals/AboutLychee.vue
+++ resources/js/components/modals/AboutLychee.vue
@@ -1,5 +1,5 @@
 <template>
-	<Dialog v-model:visible="visible" modal :closable="true" pt:root:class="border-none m-3" pt:mask:style="backdrop-filter: blur(2px)" @hide="closeCallback">
+	<Dialog v-model:visible="visible" modal pt:root:class="border-none m-3" pt:mask:style="backdrop-filter: blur(2px)" @hide="closeCallback">
 		<template #container="{ closeCallback }">
 			<div class="flex flex-col gap-4 bg-gradient-to-b from-bg-300 to-bg-400 relative max-w-[500px] text-sm rounded-md text-muted-color">
 				<div class="p-9 text-muted-color-emphasis" v-if="version">
