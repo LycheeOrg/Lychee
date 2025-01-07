51cc1970930c8cf7097e28ccfac18686ce7b66f4
Sanchit Singh <sanckde@gmail.com>
2025-01-07 16:08:54 +0100

    Better documentation and adding new HTTP status code

--- app/Exceptions/SessionExpiredException.php
+++ app/Exceptions/SessionExpiredException.php
@@ -7,10 +7,12 @@ class SessionExpiredException extends BaseLycheeException
 	/** @var int HTTP_PAGE_EXPIRED proprietary Laravel HTTP status code to indicate session expiration */
 	public const HTTP_PAGE_EXPIRED = 419;
 
+	public const HTTP_LOGIN_TIMEOUT = 440;
+
 	public const DEFAULT_MESSAGE = 'Session expired';
 
-	public function __construct(string $msg = self::DEFAULT_MESSAGE, ?\Throwable $previous = null)
+	public function __construct(int $httpStatusCode = self::HTTP_PAGE_EXPIRED, string $msg = self::DEFAULT_MESSAGE, ?\Throwable $previous = null)
 	{
-		parent::__construct(self::HTTP_PAGE_EXPIRED, $msg, $previous);
+		parent::__construct($httpStatusCode, $msg, $previous);
 	}
 }
--- app/Http/Middleware/CheckSessionTimout.php
+++ app/Http/Middleware/CheckSessionTimout.php
@@ -11,7 +11,12 @@ use Symfony\Component\HttpFoundation\Response;
 class CheckSessionTimout
 {
 	/**
-	 * Handle an incoming request.
+	 * This middleware is used to reliably track session time of a logged-in user. It does so
+	 * by storing a cookie containing timestamp of when a user logs in. This timestamp is reset each
+	 * time an http request is sent by the user. If the difference between the
+	 * timestamp stored in the cookie and current timestamp is larger than session.lifetime, this means that
+	 * the user session has timed-out. At this point it forgets about this cookie and throws an
+	 * {@link \App\Exceptions\SessionExpiredException}.
 	 *
 	 * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
 	 */
@@ -20,20 +25,21 @@ class CheckSessionTimout
 		$timeout = config('session.lifetime') * 60;
 		$lastActivity = Cookie::get('lastActivityTime');
 
-		fwrite(STDOUT, print_r('Timeout: ', true) . $timeout . PHP_EOL);
-
-		fwrite(STDOUT, print_r('Last activity:  ', true) . $lastActivity . PHP_EOL);
-		fwrite(STDOUT, print_r('Current time:  ', true) . now()->timestamp . PHP_EOL);
+		fwrite(STDOUT, print_r('Last activity before Auth::guest(): ', true) . $lastActivity . PHP_EOL);
 
 		if (Auth::guest()) {
 			Cookie::queue(Cookie::forget('lastActivityTime'));
 		}
 
+		fwrite(STDOUT, print_r('Last activity after Auth::guest(): ', true) . $lastActivity . PHP_EOL);
+
 		if ($lastActivity && (now()->timestamp - $lastActivity > $timeout)) {
 			Cookie::queue(Cookie::forget('lastActivityTime'));
-			fwrite(STDOUT, print_r('Going to throw Session Expired Exception! ', true) . PHP_EOL);
-			throw new SessionExpiredException();
+			fwrite(STDOUT, print_r('Throwing session expired exception', true) . PHP_EOL);
+			throw new SessionExpiredException(SessionExpiredException::HTTP_LOGIN_TIMEOUT);
 		}
+
+		fwrite(STDOUT, print_r('Type of Last activity: ', true) . gettype($lastActivity) . PHP_EOL);
 
 		if (Auth::check()) {
 			cookie::queue('lastActivityTime', now()->timestamp, 0);
