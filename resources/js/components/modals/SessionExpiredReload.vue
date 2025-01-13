0a3cd97de6232666d9bc0d09d3964980acba1606
Sanchit Singh <sanckde@gmail.com>
2025-01-13 21:22:47 +0100

    Formatting fix

--- resources/js/components/modals/SessionExpiredReload.vue
+++ resources/js/components/modals/SessionExpiredReload.vue
@@ -1,9 +1,13 @@
 <template>
-	<Dialog v-model:visible="visible" modal pt:root:class="border-none m-3  max-w-md w-full"
-		pt:mask:style="backdrop-filter: blur(2px)" @hide="closeAndReload">
+	<Dialog
+		v-model:visible="visible"
+		modal
+		pt:root:class="border-none m-3  max-w-md w-full"
+		pt:mask:style="backdrop-filter: blur(2px)"
+		@hide="closeAndReload"
+	>
 		<template #container="{ closeCallback }">
-			<div
-				class="flex flex-col gap-4 bg-gradient-to-b from-bg-300 to-bg-400 relative text-sm rounded-md text-muted-color">
+			<div class="flex flex-col gap-4 bg-gradient-to-b from-bg-300 to-bg-400 relative text-sm rounded-md text-muted-color">
 				<div class="p-9 text-muted-color-emphasis">
 					<h1 class="mb-6 text-center text-2xl font-bold text-warning-700">
 						<i class="pi pi-exclamation-triangle mr-2" /> {{ $t("dialogs.session_expired.title") }}
@@ -11,12 +15,10 @@
 					<p class="text-muted-color text-center" v-html="$t('dialogs.session_expired.message')" />
 				</div>
 				<div class="flex justify-center">
-					<Button @click="closeCallback" severity="info"
-						class="w-full font-bold border-none rounded-none rounded-bl-xl">
+					<Button @click="closeCallback" severity="info" class="w-full font-bold border-none rounded-none rounded-bl-xl">
 						{{ $t("dialogs.session_expired.reload") }}
 					</Button>
-					<Button @click="gotoGallery" severity="contrast"
-						class="w-full font-bold border-none rounded-none rounded-br-xl">
+					<Button @click="gotoGallery" severity="contrast" class="w-full font-bold border-none rounded-none rounded-br-xl">
 						{{ $t("dialogs.session_expired.go_to_gallery") }}
 					</Button>
 				</div>
