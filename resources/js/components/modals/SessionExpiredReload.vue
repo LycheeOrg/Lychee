bd54d9ca207c3331a168236974723ea71d13cb66
Sanchit Singh <sanckde@gmail.com>
2025-01-13 21:10:39 +0100

    Close SessionExpiredReload dialog on routing to gallery

--- resources/js/components/modals/SessionExpiredReload.vue
+++ resources/js/components/modals/SessionExpiredReload.vue
@@ -1,13 +1,9 @@
 <template>
-	<Dialog
-		v-model:visible="visible"
-		modal
-		pt:root:class="border-none m-3  max-w-md w-full"
-		pt:mask:style="backdrop-filter: blur(2px)"
-		@hide="closeAndReload"
-	>
+	<Dialog v-model:visible="visible" modal pt:root:class="border-none m-3  max-w-md w-full"
+		pt:mask:style="backdrop-filter: blur(2px)" @hide="closeAndReload">
 		<template #container="{ closeCallback }">
-			<div class="flex flex-col gap-4 bg-gradient-to-b from-bg-300 to-bg-400 relative text-sm rounded-md text-muted-color">
+			<div
+				class="flex flex-col gap-4 bg-gradient-to-b from-bg-300 to-bg-400 relative text-sm rounded-md text-muted-color">
 				<div class="p-9 text-muted-color-emphasis">
 					<h1 class="mb-6 text-center text-2xl font-bold text-warning-700">
 						<i class="pi pi-exclamation-triangle mr-2" /> {{ $t("dialogs.session_expired.title") }}
@@ -15,15 +11,12 @@
 					<p class="text-muted-color text-center" v-html="$t('dialogs.session_expired.message')" />
 				</div>
 				<div class="flex justify-center">
-					<Button @click="closeCallback" severity="info" class="w-full font-bold border-none rounded-none rounded-bl-xl">
+					<Button @click="closeCallback" severity="info"
+						class="w-full font-bold border-none rounded-none rounded-bl-xl">
 						{{ $t("dialogs.session_expired.reload") }}
 					</Button>
-					<Button
-						as="router-link"
-						:to="{ name: 'gallery' }"
-						severity="contrast"
-						class="w-full font-bold border-none rounded-none rounded-br-xl"
-					>
+					<Button @click="gotoGallery" severity="contrast"
+						class="w-full font-bold border-none rounded-none rounded-br-xl">
 						{{ $t("dialogs.session_expired.go_to_gallery") }}
 					</Button>
 				</div>
@@ -35,11 +28,18 @@
 import Button from "primevue/button";
 import Dialog from "primevue/dialog";
 import { Ref } from "vue";
+import { useRouter } from "vue-router";
 
 const visible = defineModel("visible", { default: false }) as Ref<boolean>;
+const router = useRouter();
 
 function closeAndReload() {
 	visible.value = false;
 	location.reload();
+}
+
+async function gotoGallery() {
+	await router.push({ name: "gallery" });
+	visible.value = false;
 }
 </script>
