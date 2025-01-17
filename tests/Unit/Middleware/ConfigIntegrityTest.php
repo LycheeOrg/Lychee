73d2bc6776d7b60da9c80dc3d9c8ecbdf4014e8d
ildyria <beviguier@gmail.com>
2025-01-17 09:00:59 +0100

    also test the other way

--- tests/Unit/Middleware/ConfigIntegrityTest.php
+++ tests/Unit/Middleware/ConfigIntegrityTest.php
@@ -33,10 +33,24 @@ class ConfigIntegrityTest extends AbstractTestCase
 	{
 		$this->msgSection = (new ConsoleOutput())->section();
 
-		$keys = DB::table('configs')->select('key')->where('level', '=', '1')->pluck('key');
+		$keys = DB::table('configs')->select('key')->where('level', '=', '1')->pluck('key')->all();
 		foreach ($keys as $key) {
 			if (!in_array($key, ConfigIntegrity::SE_FIELDS, true)) {
 				$this->msgSection->writeln(sprintf('<comment>Error:</comment> Key %s is not in the list of keys.', $key));
+				$this->failed = true;
+			}
+		}
+
+		static::assertFalse($this->failed);
+	}
+
+	public function testKeys(): void
+	{
+		/** @var string[] $keys */
+		$keys = DB::table('configs')->select('key')->where('level', '=', '1')->pluck('key')->all();
+		foreach (ConfigIntegrity::SE_FIELDS as $key) {
+			if (!in_array($key, $keys, true)) {
+				$this->msgSection->writeln(sprintf('<comment>Error:</comment> Key %s is not in the database.', $key));
 				$this->failed = true;
 			}
 		}
