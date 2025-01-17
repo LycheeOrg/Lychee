23f5b77ea7b81e41bf23e49abc892cf6ef23a7f6
ildyria <beviguier@gmail.com>
2025-01-17 08:58:15 +0100

    add configuration integrity check

---
+++ tests/Unit/Middleware/ConfigIntegrityTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+/**
+ * We don't care for unhandled exceptions in tests.
+ * It is the nature of a test to throw an exception.
+ * Without this suppression we had 100+ Linter warning in this file which
+ * don't help anything.
+ *
+ * @noinspection PhpDocMissingThrowsInspection
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+
+namespace Tests\Unit\Middleware;
+
+use App\Http\Middleware\ConfigIntegrity;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\ConsoleSectionOutput;
+use Tests\AbstractTestCase;
+
+class ConfigIntegrityTest extends AbstractTestCase
+{
+	private ConsoleSectionOutput $msgSection;
+	private bool $failed = false;
+
+	public function testConfiguration(): void
+	{
+		$this->msgSection = (new ConsoleOutput())->section();
+
+		$keys = DB::table('configs')->select('key')->where('level', '=', '1')->pluck('key');
+		foreach ($keys as $key) {
+			if (!in_array($key, ConfigIntegrity::SE_FIELDS, true)) {
+				$this->msgSection->writeln(sprintf('<comment>Error:</comment> Key %s is not in the list of keys.', $key));
+				$this->failed = true;
+			}
+		}
+
+		static::assertFalse($this->failed);
+	}
+}
