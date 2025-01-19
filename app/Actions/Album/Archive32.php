98dd8f9a6b2bedb1b29230901a727e61daeba599
ildyria <beviguier@gmail.com>
2025-01-19 22:44:42 +0100

    zip-stream-2.1

---
+++ app/Actions/Album/Archive32.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+namespace App\Actions\Album;
+
+use App\Services\Archives\Zip21Trait;
+
+final class Archive32 extends BaseArchive
+{
+	use Zip21Trait;
+}
