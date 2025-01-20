b30a968fd5b8522a378c2d08c672b27ee3fff495
ildyria <beviguier@gmail.com>
2025-01-20 07:46:12 +0100

    fix alias

--- app/Services/Archives/Zip31Trait.php
+++ app/Services/Archives/Zip31Trait.php
@@ -16,6 +16,7 @@ use App\Models\Configs;
 use App\Models\Photo;
 use Composer\InstalledVersions;
 use Composer\Semver\VersionParser;
+use ZipStream\CompressionMethod as ZipMethod;
 use ZipStream\ZipStream;
 
 trait Zip31Trait
