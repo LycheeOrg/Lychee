52a983ae6bff47b4ebe290ea63e90d7fd7f78432
ildyria <beviguier@gmail.com>
2025-01-23 23:19:30 +0100

    add confirmation dialog as the action is destructive

--- lang/en/sharing.php
+++ lang/en/sharing.php
@@ -21,7 +21,15 @@ return [
 	'share' => 'Share',
 	'add_new_access_permission' => 'Add a new access permission',
 	'permission_deleted' => 'Permission deleted!',
-	'permission_created' => 'Permission created!',
+	'propagate' => 'Propagate',
+
+	'propagate_help' => 'Propagate the current access permissions to all descendants<br>(sub-albums and their respective sub-albums etc)',
+	'propagate_default' => 'By default, existing permissions (album-user)<br>are updated and the missing ones added.<br>Additional permissions not present in list are left untouched.',
+	'propagate_overwrite' => 'Overwrite the existing permissions instead of updating.<br>This will also remove all permissions not present in the list.',
+	'propagate_warning' => 'This action cannot be undone.',
+
+	'permission_overwritten' => 'Propagation successful! Permission overwritten!',
+	'permission_updated' => 'Propagation successful! Permission updated!',
 
 	'grants' => [
 		'read' => 'Grants read access',
