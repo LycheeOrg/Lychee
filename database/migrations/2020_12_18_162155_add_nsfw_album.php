385a297078f97e1c0f575b6791103fe26822a9d1
ildyria <beviguier@gmail.com>
2025-01-26 16:55:49 +0100

    add class-leak for later use

--- database/migrations/2020_12_18_162155_add_nsfw_album.php
+++ database/migrations/2020_12_18_162155_add_nsfw_album.php
@@ -22,10 +22,6 @@ return new class() extends Migration {
 	 */
 	public function up(): void
 	{
-		defined('STRING') or define('STRING', 'string');
-		defined('STRING_REQ') or define('STRING_REQ', 'string_required');
-		defined('BOOL') or define('BOOL', '0|1');
-
 		Schema::table(self::ALBUM, function ($table) {
 			$table->boolean(self::NSFW_COLUMN_NAME)->default(false)->after(self::VISIBLE_HIDDEN);
 		});
@@ -34,11 +30,11 @@ return new class() extends Migration {
 		});
 
 		DB::table('configs')->insert([
-			['key' => 'nsfw_visible', 'value' => '1', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => BOOL],
-			['key' => 'nsfw_blur', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => BOOL],
-			['key' => 'nsfw_warning', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => BOOL],
-			['key' => 'nsfw_warning_admin', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => BOOL],
-			['key' => 'nsfw_warning_text', 'value' => '<h1>Sensitive content</h1><p>This album contains sensitive content which some people may find offensive or disturbing.</p><p>Tap to consent.</p>', 'cat' => 'Mod NSFW', 'confidentiality' => '3', 'type_range' => STRING_REQ],
+			['key' => 'nsfw_visible', 'value' => '1', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => '0|1'],
+			['key' => 'nsfw_blur', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => '0|1'],
+			['key' => 'nsfw_warning', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => '0|1'],
+			['key' => 'nsfw_warning_admin', 'value' => '0', 'cat' => 'Mod NSFW', 'confidentiality' => '0', 'type_range' => '0|1'],
+			['key' => 'nsfw_warning_text', 'value' => '<h1>Sensitive content</h1><p>This album contains sensitive content which some people may find offensive or disturbing.</p><p>Tap to consent.</p>', 'cat' => 'Mod NSFW', 'confidentiality' => '3', 'type_range' => 'string_required'],
 		]);
 	}
 
