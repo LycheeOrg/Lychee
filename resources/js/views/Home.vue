497763a492834ac1f7dcab4a28dc6e639fbce147
ildyria <beviguier@gmail.com>
2025-02-21 19:18:54 +0100

    formatting

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
-<!-- Nothing to see here is is more of a redirection component -->
+	<!-- Nothing to see here is is more of a redirection component -->
 </template>
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-
+import { useRouter } from "vue-router";
 
 const router = useRouter();
 
 console.log("oups!");
-
 </script>
