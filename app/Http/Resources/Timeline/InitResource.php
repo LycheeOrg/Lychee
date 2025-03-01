d66813cb3a54da38c63d4b99cacbebdc738b34b1
ildyria <beviguier@gmail.com>
2025-03-01 18:52:19 +0100

    squash

---
+++ app/Http/Resources/Timeline/InitResource.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+namespace App\Http\Resources\Timeline;
+
+use App\Enum\PhotoLayoutType;
+use App\Models\Configs;
+use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Attributes\TypeScript;
+
+/**
+ * Initialization resource for the search.
+ */
+#[TypeScript()]
+class InitResource extends Data
+{
+	public PhotoLayoutType $photo_layout;
+
+	public function __construct()
+	{
+		$this->photo_layout = Configs::getValueAsEnum('timeline_photos_layout', PhotoLayoutType::class);
+	}
+}
