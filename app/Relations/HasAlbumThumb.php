764ee32e1f83faa520d114dd61715ec93083f7c6
Shobi <ppshobi@gmail.com>
2025-03-16 13:55:45 +0100

    phpstan: fix `phpstan-ignore-*` calls, + fix the dynamic call error by adding `$this->query()`

--- app/Relations/HasAlbumThumb.php
+++ app/Relations/HasAlbumThumb.php
@@ -64,7 +64,6 @@ class HasAlbumThumb extends Relation
 		 *
 		 * @noinspection PhpIncompatibleReturnTypeInspection
 		 *
-		 * @phpstan-ignore-next-line
 		 */
 		return $this->query;
 	}
@@ -83,7 +82,7 @@ class HasAlbumThumb extends Relation
 			/** @var Album $album */
 			$album = $this->parent;
 			if ($album->cover_id !== null) {
-				$this->where('photos.id', '=', $album->cover_id);
+				$this->query()->where('photos.id', '=', $album->cover_id);
 			} else {
 				$this->photoQueryPolicy
 					->applySearchabilityFilter(
@@ -269,7 +268,6 @@ class HasAlbumThumb extends Relation
 	public function match(array $models, Collection $results, $relation): array
 	{
 		$dictionary = $results->mapToDictionary(function ($result) {
-			/** @phpstan-ignore-next-line undefied property */
 			return [$result->covered_album_id => $result];
 		})->all();
 
