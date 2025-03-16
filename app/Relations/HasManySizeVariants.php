b2b186a3bedc4d7cca30f3f02bac59d8b0f650bd
Shobi <ppshobi@gmail.com>
2025-03-16 19:09:20 +0100

    review: revert informative comment

--- app/Relations/HasManySizeVariants.php
+++ app/Relations/HasManySizeVariants.php
@@ -38,8 +38,15 @@ class HasManySizeVariants extends HasMany
 
 	/**
 	 * Get the results of the relationship.
+ 	 * @internal The parent class allows to return `mixed`, hence it is
+	 *  perfectly fine to return `SizeVariants` acc. to Liskov's substitution
+	 *  principle.
+	 *  However, the buggy `larastan` ruleset lies to PhpStan about the return
+	 *  type.
+	 *  Hence, we must ignore the false positive here.
 	 *
 	 * @return SizeVariants
+	 *
 	 */
 	public function getResults(): SizeVariants
 	{
