9de5442e95c237bc2c65b20caa742b1a547277bf
Shobi <ppshobi@gmail.com>
2025-03-27 23:38:48 +0100

    test: AddAlbumRequestTest

---
+++ tests/Unit/Http/Requests/Album/AddAlbumRequestTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Http\Requests\Album;
+
+use App\Contracts\Http\Requests\RequestAttribute;
+use App\Contracts\Models\AbstractAlbum;
+use App\Http\Requests\Album\AddAlbumRequest;
+use App\Models\Album;
+use App\Policies\AlbumPolicy;
+use App\Rules\RandomIDRule;
+use App\Rules\TitleRule;
+use Illuminate\Support\Facades\Gate;
+use Tests\AbstractTestCase;
+
+class AddAlbumRequestTest extends AbstractTestCase
+{
+	public function testAuthorization()
+	{
+		$album = Album::factory()->create();
+		Gate::shouldReceive('check')
+			->with(AlbumPolicy::CAN_EDIT, [AbstractAlbum::class, null])
+			->andReturn(true);
+
+		$request = new AddAlbumRequest();
+		$request->merge([RequestAttribute::PARENT_ID_ATTRIBUTE => $album->id]);
+
+		$this->assertTrue($request->authorize());
+	}
+
+	public function testRules(): void
+	{
+		$request = new AddAlbumRequest();
+
+		$rules = $request->rules();
+
+		$this->assertArrayHasKey(RequestAttribute::PARENT_ID_ATTRIBUTE, $rules);
+		$this->assertArrayHasKey(RequestAttribute::TITLE_ATTRIBUTE, $rules);
+
+		$this->assertEquals(['present', new RandomIDRule(true)], $rules[RequestAttribute::PARENT_ID_ATTRIBUTE]);
+		$this->assertEquals(['required', new TitleRule()], $rules[RequestAttribute::TITLE_ATTRIBUTE]);
+	}
+}
