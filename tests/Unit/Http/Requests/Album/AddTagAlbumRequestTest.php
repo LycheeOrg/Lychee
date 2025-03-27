f417a36393bc1f8fc189ecaaf6d5a8d04a5b3ee0
Shobi <ppshobi@gmail.com>
2025-03-28 00:11:44 +0100

    test: mock VerifyInterface to not mix up with the db&tests unnecessarily

--- tests/Unit/Http/Requests/Album/AddTagAlbumRequestTest.php
+++ tests/Unit/Http/Requests/Album/AddTagAlbumRequestTest.php
@@ -7,23 +7,30 @@ namespace Tests\Unit\Http\Requests\Album;
 use App\Contracts\Http\Requests\RequestAttribute;
 use App\Contracts\Models\AbstractAlbum;
 use App\Http\Requests\Album\AddTagAlbumRequest;
-use App\Models\Album;
 use App\Policies\AlbumPolicy;
 use App\Rules\TitleRule;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Gate;
+use LycheeVerify\Contract\VerifyInterface;
 use Tests\AbstractTestCase;
 
 class AddTagAlbumRequestTest extends AbstractTestCase
 {
+	protected function setUp(): void
+	{
+		parent::setUp();
+		$mockVerify = $this->createMock(VerifyInterface::class);
+		App::instance(VerifyInterface::class, $mockVerify); // VerifyInterface is talking to DB & that is not needed for Request classes
+	}
+
 	public function testAuthorization()
 	{
-		$album = Album::factory()->create();
 		Gate::shouldReceive('check')
 			->with(AlbumPolicy::CAN_EDIT, [AbstractAlbum::class, null])
 			->andReturn(true);
 
 		$request = new AddTagAlbumRequest();
-		$request->merge([RequestAttribute::PARENT_ID_ATTRIBUTE => $album->id]);
+		$request->merge([RequestAttribute::PARENT_ID_ATTRIBUTE => null]);
 
 		$this->assertTrue($request->authorize());
 	}
