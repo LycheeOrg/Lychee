d9fc5dd5387040368bbfdbd42c8182216e4a945a
Shobi <ppshobi@gmail.com>
2025-04-11 11:53:34 +0200

    more http tests

---
+++ tests/Unit/Http/Requests/Album/UpdateTagAlbumRequestTest.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Http\Requests\Album;
+
+use App\Contracts\Http\Requests\RequestAttribute;
+use App\Contracts\Models\AbstractAlbum;
+use App\Enum\ColumnSortingPhotoType;
+use App\Enum\OrderSortingType;
+use App\Enum\PhotoLayoutType;
+use App\Enum\TimelinePhotoGranularity;
+use App\Factories\AlbumFactory;
+use App\Http\Requests\Album\UpdateTagAlbumRequest;
+use App\Models\TagAlbum;
+use App\Policies\AlbumPolicy;
+use App\Rules\CopyrightRule;
+use App\Rules\DescriptionRule;
+use App\Rules\EnumRequireSupportRule;
+use App\Rules\RandomIDRule;
+use App\Rules\TitleRule;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rules\Enum;
+use LycheeVerify\Contract\VerifyInterface;
+use LycheeVerify\Verify;
+use Tests\AbstractTestCase;
+
+class UpdateTagAlbumRequestTest extends AbstractTestCase
+{
+	protected function setUp(): void
+	{
+		parent::setUp();
+		$mockVerify = $this->createMock(VerifyInterface::class);
+		$mockVerify->expects($this->any())
+			->method('is_supporter')
+			->willReturn(true);
+		$this->mockVerify = $mockVerify;
+		App::instance(Verify::class, $mockVerify); // VerifyInterface is talking to DB & that is not needed for Request classes
+	}
+
+	public function testAuthorization()
+	{
+		$tagalbumMock = $this->createMock(TagAlbum::class);
+
+		Gate::shouldReceive('check')
+			->with(AlbumPolicy::CAN_EDIT, [AbstractAlbum::class, $tagalbumMock])
+			->andReturn(true);
+
+		$mockAlbumFactory = $this->createMock(AlbumFactory::class);
+		$mockAlbumFactory->method('findBaseAlbumOrFail')
+			->willReturn($tagalbumMock);
+		$this->app->instance(AlbumFactory::class, $mockAlbumFactory);
+
+		$request = new UpdateTagAlbumRequest();
+		$request->setContainer($this->app);
+		$request->setRedirector($this->app['redirect']);
+
+		$request->merge([
+			RequestAttribute::ALBUM_ID_ATTRIBUTE => 'ePN3Y_kA16KtZGXmxv-kdBrg',
+			RequestAttribute::TITLE_ATTRIBUTE => 'Test Title',
+			RequestAttribute::DESCRIPTION_ATTRIBUTE => 'example description',
+			RequestAttribute::PHOTO_SORTING_COLUMN_ATTRIBUTE => ColumnSortingPhotoType::TAKEN_AT->value,
+			RequestAttribute::PHOTO_SORTING_ORDER_ATTRIBUTE => OrderSortingType::ASC->value,
+			RequestAttribute::ALBUM_PHOTO_LAYOUT => PhotoLayoutType::JUSTIFIED->value,
+			RequestAttribute::ALBUM_TIMELINE_PHOTO => TimelinePhotoGranularity::DEFAULT->value,
+			RequestAttribute::COPYRIGHT_ATTRIBUTE => 'Copyright (c) 2017-2018 Tobias Reich',
+			RequestAttribute::TAGS_ATTRIBUTE => ['tag1', 'tag2'],
+		]);
+
+		$request->validateResolved(); // hydrate the request Class with the data before authorizing . Fighting the framework a bit
+
+		$this->assertTrue($request->authorize());
+	}
+
+	public function testRules(): void
+	{
+		$request = new UpdateTagAlbumRequest();
+
+		$rules = $request->rules();
+
+		$expectedRuleMap = [
+			RequestAttribute::ALBUM_ID_ATTRIBUTE => ['required', new RandomIDRule(false)],
+			RequestAttribute::TITLE_ATTRIBUTE => ['required', new TitleRule()],
+			RequestAttribute::DESCRIPTION_ATTRIBUTE => ['present', new DescriptionRule()],
+			RequestAttribute::PHOTO_SORTING_COLUMN_ATTRIBUTE => ['present', 'nullable', new Enum(ColumnSortingPhotoType::class)],
+			RequestAttribute::PHOTO_SORTING_ORDER_ATTRIBUTE => [
+				'required_with:' . RequestAttribute::PHOTO_SORTING_COLUMN_ATTRIBUTE,
+				'nullable', new Enum(OrderSortingType::class),
+			],
+			RequestAttribute::TAGS_ATTRIBUTE => 'required|array|min:1',
+			RequestAttribute::TAGS_ATTRIBUTE . '.*' => 'required|string|min:1',
+			RequestAttribute::COPYRIGHT_ATTRIBUTE => ['present', 'nullable', new CopyrightRule()],
+			RequestAttribute::ALBUM_PHOTO_LAYOUT => ['present', 'nullable', new Enum(PhotoLayoutType::class)],
+			RequestAttribute::ALBUM_TIMELINE_PHOTO => ['present', 'nullable', new Enum(TimelinePhotoGranularity::class), new EnumRequireSupportRule(TimelinePhotoGranularity::class, [TimelinePhotoGranularity::DEFAULT, TimelinePhotoGranularity::DISABLED], $this->mockVerify)],
+		];
+		$this->assertCount(count($expectedRuleMap), $rules); // only validating the first 7 rules & the GRANTS_UPLOAD_ATTRIBUTE is tested afterwards
+
+		foreach ($expectedRuleMap as $key => $value) {
+			$this->assertEquals($value, $rules[$key]);
+		}
+	}
+}
