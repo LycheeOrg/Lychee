7919b186b739121a163bb5cdccd216ddcb6a18e7
ildyria <beviguier@gmail.com>
2025-04-13 20:46:20 +0200

    improve "coverage"

--- app/Events/Metrics/BaseMetricsEvent.php
+++ app/Events/Metrics/BaseMetricsEvent.php
@@ -23,7 +23,21 @@ abstract class BaseMetricsEvent
 	) {
 	}
 
+	/**
+	 * Return the type of key : photo_id or album_id.
+	 *
+	 * @return string
+	 *
+	 * @codeCoverageIgnore (abstract method can't be covered)
+	 */
 	abstract public function key(): string;
 
+	/**
+	 * Return the column name in the database to update.
+	 *
+	 * @return MetricsAction
+	 *
+	 * @codeCoverageIgnore (abstract method can't be covered)
+	 */
 	abstract public function metricAction(): MetricsAction;
 }
