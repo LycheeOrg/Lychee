4dee1c0ebd3dd4935fb9f81c19794dca84ada8f4
ildyria <beviguier@gmail.com>
2025-04-21 17:09:34 +0200

    version 6.5.1 + force cache clear

---
+++ database/migrations/2025_04_21_150310_bump_version060501.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\ConsoleSectionOutput;
+
+return new class() extends Migration {
+	private ConsoleOutput $output;
+	private ConsoleSectionOutput $msg_section;
+
+	/**
+	 * @throws DBALException
+	 */
+	public function __construct()
+	{
+		$this->output = new ConsoleOutput();
+		$this->msg_section = $this->output->section();
+	}
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(): void
+	{
+		DB::table('configs')->where('key', 'version')->update(['value' => '060501']);
+		Artisan::call('cache:clear');
+		$this->msg_section->writeln('<info>Info:</info> Cleared cache for version 060501');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(): void
+	{
+		DB::table('configs')->where('key', 'version')->update(['value' => '060500']);
+	}
+};
