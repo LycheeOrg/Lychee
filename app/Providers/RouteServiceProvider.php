710fb211f900473e3161fe80c8fbe1d5d338c967
ildyria <beviguier@gmail.com>
2025-04-26 22:21:33 +0200

    remove legacy v4

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,7 +8,6 @@
 
 namespace App\Providers;
 
-use App\Assets\Features;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -47,30 +46,10 @@ class RouteServiceProvider extends ServiceProvider
 		// Note: `web.php` must be registered last, because it contains a
 		// "catch all" route and the routes are considered in a "first match"
 		// fashion.
-		$this->routes(function (): void {
-			Features::when('vuejs', fn () => $this->getLycheeV6Routes(), fn () => $this->getLegacyRoutes());
-		});
-	}
-
-	private function getLycheeV6Routes(): void
-	{
 		Route::middleware('web-admin')->group(base_path('routes/web-admin-v2.php'));
 		Route::middleware('api')->prefix('api/v2')->group(base_path('routes/api_v2.php'));
 		Route::middleware('web-install')->group(base_path('routes/web-install.php'));
-
-		if (Features::active('legacy_api')) {
-			Route::middleware('api')->prefix('api')->group(base_path('routes/api_v1.php'));
-		}
-
 		Route::middleware('web')->group(base_path('routes/web_v2.php'));
-	}
-
-	private function getLegacyRoutes(): void
-	{
-		Route::middleware('web-install')->group(base_path('routes/web-install.php'));
-		Route::middleware('api')->prefix('api')->group(base_path('routes/api_v1.php'));
-		Route::middleware('web-admin')->group(base_path('routes/web-admin-v1.php'));
-		Route::middleware('web')->group(base_path('routes/web_v1.php'));
 	}
 
 	/**
