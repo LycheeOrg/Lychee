b67ce55c84787420f9dcfd22f22471e30e5e7735
ildyria <beviguier@gmail.com>
2025-04-26 22:54:13 +0200

    All the code from electerious is gone as 100% of the application has been rewriten

--- app/Events/Metrics/PhotoShared.php
+++ app/Events/Metrics/PhotoShared.php
@@ -2,7 +2,6 @@
 
 /**
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
 
--- scripts/gen_release.php
+++ scripts/gen_release.php
@@ -3,7 +3,6 @@
 
 /**
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
 
@@ -22,7 +21,6 @@ $template = "<?php
 
 /**
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
 
