e37ee67fc3d4d8c5461afa793b4518835be5b4df
ildyria <beviguier@gmail.com>
2025-04-26 22:34:58 +0200

    cleaning up

--- app/Actions/Diagnostics/Pipes/Infos/InstallTypeInfo.php
+++ app/Actions/Diagnostics/Pipes/Infos/InstallTypeInfo.php
@@ -37,7 +37,6 @@ class InstallTypeInfo implements DiagnosticStringPipe
 		$data[] = Diagnostics::line('APP_URL:', config('app.url') !== 'http://localhost' ? 'set' : 'default'); // Some people leave that value by default... It is now breaking their visual.
 		$data[] = Diagnostics::line('APP_DIR:', config('app.dir_url') !== '' ? 'set' : 'default'); // Some people leave that value by default... It is now breaking their visual.
 		$data[] = Diagnostics::line('LOG_VIEWER_ENABLED:', Features::when('log-viewer', 'true', 'false'));
-		$data[] = Diagnostics::line('VUEJS_ENABLED:', Features::when('vuejs', 'true', 'false'));
 		$data[] = '';
 
 		return $next($data);
--- tests/LoadedSubscriber.php
+++ tests/LoadedSubscriber.php
@@ -9,12 +9,8 @@
 namespace Tests;
 
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Schema;
 use PHPUnit\Event\TestSuite\Loaded;
 use PHPUnit\Event\TestSuite\LoadedSubscriber as LoadedSubscriberInterface;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
 final class LoadedSubscriber implements LoadedSubscriberInterface
 {
@@ -26,47 +22,11 @@ final class LoadedSubscriber implements LoadedSubscriberInterface
 		$this->createApplication();
 		$this->migrateApplication();
 
-		// Detect local run base on the phpunit.xml file
-		if (str_contains($event->testSuite()->name(), 'phpunit.xml') &&
-			str_contains($event->testSuite()->tests()->asArray()[0]->file(), 'Feature_v1') &&
-			config('features.vuejs')) {
-			(new ConsoleOutput())->section()->writeln('<error>Wrong configuration:</error> <options=bold>Tests v1 are not compatible with VUEJS_ENABLED=true.</>');
-			exit(1);
+		// If there are any users in the DB, this tends to crash some tests (because we check exact count of users).
+		if (User::query()->count() > 0) {
+			User::truncate();
 		}
 
-		if (config('features.vuejs')) {
-			// If there are any users in the DB, this tends to crash some tests (because we check exact count of users).
-			if (User::query()->count() > 0) {
-				User::truncate();
-			}
-
-			return;
-		}
-
-		/** @var User|null $admin */
-		$admin = User::find(1);
-		if ($admin === null) {
-			$admin = new User();
-			$admin->incrementing = false;
-			$admin->id = 1;
-			$admin->may_upload = true;
-			$admin->may_edit_own_settings = true;
-			$admin->may_administrate = true;
-			$admin->username = 'admin';
-			$admin->password = Hash::make('password');
-			$admin->save();
-
-			if (Schema::connection(null)->getConnection()->getDriverName() === 'pgsql' && DB::table('users')->count() > 0) {
-				// when using PostgreSQL, the next ID value is kept when inserting without incrementing
-				// which results in errors because trying to insert a user with ID = 1.
-				// Thus, we need to reset the index to the greatest ID + 1
-				/** @var User $lastUser */
-				$lastUser = User::query()->orderByDesc('id')->first();
-				DB::statement('ALTER SEQUENCE users_id_seq1 RESTART WITH ' . strval($lastUser->id + 1));
-			}
-		} elseif (!$admin->may_administrate) {
-			$admin->may_administrate = true;
-			$admin->save();
-		}
+		return;
 	}
 }
