a49bbc5ae1e28deada76cbc7312ffccfa71e013a
ildyria <beviguier@gmail.com>
2025-04-27 17:41:07 +0200

    fix broken stats

---
+++ database/migrations/2025_04_27_153213_fix-stats-missing.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $ids = DB::table('photos')->leftJoin('statistics', 'photos.id', '=', 'statistics.photo_id')
+            ->whereNull('statistics.photo_id')->select('photos.id')->pluck('id');
+
+        if ($ids->isEmpty()) {
+            return;
+        }
+
+        DB::table('statistics')->insert($ids->map(fn ($id) => ['photo_id' => $id])->all());
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Nothing to do here.
+    }
+};
