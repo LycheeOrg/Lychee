28a08c0ee002a34e640dafd5e0d143121f327ddd
Novapixel1010 <msocial@brwfamily.com>
2025-04-28 15:00:50 -0600

    propose revising the footer text ru

--- lang/ru/landing.php
+++ lang/ru/landing.php
@@ -14,6 +14,6 @@ return [
 	*/
 	'gallery' => 'Галерея',
 	'access_gallery' => 'Доступ к галерее',
-	'hosted_with_lychee' => 'Размещено с Lychee',
+	'Powered_by_lychee' => 'Размещено с Lychee',
 	'copyright' => 'Все изображения на этом сайте защищены авторским правом %1$s © %2$s',
 ];
