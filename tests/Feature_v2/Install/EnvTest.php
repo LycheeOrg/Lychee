8c850e9f5e1ad0016df1aaf06d8d650861a7d64e
ildyria <beviguier@gmail.com>
2025-04-28 19:06:33 +0200

    add more tests on install

---
+++ tests/Feature_v2/Install/EnvTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+/**
+ * We don't care for unhandled exceptions in tests.
+ * It is the nature of a test to throw an exception.
+ * Without this suppression we had 100+ Linter warning in this file which
+ * don't help anything.
+ *
+ * @noinspection PhpDocMissingThrowsInspection
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+
+namespace Tests\Feature_v2\Install;
+
+use App\Http\Middleware\InstallationStatus;
+use Tests\Feature_v2\Base\BaseApiV2Test;
+
+class EnvTest extends BaseApiV2Test
+{
+	public function testGet(): void
+	{
+		$response = $this->get('install/env');
+		$this->assertForbidden($response);
+
+		$response = $this->withoutMiddleware(InstallationStatus::class)->get('install/env');
+		$this->assertOk($response);
+	}
+
+	public function testPost(): void
+	{
+		$response = $this->post('install/env');
+		$this->assertForbidden($response);
+
+		$response = $this->withoutMiddleware(InstallationStatus::class)->get('install/env');
+		$this->assertOk($response);
+	}
+}
