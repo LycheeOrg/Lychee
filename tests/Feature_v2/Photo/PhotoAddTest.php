7519d76f9ed9e27b4c6929f150dd35be6f639618
ildyria <beviguier@gmail.com>
2025-04-29 19:54:36 +0200

    more coverage

--- tests/Feature_v2/Photo/PhotoAddTest.php
+++ tests/Feature_v2/Photo/PhotoAddTest.php
@@ -76,4 +76,71 @@ class PhotoAddTest extends BaseApiWithDataTest
 		$this->assertOk($response);
 		$response->assertJsonCount(1, 'resource.photos');
 	}
+
+	public function testDuplicate(): void
+	{
+		$this->catchFailureSilence = [];
+		$response = $this->actingAs($this->admin)->upload('Photo', filename: TestConstants::SAMPLE_FILE_NIGHT_IMAGE);
+		$this->assertCreated($response);
+
+		$this->clearCachedSmartAlbums();
+		$response = $this->getJsonWithData('Album', ['album_id' => 'unsorted']);
+		$this->assertOk($response);
+		$id1 = $response->json('resource.photos.0.id');
+
+		$response = $this->actingAs($this->admin)->upload('Photo', filename: TestConstants::SAMPLE_FILE_NIGHT_IMAGE);
+		$this->assertCreated($response);
+
+		$this->clearCachedSmartAlbums();
+		$response = $this->getJsonWithData('Album', ['album_id' => 'unsorted']);
+		$this->assertOk($response);
+		$id2 = $response->json('resource.photos.1.id');
+
+		self::assertNotEquals($id1, $id2);
+
+		$response = $this->deleteJson('Photo', ['photo_ids' => [$id1, $id2]]);
+		$this->assertNoContent($response);
+
+		$this->catchFailureSilence = ["App\Exceptions\MediaFileOperationException"];
+	}
+
+	public function testAppleLivePhoto1(): void
+	{
+		$this->catchFailureSilence = [];
+		$response = $this->actingAs($this->admin)->upload('Photo', filename: TestConstants::SAMPLE_FILE_TRAIN_IMAGE);
+		$this->assertCreated($response);
+		$response = $this->actingAs($this->admin)->upload('Photo', filename: TestConstants::SAMPLE_FILE_TRAIN_VIDEO);
+		$this->assertCreated($response);
+
+		$this->clearCachedSmartAlbums();
+		$response = $this->getJsonWithData('Album', ['album_id' => 'unsorted']);
+		$this->assertOk($response);
+		$photo = $response->json('resource.photos.0');
+
+		self::assertEquals('E905E6C6-C747-4805-942F-9904A0281F02', $photo['live_photo_content_id']);
+
+		self::assertStringEndsWith('.mov', $photo['live_photo_url']);
+		self::assertEquals(pathinfo($photo['live_photo_url'], PATHINFO_DIRNAME), pathinfo($photo['size_variants']['original']['url'], PATHINFO_DIRNAME));
+		self::assertEquals(pathinfo($photo['live_photo_url'], PATHINFO_FILENAME), pathinfo($photo['size_variants']['original']['url'], PATHINFO_FILENAME));
+	}
+
+	public function testAppleLivePhoto2(): void
+	{
+		$this->catchFailureSilence = [];
+		$response = $this->actingAs($this->admin)->upload('Photo', filename: TestConstants::SAMPLE_FILE_TRAIN_VIDEO);
+		$this->assertCreated($response);
+		$response = $this->actingAs($this->admin)->upload('Photo', filename: TestConstants::SAMPLE_FILE_TRAIN_IMAGE);
+		$this->assertCreated($response);
+
+		$this->clearCachedSmartAlbums();
+		$response = $this->getJsonWithData('Album', ['album_id' => 'unsorted']);
+		$this->assertOk($response);
+		$photo = $response->json('resource.photos.0');
+
+		self::assertEquals('E905E6C6-C747-4805-942F-9904A0281F02', $photo['live_photo_content_id']);
+
+		self::assertStringEndsWith('.mov', $photo['live_photo_url']);
+		self::assertEquals(pathinfo($photo['live_photo_url'], PATHINFO_DIRNAME), pathinfo($photo['size_variants']['original']['url'], PATHINFO_DIRNAME));
+		self::assertEquals(pathinfo($photo['live_photo_url'], PATHINFO_FILENAME), pathinfo($photo['size_variants']['original']['url'], PATHINFO_FILENAME));
+	}
 }
