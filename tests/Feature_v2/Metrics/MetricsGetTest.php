d8a39ab8c563fb3c77909222fdc282c54192afa0
ildyria <beviguier@gmail.com>
2025-05-01 14:21:33 +0200

    fix tests

--- tests/Feature_v2/Metrics/MetricsGetTest.php
+++ tests/Feature_v2/Metrics/MetricsGetTest.php
@@ -21,9 +21,9 @@ namespace Tests\Feature_v2\Metrics;
 use App\Enum\LiveMetricsAccess;
 use App\Models\Configs;
 use App\Models\LiveMetrics;
-use Tests\Feature_v2\Base\BaseApiV2Test;
+use Tests\Feature_v2\Base\BaseApiWithDataTest;
 
-class MetricsGetTest extends BaseApiV2Test
+class MetricsGetTest extends BaseApiWithDataTest
 {
 	public function tearDown(): void
 	{
