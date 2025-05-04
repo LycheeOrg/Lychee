e9fd28fe037deb253530609fe13cdb237a7d0a23
ildyria <beviguier@gmail.com>
2025-05-04 19:05:28 +0200

    WIP

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -34,6 +34,7 @@ use App\Models\Extensions\UTCBasedTimes;
 use App\Relations\HasManySizeVariants;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
