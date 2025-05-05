c39b433a70907b8904e5c061af920fc44a31681e
ildyria <benoit@viguier.nl>
2025-05-05 20:27:16 +0000

    Translated using Weblate (French)
    Currently translated at 86.6% (13 of 15 strings)
    
    Translation: Lychee/left-menu
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/left-menu/fr/

--- lang/fr/left-menu.php
+++ lang/fr/left-menu.php
@@ -1,33 +1,28 @@
 <?php
-
-/**
+return [
+    /**
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
-
-return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Left Menu
 	|--------------------------------------------------------------------------
 	*/
-	'back_to_gallery' => 'Retour à la galerie',
-
-	'login' => 'Login',
-	'frame' => 'Frame',
-	'map' => 'Map',
-	'admin' => 'Administration',
-	'clockwork' => 'Application Clockwork',
-	'logs' => 'Afficher les journaux',
-	'jobs' => 'Afficher l’historique des tâches',
-	'user' => 'Utilisateur',
-
-	'sign_out' => 'Déconnexion',
-
-	'about' => 'À propos',
-	'changelog' => 'Changelog',
-	'api' => 'Documentation API',
-	'source_code' => 'Code source',
-	'support' => 'Support',
+    'back_to_gallery' => 'Retour à la galerie',
+    'login' => 'Login',
+    'frame' => 'Cadre',
+    'map' => 'Carte',
+    'admin' => 'Administration',
+    'clockwork' => 'Application Clockwork',
+    'logs' => 'Afficher les journaux',
+    'jobs' => 'Afficher l’historique des tâches',
+    'user' => 'Utilisateur',
+    'sign_out' => 'Déconnexion',
+    'about' => 'À propos',
+    'changelog' => 'Historique des changements',
+    'api' => 'Documentation API',
+    'source_code' => 'Code source',
+    'support' => 'Support',
 ];
