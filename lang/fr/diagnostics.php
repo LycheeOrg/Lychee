650473010ff4d05d15249b58dfea71918f56e3cf
ildyria <benoit@viguier.nl>
2025-05-05 14:28:57 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: Lychee/diagnostics
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/diagnostics/fr/

--- lang/fr/diagnostics.php
+++ lang/fr/diagnostics.php
@@ -1,30 +1,26 @@
 <?php
-
-/**
+return [
+    /**
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
-
-return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Diagnostics page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Diagnostics',
-
-	'copy_to_clipboard' => 'Copier les diagnostics dans le presse-papiers',
-	'self-diagnosis' => 'Auto-diagnostic',
-	'info' => 'Informations',
-	'space' => 'Espace',
-	'load_space' => 'Charger l’utilisation de l’espace disque.',
-	'configuration' => 'Configuration',
-	'loading' => 'Chargement...',
-	'identical_content' => 'Contenu identique',
-
-	'toast' => [
-		'info' => 'Informations',
-		'copy' => 'Diagnostics copiés dans le presse-papiers !',
-	],
+    'title' => 'Diagnostics',
+    'copy_to_clipboard' => 'Copier les diagnostics dans le presse-papiers',
+    'self-diagnosis' => 'Auto-diagnostic',
+    'info' => 'Informations',
+    'space' => 'Espace',
+    'load_space' => 'Charger l’utilisation de l’espace disque.',
+    'configuration' => 'Configuration',
+    'loading' => 'Chargement...',
+    'identical_content' => 'Contenu identique',
+    'toast' => [
+        'info' => 'Informations',
+        'copy' => 'Diagnostics copiés dans le presse-papiers !',
+    ],
 ];
