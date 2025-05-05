2b610b4c81b2ac1ae0827cd5f5a3bb42619d1a3c
ildyria <benoit@viguier.nl>
2025-05-05 20:26:38 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Lychee/jobs
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/jobs/fr/

--- lang/fr/jobs.php
+++ lang/fr/jobs.php
@@ -1,23 +1,19 @@
 <?php
-
-/**
+return [
+    /**
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
-
-return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Jobs page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Tâches',
-
-	'no_data' => 'Aucune tâche n’a encore été exécutée.',
-
-	'ready' => 'Ready',
-	'success' => 'Success',
-	'failure' => 'Failure',
-	'started' => 'Started',
+    'title' => 'Tâches',
+    'no_data' => 'Aucune tâche n’a encore été exécutée.',
+    'ready' => 'Pret',
+    'success' => 'Succès',
+    'failure' => 'Erreur',
+    'started' => 'En cours',
 ];
