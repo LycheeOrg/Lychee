9fcc4f600505ef8724962a74cbcc5a9067dc0096
ildyria <benoit@viguier.nl>
2025-05-05 14:49:15 +0000

    Translated using Weblate (French)
    Currently translated at 92.3% (12 of 13 strings)
    
    Translation: Lychee/duplicate-finder
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/duplicate-finder/fr/

--- lang/fr/duplicate-finder.php
+++ lang/fr/duplicate-finder.php
@@ -1,35 +1,30 @@
 <?php
-
-/**
+return [
+    /**
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
-
-return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Duplicate Finder Page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Maintenance',
-	'intro' => 'Sur cette page, vous trouverez les photos en double détectées dans votre base de données.',
-	'found' => ' doublons trouvés !',
-	'invalid-search' => ' Au moins une des conditions "checksum" ou "titre" doit être cochée.',
-	'checksum-must-match' => 'Le checksum doit correspondre.',
-	'title-must-match' => 'Le titre doit correspondre.',
-	'must-be-in-same-album' => 'Doivent appartenir au même album.',
-
-	'columns' => [
-		'album' => 'Album',
-		'photo' => 'Photo',
-		'checksum' => 'Checksum',
-	],
-
-	'warning' => [
-		'no-original-left' => 'Aucun original restant.',
-		'keep-one' => 'Vous avez sélectionné tous les doublons de ce groupe. Veuillez en conserver au moins un.',
-	],
-
-	'delete-selected' => 'Supprimer la sélection',
+    'title' => 'Maintenance',
+    'intro' => 'Sur cette page, vous trouverez les photos en double détectées dans votre base de données.',
+    'found' => ' doublons trouvés !',
+    'invalid-search' => ' Au moins une des conditions "checksum" ou "titre" doit être cochée.',
+    'checksum-must-match' => 'Le checksum doit correspondre.',
+    'title-must-match' => 'Le titre doit correspondre.',
+    'must-be-in-same-album' => 'Doivent appartenir au même album.',
+    'columns' => [
+        'album' => 'Album',
+        'photo' => 'Photo',
+        'checksum' => 'Checksum',
+    ],
+    'warning' => [
+        'no-original-left' => 'Aucun original restant.',
+        'keep-one' => 'Vous avez sélectionné tous les doublons de ce groupe. Veuillez en conserver au moins un.',
+    ],
+    'delete-selected' => 'Supprimer la sélection',
 ];
