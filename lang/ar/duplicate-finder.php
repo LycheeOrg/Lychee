2b76c6e94d30441cd16bdc583f239f60e4c993ed
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:16 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/duplicate-finder.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Duplicate Finder Page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'intro' => '',
+    'found' => '',
+    'invalid-search' => '',
+    'checksum-must-match' => '',
+    'title-must-match' => '',
+    'must-be-in-same-album' => '',
+    'columns' => [
+        'album' => '',
+        'photo' => '',
+        'checksum' => '',
+    ],
+    'warning' => [
+        'no-original-left' => '',
+        'keep-one' => '',
+    ],
+    'delete-selected' => '',
+];
