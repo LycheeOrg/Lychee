778087cbe9d723354359c4f6fc3799d7fcc0babc
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:20 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/profile.php
@@ -0,0 +1,57 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Profile page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'login' => [
+        'header' => '',
+        'enter_current_password' => '',
+        'current_password' => '',
+        'credentials_update' => '',
+        'username' => '',
+        'new_password' => '',
+        'confirm_new_password' => '',
+        'email_instruction' => '',
+        'email' => '',
+        'change' => '',
+        'api_token' => '',
+        'missing_fields' => '',
+    ],
+    'token' => [
+        'unavailable' => '',
+        'no_data' => '',
+        'disable' => '',
+        'disabled' => '',
+        'warning' => '',
+        'reset' => '',
+        'create' => '',
+    ],
+    'oauth' => [
+        'header' => '',
+        'header_not_available' => '',
+        'setup_env' => '',
+        'token_registered' => '',
+        'setup' => '',
+        'reset' => '',
+        'credential_deleted' => '',
+    ],
+    'u2f' => [
+        'header' => '',
+        'info' => '',
+        'empty' => '',
+        'not_secure' => '',
+        'new' => '',
+        'credential_deleted' => '',
+        'credential_updated' => '',
+        'credential_registred' => '',
+        '5_chars' => '',
+    ],
+];
