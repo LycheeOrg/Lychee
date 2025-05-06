38f94a3ac9263d876262f5f225e237de58401f55
ildyria <benoit@viguier.nl>
2025-05-06 09:21:59 +0000

    Translated using Weblate (Polish)
    Currently translated at 72.7% (8 of 11 strings)
    
    Translation: Lychee/diagnostics
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/diagnostics/pl/

--- lang/pl/diagnostics.php
+++ lang/pl/diagnostics.php
@@ -1,30 +1,26 @@
 <?php
-
-/**
+return [
+    /**
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
-
-return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Diagnostics page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Diagnostyka',
-
-	'copy_to_clipboard' => 'Kopiuj diagnostykę do schowka',
-	'self-diagnosis' => 'Auto-diagnoza',
-	'info' => 'Info',
-	'space' => 'Przestrzeń',
-	'load_space' => 'Wykorzystanie przestrzeni',
-	'configuration' => 'Konfiguracja',
-	'loading' => 'Ładowanie...',
-	'identical_content' => 'Identyczna zawartość',
-
-	'toast' => [
-		'info' => 'Info',
-		'copy' => 'Diagnostyka skopiowana do schowka !',
-	],
+    'title' => 'Diagnostyka',
+    'copy_to_clipboard' => 'Kopiuj diagnostykę do schowka',
+    'self-diagnosis' => 'Auto-diagnoza',
+    'info' => 'Info',
+    'space' => 'Przestrzeń',
+    'load_space' => 'Wykorzystanie przestrzeni.',
+    'configuration' => 'Konfiguracja',
+    'loading' => 'Ładowanie...',
+    'identical_content' => 'Identyczna zawartość',
+    'toast' => [
+        'info' => 'Info',
+        'copy' => 'Diagnostyka skopiowana do schowka !',
+    ],
 ];
