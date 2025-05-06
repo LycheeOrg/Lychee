60fcb0574cce3fbd3c98c6a6fad53a746b70b253
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:19 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/jobs.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Jobs page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'no_data' => '',
+    'ready' => '',
+    'success' => '',
+    'failure' => '',
+    'started' => '',
+];
