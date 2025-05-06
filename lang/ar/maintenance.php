00a2f5a163f8bb15f6fee06e1431a9aa3ed31df2
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:20 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/maintenance.php
@@ -0,0 +1,70 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Maintenance Page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'description' => '',
+    'cleaning' => [
+        'title' => '',
+        'result' => '',
+        'description' => '',
+        'button' => '',
+    ],
+    'duplicate-finder' => [
+        'title' => '',
+        'description' => '',
+        'duplicates-all' => '',
+        'duplicates-title' => '',
+        'duplicates-per-album' => '',
+        'show' => '',
+    ],
+    'fix-jobs' => [
+        'title' => '',
+        'description' => '',
+        'button' => '',
+    ],
+    'gen-sizevariants' => [
+        'title' => '',
+        'description' => '',
+        'button' => '',
+        'success' => '',
+    ],
+    'fill-filesize-sizevariants' => [
+        'title' => '',
+        'description' => '',
+        'button' => '',
+        'success' => '',
+    ],
+    'fix-tree' => [
+        'title' => '',
+        'Oddness' => '',
+        'Duplicates' => '',
+        'Wrong parents' => '',
+        'Missing parents' => '',
+        'button' => '',
+    ],
+    'optimize' => [
+        'title' => '',
+        'description' => '',
+        'button' => '',
+    ],
+    'update' => [
+        'title' => '',
+        'check-button' => '',
+        'update-button' => '',
+        'no-pending-updates' => '',
+    ],
+    'flush-cache' => [
+        'title' => '',
+        'description' => '',
+        'button' => '',
+    ],
+];
