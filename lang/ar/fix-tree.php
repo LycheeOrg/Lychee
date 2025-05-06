887d3bb87175871a601b82b9af861c1374f01254
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:17 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/fix-tree.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Fix-tree Page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'intro' => '',
+    'warning' => '',
+    'help' => [
+        'header' => '',
+        'hover' => '',
+        'left' => '',
+        'right' => '',
+        'convenience' => '',
+        'left-right-warn' => '',
+        'parent-marked' => '',
+        'slowness' => '',
+    ],
+    'buttons' => [
+        'reset' => '',
+        'check' => '',
+        'apply' => '',
+    ],
+    'table' => [
+        'title' => '',
+        'left' => '',
+        'right' => '',
+        'id' => '',
+        'parent' => '',
+    ],
+    'errors' => [
+        'invalid' => '',
+        'invalid_details' => '',
+        'invalid_left' => '',
+        'invalid_right' => '',
+        'invalid_left_right' => '',
+        'duplicate_left' => '',
+        'duplicate_right' => '',
+        'parent' => '',
+        'unknown' => '',
+    ],
+];
