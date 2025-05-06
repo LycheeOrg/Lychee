e84f1a195e0897f11032ce565807b2d081f46e11
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:19 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/left-menu.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Left Menu
+	|--------------------------------------------------------------------------
+	*/
+    'back_to_gallery' => '',
+    'login' => '',
+    'frame' => '',
+    'map' => '',
+    'admin' => '',
+    'clockwork' => '',
+    'logs' => '',
+    'jobs' => '',
+    'user' => '',
+    'sign_out' => '',
+    'about' => '',
+    'changelog' => '',
+    'api' => '',
+    'source_code' => '',
+    'support' => '',
+];
