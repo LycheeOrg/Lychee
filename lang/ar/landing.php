a0fbfca358b7a9ac342ef9a80cba9729874cf03a
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:19 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/landing.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Landing page
+	|--------------------------------------------------------------------------
+	*/
+    'gallery' => '',
+    'access_gallery' => '',
+    'Powered_by_Lychee' => '',
+    'copyright' => '',
+];
