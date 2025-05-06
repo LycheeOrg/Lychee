76138badfd346d07c898b4dbd9e4bb45d1874115
ildyria <benoit@viguier.nl>
2025-05-06 08:40:13 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/diagnostics.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Diagnostics page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'copy_to_clipboard' => '',
+    'self-diagnosis' => '',
+    'info' => '',
+    'space' => '',
+    'load_space' => '',
+    'configuration' => '',
+    'loading' => '',
+    'identical_content' => '',
+    'toast' => [
+        'info' => '',
+        'copy' => '',
+    ],
+];
