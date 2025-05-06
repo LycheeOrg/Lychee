2f073e2b92f937ff2f26ff5f9c80e02e50f8de1d
ildyria <benoit@viguier.nl>
2025-05-06 09:21:59 +0000

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 90.9% (10 of 11 strings)
    
    Translation: Lychee/diagnostics
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/diagnostics/zh_Hans/

--- lang/zh_CN/diagnostics.php
+++ lang/zh_CN/diagnostics.php
@@ -1,30 +1,26 @@
 <?php
-
-/**
+return [
+    /**
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2017-2018 Tobias Reich
  * Copyright (c) 2018-2025 LycheeOrg.
  */
-
-return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Diagnostics page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => '系统诊断',
-
-	'copy_to_clipboard' => '复制诊断信息到剪贴板',
-	'self-diagnosis' => '自我诊断',
-	'info' => '信息',
-	'space' => '空间',
-	'load_space' => '加载空间使用情况',
-	'configuration' => '配置',
-	'loading' => '加载中...',
-	'identical_content' => '重复内容',
-
-	'toast' => [
-		'info' => '提示',
-		'copy' => '诊断信息已复制到剪贴板！',
-	],
+    'title' => '系统诊断',
+    'copy_to_clipboard' => '复制诊断信息到剪贴板',
+    'self-diagnosis' => '自我诊断',
+    'info' => '信息',
+    'space' => '空间',
+    'load_space' => '加载空间使用情况',
+    'configuration' => '配置',
+    'loading' => '加载中...',
+    'identical_content' => '重复内容',
+    'toast' => [
+        'info' => '提示',
+        'copy' => '诊断信息已复制到剪贴板！',
+    ],
 ];
