116fa8a265352390a226aca389613acc877a7279
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:21 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/sharing.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Sharing page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'info' => '',
+    'album_title' => '',
+    'username' => '',
+    'no_data' => '',
+    'share' => '',
+    'add_new_access_permission' => '',
+    'permission_deleted' => '',
+    'permission_created' => '',
+    'propagate' => '',
+    'propagate_help' => '',
+    'propagate_default' => '',
+    'propagate_overwrite' => '',
+    'propagate_warning' => '',
+    'permission_overwritten' => '',
+    'permission_updated' => '',
+    'bluk_share' => '',
+    'bulk_share_instr' => '',
+    'albums' => '',
+    'users' => '',
+    'no_users' => '',
+    'no_albums' => '',
+    'grants' => [
+        'read' => '',
+        'original' => '',
+        'download' => '',
+        'upload' => '',
+        'edit' => '',
+        'delete' => '',
+    ],
+];
