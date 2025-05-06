ea5e1e7228a9b76e2f333fb8737a43b9d1e57f7f
Languages add-on <noreply-addon-languages@weblate.org>
2025-05-06 08:40:22 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/users.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    /**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+    /*
+	|--------------------------------------------------------------------------
+	| Users page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => '',
+    'description' => '',
+    'create' => '',
+    'username' => '',
+    'password' => '',
+    'legend' => '',
+    'upload_rights' => '',
+    'edit_rights' => '',
+    'quota' => '',
+    'user_deleted' => '',
+    'user_created' => '',
+    'user_updated' => '',
+    'change_saved' => '',
+    'create_edit' => [
+        'upload_rights' => '',
+        'edit_rights' => '',
+        'quota' => '',
+        'quota_kb' => '',
+        'note' => '',
+        'create' => '',
+        'edit' => '',
+    ],
+    'line' => [
+        'admin' => '',
+        'edit' => '',
+        'delete' => '',
+    ],
+];
