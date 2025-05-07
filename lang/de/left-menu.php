76b6a59fbd0d3a14fcb240f21251a38df0197d85
Rayna.Butler <Dirk@2concept.de>
2025-05-07 10:35:40 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: Lychee/left-menu
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/left-menu/de/

--- lang/de/left-menu.php
+++ lang/de/left-menu.php
@@ -1,27 +1,23 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Left Menu
 	|--------------------------------------------------------------------------
 	*/
-	'back_to_gallery' => 'Back to Gallery',
-
-	'login' => 'Login',
-	'frame' => 'Frame',
-	'map' => 'Map',
-	'admin' => 'Admin',
-	'clockwork' => 'Clockwork App',
-	'logs' => 'Show Logs',
-	'jobs' => 'Show Job History',
-	'user' => 'User',
-
-	'sign_out' => 'Sign Out',
-
-	'about' => 'About',
-	'changelog' => 'Changelog',
-	'api' => 'API Documentation',
-	'source_code' => 'Source Code',
-	'support' => 'Support',
+    'back_to_gallery' => 'Zurück zur Galerie',
+    'login' => 'Anmelden',
+    'frame' => 'Bilderrahmen',
+    'map' => 'Karte',
+    'admin' => 'Admin',
+    'clockwork' => 'Clockwork App',
+    'logs' => 'Logs anzeigen',
+    'jobs' => 'Auftragsverlauf anzeigen',
+    'user' => 'Benutzer',
+    'sign_out' => 'Abmelden',
+    'about' => 'Über',
+    'changelog' => 'Changelog',
+    'api' => 'API Dokumentation',
+    'source_code' => 'Quellcode',
+    'support' => 'Support',
 ];
