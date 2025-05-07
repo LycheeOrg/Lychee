4ade1295f5766452ca3cd7259fbd0971163179e7
Rayna.Butler <Dirk@2concept.de>
2025-05-07 10:18:26 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Lychee/jobs
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/jobs/de/

--- lang/de/jobs.php
+++ lang/de/jobs.php
@@ -1,17 +1,14 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Jobs page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Jobs',
-
-	'no_data' => 'No Jobs have been executed yet.',
-
-	'ready' => 'Ready',
-	'success' => 'Success',
-	'failure' => 'Failure',
-	'started' => 'Started',
+    'title' => 'Aufträge',
+    'no_data' => 'Es wurden noch keine Aufträge ausgeführt.',
+    'ready' => 'Fertig',
+    'success' => 'Erfolgreich',
+    'failure' => 'Fehlgeschlagen',
+    'started' => 'Gestartet',
 ];
