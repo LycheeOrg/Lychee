4202d3191670349ac43fce8a7505ea2c8cc7bcaf
Rayna.Butler <Dirk@2concept.de>
2025-05-07 10:30:31 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: Lychee/diagnostics
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/diagnostics/de/

--- lang/de/diagnostics.php
+++ lang/de/diagnostics.php
@@ -1,24 +1,21 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Diagnostics page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Diagnostics',
-
-	'copy_to_clipboard' => 'Copy diagnostics to clipboard',
-	'self-diagnosis' => 'Self-diagnosis',
-	'info' => 'Info',
-	'space' => 'Space',
-	'load_space' => 'Load space usage.',
-	'configuration' => 'Configuration',
-	'loading' => 'Loading...',
-	'identical_content' => 'Identical content',
-
-	'toast' => [
-		'info' => 'Info',
-		'copy' => 'Diagnostics copied to clipboard!',
-	],
+    'title' => 'Diagnostik',
+    'copy_to_clipboard' => 'Diagnose in die Zwischenablage kopieren',
+    'self-diagnosis' => 'Selbst-Diagnose',
+    'info' => 'Info',
+    'space' => 'Speicherplatz',
+    'load_space' => 'Speicherplatzverbrauch laden.',
+    'configuration' => 'Konfiguration',
+    'loading' => 'Lade …',
+    'identical_content' => 'Identischer Inhalt',
+    'toast' => [
+        'info' => 'Info',
+        'copy' => 'Diagnosen in die Zwischenablage kopiert!',
+    ],
 ];
