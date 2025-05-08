312eb319da2dacc0003d2f162d24e16de5ada703
Benoît Viguier <ildyria@users.noreply.github.com>
2025-05-08 21:16:32 +0200

    Translations update from LycheeOrg - Weblate (#3327)
    * Translated using Weblate (Dutch)
    
    Currently translated at 27.2% (3 of 11 strings)
    
    Translation: Lychee/diagnostics
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/diagnostics/nl/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (154 of 154 strings)
    
    Translation: Lychee/dialogs
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/dialogs/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: Lychee/fix-tree
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/fix-tree/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (179 of 179 strings)
    
    Translation: Lychee/gallery
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/gallery/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (36 of 36 strings)
    
    Translation: Lychee/profile
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/profile/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (64 of 64 strings)
    
    Translation: Lychee/settings
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/settings/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (43 of 43 strings)
    
    Translation: Lychee/statistics
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/statistics/de/
    
    ---------
    
    Co-authored-by: Rayna.Butler <Dirk@2concept.de>

--- lang/de/statistics.php
+++ lang/de/statistics.php
@@ -1,64 +1,61 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Statistics page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Statistics',
-
-	'preview_text' => 'This is a preview of the statistics page available in Lychee <span class="text-primary-emphasis font-bold">SE</span>.<br />The data shown here are randomly generated and do not reflect your server.',
-	'no_data' => 'User does not have data on server.',
-	'collapse' => 'Collapse albums sizes',
-
-	'total' => [
-		'total' => 'Total',
-		'albums' => 'Albums',
-		'photos' => 'Photos',
-		'size' => 'Size',
-	],
-	'table' => [
-		'username' => 'Owner',
-		'title' => 'Title',
-		'photos' => 'Photos',
-		'descendants' => 'Children',
-		'size' => 'Size',
-	],
-	'punch_card' => [
-		'title' => 'Activity',
-		'photo-taken' => '%d photos taken',
-		'photo-taken-in' => '%d photos taken in %d',
-		'photo-uploaded' => '%d photos uploaded',
-		'photo-uploaded-in' => '%d photos uploaded in %d',
-		'with-exif' => 'with exif data',
-		'less' => 'Less',
-		'more' => 'More',
-		'tooltip' => '%d photos on %s',
-		'created_at' => 'Upload date',
-		'taken_at' => 'Exif date',
-		'caption' => 'Each column represents a week.',
-	],
-	'metrics' => [
-		'header' => 'Live metrics',
-		'a_visitor' => 'A visitor',
-		'visitors' => '%d visitors',
-		'visit_singular' => '%1$s viewed %2$s',
-		'favourite_singular' => '%1$s favourited %2$s',
-		'download_singular' => '%1$s downloaded %2$s',
-		'shared_singular' => '%1$s shared %2$s',
-		'visit_plural' => '%1$s viewed %2$s',
-		'favourite_plural' => '%1$s favourited %2$s',
-		'download_plural' => '%1$s downloaded %2$s',
-		'shared_plural' => '%1$s shared %2$s',
-		'ago' => [
-			'days' => '%d days ago',
-			'day' => 'a day ago',
-			'hours' => '%d hours ago',
-			'hour' => 'an hour ago',
-			'minutes' => '%d minutes ago',
-			'few_minutes' => 'a few minute ago',
-			'seconds' => 'a few seconds ago',
-		],
-	],
+    'title' => 'Statistiken',
+    'preview_text' => 'Dies ist eine Vorschau auf die Statistikseite in Lychee <span class="text-primary-emphasis font-bold">SE</span>.<br />Die hier gezeigten Daten sind zufällig generiert und spiegeln nicht Ihren Server wider.',
+    'no_data' => 'Der Benutzer hat keine Daten auf dem Server.',
+    'collapse' => 'Album Größen ausklappen',
+    'total' => [
+        'total' => 'Insgesamt',
+        'albums' => 'Alben',
+        'photos' => 'Fotos',
+        'size' => 'Größe',
+    ],
+    'table' => [
+        'username' => 'Besitzer',
+        'title' => 'Titel',
+        'photos' => 'Fotos',
+        'descendants' => 'Untergeordnete',
+        'size' => 'Größe',
+    ],
+    'punch_card' => [
+        'title' => 'Aktivität',
+        'photo-taken' => '%d Fotos gemacht',
+        'photo-taken-in' => '%d Fotos aufgenommen in %d',
+        'photo-uploaded' => '%d Fotos hochgeladen',
+        'photo-uploaded-in' => '%d Fotos hochgeladen in %d',
+        'with-exif' => 'mit Exif-Daten',
+        'less' => 'Weniger',
+        'more' => 'Mehr',
+        'tooltip' => '%d Fotos auf %s',
+        'created_at' => 'Datum des Hochladens',
+        'taken_at' => 'Exif-Datum',
+        'caption' => 'Jede Spalte steht für eine Woche.',
+    ],
+    'metrics' => [
+        'header' => 'Live-Metriken',
+        'a_visitor' => 'Ein Besucher',
+        'visitors' => '%d Besucher',
+        'visit_singular' => '%1$s angesehen %2$s',
+        'favourite_singular' => '%1$s favorisiert %2$s',
+        'download_singular' => '%1$s heruntergeladen %2$s',
+        'shared_singular' => '%1$s geteilt %2$s',
+        'visit_plural' => '%1$s angesehen %2$s',
+        'favourite_plural' => '%1$s favorisiert %2$s',
+        'download_plural' => '%1$s heruntergeladen %2$s',
+        'shared_plural' => '%1$s geteilt %2$s',
+        'ago' => [
+            'days' => 'vor %d Tagen',
+            'day' => 'vor einem Tag',
+            'hours' => 'vor %d Stunden',
+            'hour' => 'vor einer Stunde',
+            'minutes' => 'vor %d Minuten',
+            'few_minutes' => 'vor wenigen Minuten',
+            'seconds' => 'vor ein paar Sekunden',
+        ],
+    ],
 ];
--- lang/nl/diagnostics.php
+++ lang/nl/diagnostics.php
@@ -1,24 +1,21 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Diagnostics page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Diagnostics',
-
-	'copy_to_clipboard' => 'Copy diagnostics to clipboard',
-	'self-diagnosis' => 'Self-diagnosis',
-	'info' => 'Info',
-	'space' => 'Space',
-	'load_space' => 'Load space usage.',
-	'configuration' => 'Configuration',
-	'loading' => 'Loading...',
-	'identical_content' => 'Identical content',
-
-	'toast' => [
-		'info' => 'Info',
-		'copy' => 'Diagnostics copied to clipboard!',
-	],
+    'title' => 'Diagnostics',
+    'copy_to_clipboard' => 'Kopieer diagnostieken naar plakbord',
+    'self-diagnosis' => 'Zelfdiagnose',
+    'info' => 'Info',
+    'space' => 'Ruimte',
+    'load_space' => 'Load space usage.',
+    'configuration' => 'Configuration',
+    'loading' => 'Loading...',
+    'identical_content' => 'Identical content',
+    'toast' => [
+        'info' => 'Info',
+        'copy' => 'Diagnostics copied to clipboard!',
+    ],
 ];
