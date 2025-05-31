eecdf37d353444667c16ecafaeeea2594ef0f983
ildyria <benoit@viguier.nl>
2025-05-31 18:54:15 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: Lychee/left-menu
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/left-menu/nl/

--- lang/nl/left-menu.php
+++ lang/nl/left-menu.php
@@ -1,27 +1,23 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Left Menu
 	|--------------------------------------------------------------------------
 	*/
-	'back_to_gallery' => 'Terug naar galerij',
-
-	'login' => 'Inloggen',
-	'frame' => 'Frame',
-	'map' => 'Kaart',
-	'admin' => 'Beheer',
-	'clockwork' => 'Clockwork-app',
-	'logs' => 'Toon logs',
-	'jobs' => 'Toon taakgeschiedenis',
-	'user' => 'Gebruiker',
-
-	'sign_out' => 'Uitloggen',
-
-	'about' => 'Over',
-	'changelog' => 'Wijzigingslogboek',
-	'api' => 'API-documentatie',
-	'source_code' => 'Broncode',
-	'support' => 'Ondersteuning',
+    'back_to_gallery' => 'Terug naar galerij',
+    'login' => 'Inloggen',
+    'frame' => 'Frame',
+    'map' => 'Kaart',
+    'admin' => 'Beheer',
+    'clockwork' => 'Clockwork-app',
+    'logs' => 'Toon logs',
+    'jobs' => 'Toon taakgeschiedenis',
+    'user' => 'Gebruiker',
+    'sign_out' => 'Uitloggen',
+    'about' => 'Over',
+    'changelog' => 'Wijzigingslogboek',
+    'api' => 'API-documentatie',
+    'source_code' => 'Broncode',
+    'support' => 'Ondersteuning',
 ];
