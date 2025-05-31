c7a879646515571245e0415ec6e684fcf8139af6
ildyria <benoit@viguier.nl>
2025-05-31 18:54:11 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Lychee/landing
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/landing/nl/

--- lang/nl/landing.php
+++ lang/nl/landing.php
@@ -1,13 +1,12 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Landing page
 	|--------------------------------------------------------------------------
 	*/
-	'gallery' => 'Galerij',
-	'access_gallery' => 'Naar de galerij',
-	'Powered_by_Lychee' => 'Powered by Lychee',
-	'copyright' => 'Alle afbeeldingen op deze website zijn auteursrechtelijk beschermd door %1$s © %2$s',
+    'gallery' => 'Galerij',
+    'access_gallery' => 'Naar de galerij',
+    'Powered_by_Lychee' => 'Powered by Lychee',
+    'copyright' => 'Alle afbeeldingen op deze website zijn auteursrechtelijk beschermd door %1$s © %2$s',
 ];
