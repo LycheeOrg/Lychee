08eacdcdfd18f52f834a52247d609bed4ca767e5
ildyria <beviguier@gmail.com>
2025-06-03 20:11:51 +0200

    Translations for user groups

---
+++ lang/nl/user-groups.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Pagina gebruikersgroepen
+    |--------------------------------------------------------------------------
+    */
+    'title' => 'Gebruikersgroepen',
+    'explanation' => 'Gebruikersgroepen stellen je in staat om gebruikers te organiseren en machtigingen gezamenlijk te beheren.',
+
+    'create_group' => 'Groep aanmaken',
+    'edit' => 'Bewerken',
+    'delete' => 'Verwijderen',
+
+    'loading' => 'Groepen laden...',
+    'empty' => 'Geen groepen gevonden.',
+
+    'add_member' => 'Lid toevoegen...',
+    'empty_group' => 'Deze groep is leeg.',
+    'no_permission_members' => 'Je hebt geen toestemming om de leden van deze groep te zien.',
+    'remove_success' => '%s is verwijderd uit %s.',
+
+    'confirm_delete_header' => 'Groep verwijderen',
+    'confirm_delete_message' => 'Weet je zeker dat je deze groep wilt verwijderen?',
+    'cancel' => 'Annuleren',
+    'confirm' => 'Verwijderen',
+
+    'create' => [
+        'name' => 'Groepsnaam',
+        'description' => 'Beschrijving',
+        'users' => 'Gebruikers',
+        'cancel' => 'Annuleren',
+        'create' => 'Aanmaken',
+
+        'error_name_required' => 'Groepsnaam is verplicht.',
+    ]
+];
