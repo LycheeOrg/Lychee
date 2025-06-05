0a526dc7ab208e99812f6e2b3fdac2ca29fe6604
Benoît Viguier <ildyria@users.noreply.github.com>
2025-06-05 17:05:36 +0200

    Add option to disable slideshow (#3383)

---
+++ database/migrations/2025_05_29_174832_add_slideshow_enabled.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+use App\Models\Extensions\BaseConfigMigration;
+
+return new class() extends BaseConfigMigration {
+	public const CAT = 'Gallery';
+
+	public function getConfigs(): array
+	{
+		return [
+			[
+				'key' => 'slideshow_enabled',
+				'value' => '1',
+				'cat' => self::CAT,
+				'type_range' => self::BOOL,
+				'description' => 'Enable the slideshow functionality.',
+				'details' => '',
+				'is_expert' => true,
+				'is_secret' => true,
+				'level' => 0,
+				'order' => 37,
+			],
+		];
+	}
+};
