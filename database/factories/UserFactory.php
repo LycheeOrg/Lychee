eabe3d32d27c06e3a196250427673860726d25bf
ildyria <beviguier@gmail.com>
2025-06-07 18:38:09 +0200

    Fix user factory uniqueness problem

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -36,11 +36,11 @@ class UserFactory extends Factory
 	public function definition(): array
 	{
 		return [
-			'username' => $this->get_name(),
+			'username' => $this->faker->unique()->name(),
 			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 			'may_administrate' => false,
 			'may_upload' => false,
-			'email' => $this->get_email(),
+			'email' => $this->faker->unique()->email(),
 			'token' => null,
 			'remember_token' => null,
 			'may_edit_own_settings' => true,
@@ -85,43 +85,6 @@ class UserFactory extends Factory
 		return $this->state(function (array $attributes) {
 			return ['may_edit_own_settings' => false];
 		});
-	}
-
-	/**
-	 * Avoid collision of generated names.
-	 *
-	 * @return string
-	 */
-	private function get_name(): string
-	{
-		$candidate = fake()->name();
-		$i = 5;
-		while ($i > 0 && in_array($candidate, $this->name_generated, true)) {
-			$candidate = fake()->name();
-			$i--;
-		}
-		if ($i === 0) {
-			throw new \TypeError('Could not generate unique name');
-		}
-		$this->name_generated[] = $candidate;
-
-		return $candidate;
-	}
-
-	private function get_email(): string
-	{
-		$candidate = fake()->email();
-		$i = 5;
-		while ($i > 0 && in_array($candidate, $this->email_generated, true)) {
-			$candidate = fake()->email();
-			$i--;
-		}
-		if ($i === 0) {
-			throw new \TypeError('Could not generate unique email');
-		}
-		$this->email_generated[] = $candidate;
-
-		return $candidate;
 	}
 
 	/**
