11c505117f4a74291b9f75d2f12eb9b0c0bc6e4b
ildyria <beviguier@gmail.com>
2025-06-13 19:12:35 +0200

    fix routes

--- routes/web_v2.php
+++ routes/web_v2.php
@@ -56,10 +56,10 @@ Route::get('/changelogs', VueController::class)->middleware(['migration:complete
 Route::get('/login', VueController::class)->middleware(['migration:complete']);
 Route::get('/register', VueController::class)->name('register')->middleware(['migration:complete']);
 
-Route::get('/settings', [VueController::class, 'view'])->middleware(['migration:complete', 'login_required:always']);
-Route::get('/permissions', [VueController::class, 'view'])->middleware(['migration:complete', 'login_required:always']);
-Route::get('/fixTree', [VueController::class, 'view'])->middleware(['migration:complete', 'login_required:always']);
-Route::get('/duplicatesFinder', [VueController::class, 'view'])->middleware(['migration:complete', 'login_required:always']);
+Route::get('/settings', VueController::class)->middleware(['migration:complete', 'login_required:always']);
+Route::get('/permissions', VueController::class)->middleware(['migration:complete', 'login_required:always']);
+Route::get('/fixTree', VueController::class)->middleware(['migration:complete', 'login_required:always']);
+Route::get('/duplicatesFinder', VueController::class)->middleware(['migration:complete', 'login_required:always']);
 
 Route::match(['get', 'post'], '/migrate', [Admin\UpdateController::class, 'migrate'])
 	->name('migrate')
