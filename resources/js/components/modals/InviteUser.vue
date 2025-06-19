aa8758f318220e5c9724b0f0d5091e6b27591e52
ildyria <beviguier@gmail.com>
2025-06-19 08:18:51 +0200

    Add localization for invite links

--- resources/js/components/modals/InviteUser.vue
+++ resources/js/components/modals/InviteUser.vue
@@ -7,14 +7,12 @@
 				</div>
 				<div class="px-9 pt-4 pb-9 text-center">
 					<h2 class="font-bold text-xl">{{ $t("users.invite.links_are_not_revokable") }}</h2>
-					<!-- Invitation links are not revokable. -->
 					<span
 						class="font-mono block mb-4 mt-6 overflow-hidden text-base cursor-pointer hover:text-primary-400 w-md whitespace-nowrap"
 						@click="copy"
 						>{{ link }}</span
 					>
 					<p class="text-muted">{{ sprintf($t("users.invite.link_is_valid_x_days"), days) }}</p>
-					<!--  This link is valid for {{ days }} days. -->
 				</div>
 				<Button @click="closeCallback" severity="secondary" class="w-full font-bold border-none rounded-none rounded-bl-xl rounded-br-xl">
 					{{ $t("dialogs.button.close") }}
