bd32598caf2139f621c2f9e0f4187e9bbdd221cf
ildyria <beviguier@gmail.com>
2025-06-26 09:03:38 +0200

    version 6.6.12

---
+++ database/migrations/2025_06_26_070132_bump_version060612.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\ConsoleSectionOutput;
+
+return new class() extends Migration {
+	private ConsoleOutput $output;
+	private ConsoleSectionOutput $msg_section;
+
+	public function __construct()
+	{
+		$this->output = new ConsoleOutput();
+		$this->msg_section = $this->output->section();
+	}
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(): void
+	{
+		// Alpha user database is a bit broken on this one.
+		// See https://github.com/LycheeOrg/Lychee/pull/3433#discussion_r2165674240
+		DB::table('configs')->where('key', 'user_invitation_ttl')->update(['type_range' => 'positive']);
+
+		DB::table('configs')->where('key', 'version')->update(['value' => '060612']);
+		Artisan::call('cache:clear');
+		$this->msg_section->writeln('<info>Info:</info> Cleared cache for version 6.6.12');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(): void
+	{
+		DB::table('configs')->where('key', 'version')->update(['value' => '060611']);
+	}
+};
