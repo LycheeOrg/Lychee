2ea3c93503f472d247ca1f6562c07d62b0b0e937
Anonymous <noreply@weblate.org>
2025-06-30 13:13:44 +0000

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 78.5% (22 of 28 strings)
    
    Translation: Lychee/users
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/users/zh_Hans/

--- lang/zh_CN/users.php
+++ lang/zh_CN/users.php
@@ -1,45 +1,42 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Users page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => '用户',
-	'description' => '在这里您可以管理 Lychee 安装的用户。您可以创建、编辑和删除用户。',
-	'create' => '创建新用户',
-	'username' => '用户名',
-	'password' => '密码',
-	'legend' => '图例',
-	'upload_rights' => '选中后，用户可以上传内容。',
-	'edit_rights' => '选中后，用户可以修改其个人资料（用户名、密码）。',
-	'quota' => '设置后，用户将有照片空间配额限制（以 kB 为单位）。',
-
-	'user_deleted' => '用户已删除',
-	'user_created' => '用户已创建',
-	'user_updated' => '用户已更新',
-	'change_saved' => '更改已保存！',
-
-	'create_edit' => [
-		'upload_rights' => '用户可以上传内容。',
-		'edit_rights' => '用户可以修改其个人资料（用户名、密码）。',
-		'admin_rights' => 'User has admin rights.',
-		'quota' => '用户有配额限制。',
-		'quota_kb' => '配额（kB，0 表示使用默认值）',
-		'note' => '管理员备注（不公开显示）',
-		'create' => '创建',
-		'edit' => '编辑',
-	],
-	'invite' => [
-		'button' => 'Invite user',
-		'links_are_not_revokable' => 'Invitation links are not revokable.',
-		'link_is_valid_x_days' => 'This link is valid for %d days.',
-	],
-	'line' => [
-		'owner' => 'Owner',
-		'admin' => '管理员用户',
-		'edit' => '编辑',
-		'delete' => '删除',
-	],
+    'title' => '用户',
+    'description' => '在这里您可以管理 Lychee 安装的用户。您可以创建、编辑和删除用户。',
+    'create' => '创建新用户',
+    'username' => '用户名',
+    'password' => '密码',
+    'legend' => '图例',
+    'upload_rights' => '选中后，用户可以上传内容。',
+    'edit_rights' => '选中后，用户可以修改其个人资料（用户名、密码）。',
+    'quota' => '设置后，用户将有照片空间配额限制（以 kB 为单位）。',
+    'user_deleted' => '用户已删除',
+    'user_created' => '用户已创建',
+    'user_updated' => '用户已更新',
+    'change_saved' => '更改已保存！',
+    'create_edit' => [
+        'upload_rights' => '用户可以上传内容。',
+        'edit_rights' => '用户可以修改其个人资料（用户名、密码）。',
+        'admin_rights' => 'User has admin rights.',
+        'quota' => '用户有配额限制。',
+        'quota_kb' => '配额（kB，0 表示使用默认值）',
+        'note' => '管理员备注（不公开显示）',
+        'create' => '创建',
+        'edit' => '编辑',
+    ],
+    'invite' => [
+        'button' => 'Invite user',
+        'links_are_not_revokable' => 'Invitation links are not revokable.',
+        'link_is_valid_x_days' => 'This link is valid for %d days.',
+    ],
+    'line' => [
+        'owner' => 'Owner',
+        'admin' => '管理员用户',
+        'edit' => '编辑',
+        'delete' => '删除',
+    ],
 ];
