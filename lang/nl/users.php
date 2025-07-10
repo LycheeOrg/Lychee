844e7eee53f703de5f9ed238be77d9e6a15ef1a1
ildyria <benoit@viguier.nl>
2025-07-10 08:54:33 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: Lychee/users
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/users/nl/

--- lang/nl/users.php
+++ lang/nl/users.php
@@ -1,45 +1,42 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Users page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Gebruikers',
-	'description' => 'Hier kunt u de gebruikers van uw Lychee-installatie beheren. U kunt gebruikers aanmaken, bewerken en verwijderen.',
-	'create' => 'Maak een nieuwe gebruiker aan',
-	'username' => 'Gebruikersnaam',
-	'password' => 'Wachtwoord',
-	'legend' => 'Legenda',
-	'upload_rights' => 'Wanneer geselecteerd, kan de gebruiker inhoud uploaden.',
-	'edit_rights' => 'Wanneer geselecteerd, kan de gebruiker zijn profiel wijzigen (gebruikersnaam, wachtwoord).',
-	'quota' => 'Wanneer ingesteld, heeft de gebruiker een ruimtequotum voor foto’s (in kB).',
-
-	'user_deleted' => 'Gebruiker verwijderd',
-	'user_created' => 'Gebruiker aangemaakt',
-	'user_updated' => 'Gebruiker bijgewerkt',
-	'change_saved' => 'Wijziging opgeslagen!',
-
-	'create_edit' => [
-		'upload_rights' => 'Gebruiker kan inhoud uploaden.',
-		'edit_rights' => 'Gebruiker kan zijn profiel wijzigen (gebruikersnaam, wachtwoord).',
-		'admin_rights' => 'User has admin rights.',
-		'quota' => 'Gebruiker heeft een quotumlimiet.',
-		'quota_kb' => 'quotum in kB (0 voor standaard)',
-		'note' => 'Beheerdersnotitie (niet openbaar zichtbaar)',
-		'create' => 'Aanmaken',
-		'edit' => 'Bewerken',
-	],
-	'invite' => [
-		'button' => 'Invite user',
-		'links_are_not_revokable' => 'Invitation links are not revokable.',
-		'link_is_valid_x_days' => 'This link is valid for %d days.',
-	],
-	'line' => [
-		'owner' => 'Owner',
-		'admin' => 'Beheerder',
-		'edit' => 'Bewerken',
-		'delete' => 'Verwijderen',
-	],
+    'title' => 'Gebruikers',
+    'description' => 'Hier kunt u de gebruikers van uw Lychee-installatie beheren. U kunt gebruikers aanmaken, bewerken en verwijderen.',
+    'create' => 'Maak een nieuwe gebruiker aan',
+    'username' => 'Gebruikersnaam',
+    'password' => 'Wachtwoord',
+    'legend' => 'Legenda',
+    'upload_rights' => 'Wanneer geselecteerd, kan de gebruiker inhoud uploaden.',
+    'edit_rights' => 'Wanneer geselecteerd, kan de gebruiker zijn profiel wijzigen (gebruikersnaam, wachtwoord).',
+    'quota' => 'Wanneer ingesteld, heeft de gebruiker een ruimtequotum voor foto’s (in kB).',
+    'user_deleted' => 'Gebruiker verwijderd',
+    'user_created' => 'Gebruiker aangemaakt',
+    'user_updated' => 'Gebruiker bijgewerkt',
+    'change_saved' => 'Wijziging opgeslagen!',
+    'create_edit' => [
+        'upload_rights' => 'Gebruiker kan inhoud uploaden.',
+        'edit_rights' => 'Gebruiker kan zijn profiel wijzigen (gebruikersnaam, wachtwoord).',
+        'admin_rights' => 'Gebruiker heeft beheerdersrechten.',
+        'quota' => 'Gebruiker heeft een quotumlimiet.',
+        'quota_kb' => 'quotum in kB (0 voor standaard)',
+        'note' => 'Beheerdersnotitie (niet openbaar zichtbaar)',
+        'create' => 'Aanmaken',
+        'edit' => 'Bewerken',
+    ],
+    'invite' => [
+        'button' => 'Nodig een gebruiker uit',
+        'links_are_not_revokable' => 'Uitnodigingslinks zijn niet herroepbaar.',
+        'link_is_valid_x_days' => 'Deze link is %d dagen geldig.',
+    ],
+    'line' => [
+        'owner' => 'Eigenaar',
+        'admin' => 'Beheerder',
+        'edit' => 'Bewerken',
+        'delete' => 'Verwijderen',
+    ],
 ];
