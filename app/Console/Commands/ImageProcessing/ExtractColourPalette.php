9f919eaf23113fcc417fd351975c1d8bba87ef45
ildyria <beviguier@gmail.com>
2025-07-13 16:10:07 +0200

    while processing in CLI, do not crash on large photos

--- app/Console/Commands/ImageProcessing/ExtractColourPalette.php
+++ app/Console/Commands/ImageProcessing/ExtractColourPalette.php
@@ -59,8 +59,14 @@ class ExtractColourPalette extends Command
 			}
 
 			foreach ($photos as $photo) {
-				$this->line(sprintf('Extracting Color Palette for %s [%s].', $photo->title, $photo->id));
-				ExtractColoursJob::dispatchSync($photo);
+				try {
+					$this->line(sprintf('Extracting Color Palette for %s [%s].', $photo->title, $photo->id));
+					ExtractColoursJob::dispatchSync($photo);
+					// @codeCoverageIgnoreStart
+				} catch (\Throwable $e) {
+					$this->error(sprintf('Error extracting color palette for %s [%s]: %s', $photo->title, $photo->id, $e->getMessage()));
+				}
+				// @codeCoverageIgnoreEnd
 			}
 
 			return 0;
