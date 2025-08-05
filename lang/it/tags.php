e8931cc4ee049aebe0015061ef93371682bba1bc
ildyria <beviguier@gmail.com>
2025-08-05 18:16:03 +0200

    translations

---
+++ lang/it/tags.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    /*
+	|--------------------------------------------------------------------------
+	| Tags page
+	|--------------------------------------------------------------------------
+	*/
+    'title' => 'Tags',
+	'description' => 'Manage your tags here. You can rename, merge, and delete tags.<br>The number indicates how many photos are associated with that tag.',
+	'no_tags' => 'No tags found.',
+	'loading' => 'Loading tags...',
+	'rename' => 'Rename',
+	'merge' => 'Merge',
+	'delete' => 'Delete',
+
+	'delete_confirm' => 'Are you sure you want to delete the tag “%s”?',
+	'delete_warning' => 'This action cannot be undone! All photos associated with this tag will no longer have this tag.',
+
+	'merge_confirm' => 'Are you sure you want to merge the tag “%s” into the tag “%s”?',
+	'merge_warning' => 'This action cannot be undone! All photos associated with the source tag will be moved to the destination tag.',
+
+	'rename_tag' => 'Rename Tag',
+];
