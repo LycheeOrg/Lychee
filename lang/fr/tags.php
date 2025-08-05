d9623be9d198b344bed2f8ec329b947d25031270
ildyria <beviguier@gmail.com>
2025-08-05 18:16:03 +0200

    translations

---
+++ lang/fr/tags.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    /*
+	|--------------------------------------------------------------------------
+	| Page des tags
+	|--------------------------------------------------------------------------
+	*/
+    'title' => 'Tags',
+	'description' => 'Gérez vos tags ici. Vous pouvez renommer, fusionner et supprimer des tags.<br>Le nombre indique combien de photos sont associées à ce tag.',
+	'no_tags' => 'Aucun tag trouvé.',
+	'loading' => 'Chargement des tags...',
+	'rename' => 'Renommer',
+	'merge' => 'Fusionner',
+	'delete' => 'Supprimer',
+
+	'delete_confirm' => 'Êtes-vous sûr de vouloir supprimer le tag “%s” ?',
+	'delete_warning' => 'Cette action est irréversible ! Toutes les photos associées à ce tag n’auront plus ce tag.',
+
+	'merge_confirm' => 'Êtes-vous sûr de vouloir fusionner le tag “%s” avec le tag “%s” ?',
+	'merge_warning' => 'Cette action est irréversible ! Toutes les photos associées au tag source seront déplacées vers le tag de destination.',
+
+	'rename_tag' => 'Renommer le tag',
+];
