144d60042d2f86eb04473a67c298de8b81c3e5bc
ildyria <beviguier@gmail.com>
2025-08-07 21:25:19 +0200

    fix stupid postgresql bug

--- app/Actions/Tag/ListTags.php
+++ app/Actions/Tag/ListTags.php
@@ -49,7 +49,8 @@ class ListTags
 			)
 			->groupBy(['tags.id', 'tags.name'])
 			->orderBy('tags.name')
-			->having('num', '>', 0) // Exclude tags with no photos => this makes sure we do not leak tags from other users.
+			->havingRaw('COUNT(photos_tags.photo_id) > 0') // Exclude tags with no photos => this makes sure we do not leak tags from other users.
+			// Here we can not use `->having('num', '>', 0)` because the aliasing in Postgresql is done AFTER the `HAVING` clause... 
 			->get();
 
 		return new TagsResource($tags->map(fn ($tag) => new TagResource(
