4d5f1d33aea5b7b5186e24181919d9572ad99ef3
Iván Campillo <info@ivancampillo.com>
2025-08-07 23:56:26 +0000

    Translated using Weblate (Spanish)
    Currently translated at 61.5% (8 of 13 strings)
    
    Translation: Lychee/duplicate-finder
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/duplicate-finder/es/

--- lang/es/duplicate-finder.php
+++ lang/es/duplicate-finder.php
@@ -1,29 +1,25 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Duplicate Finder Page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Maintenance',
-	'intro' => 'On this page you will find the duplicate pictures found in your database.',
-	'found' => ' duplicates found!',
-	'invalid-search' => ' At least the checksum or title condition must be checked.',
-	'checksum-must-match' => 'Checksum must match.',
-	'title-must-match' => 'Title must match.',
-	'must-be-in-same-album' => 'Must be in the same album.',
-
-	'columns' => [
-		'album' => 'Album',
-		'photo' => 'Photo',
-		'checksum' => 'Checksum',
-	],
-
-	'warning' => [
-		'no-original-left' => 'No original left.',
-		'keep-one' => 'You selected all duplicates in this group. Please chose at least one duplicate to keep.',
-	],
-
-	'delete-selected' => 'Delete selected',
+    'title' => 'Mantenimiento',
+    'intro' => 'En esta página encontrará las imágenes duplicadas que se encuentran en su base de datos.',
+    'found' => ' ¡Se han encontrado duplicados!',
+    'invalid-search' => ' Como mínimo, se debe comprobar la suma de comprobación o el estado del título.',
+    'checksum-must-match' => 'La suma de comprobación debe coincidir.',
+    'title-must-match' => 'El título debe coincidir.',
+    'must-be-in-same-album' => 'Debe estar en el mismo álbum.',
+    'columns' => [
+        'album' => 'Álbum',
+        'photo' => 'Photo',
+        'checksum' => 'Checksum',
+    ],
+    'warning' => [
+        'no-original-left' => 'No original left.',
+        'keep-one' => 'You selected all duplicates in this group. Please chose at least one duplicate to keep.',
+    ],
+    'delete-selected' => 'Delete selected',
 ];
