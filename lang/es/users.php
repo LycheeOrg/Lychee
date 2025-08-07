fa4fff7f9a405afc967ee31b5f558ec2242c0750
Iván Campillo <info@ivancampillo.com>
2025-08-07 23:47:17 +0000

    Translated using Weblate (Spanish)
    Currently translated at 32.1% (9 of 28 strings)
    
    Translation: Lychee/users
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/users/es/

--- lang/es/users.php
+++ lang/es/users.php
@@ -1,45 +1,42 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Users page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Users',
-	'description' => 'Here you can manage the users of your Lychee installation. You can create, edit and delete users.',
-	'create' => 'Create a new user',
-	'username' => 'Username',
-	'password' => 'Password',
-	'legend' => 'Legend',
-	'upload_rights' => 'When selected, the user can upload content.',
-	'edit_rights' => 'When selected, the user can modify their profile (username, password).',
-	'quota' => 'When set, the user has a space quota for pictures (in kB).',
-
-	'user_deleted' => 'User deleted',
-	'user_created' => 'User created',
-	'user_updated' => 'User updated',
-	'change_saved' => 'Change saved!',
-
-	'create_edit' => [
-		'upload_rights' => 'User can upload content.',
-		'edit_rights' => 'User can modify their profile (username, password).',
-		'admin_rights' => 'User has admin rights.',
-		'quota' => 'User has quota limit.',
-		'quota_kb' => 'quota in kB (0 for default)',
-		'note' => 'Admin note (not publically visible)',
-		'create' => 'Create',
-		'edit' => 'Edit',
-	],
-	'invite' => [
-		'button' => 'Invite user',
-		'links_are_not_revokable' => 'Invitation links are not revokable.',
-		'link_is_valid_x_days' => 'This link is valid for %d days.',
-	],
-	'line' => [
-		'owner' => 'Owner',
-		'admin' => 'Admin user',
-		'edit' => 'Edit',
-		'delete' => 'Delete',
-	],
+    'title' => 'Usuarios',
+    'description' => 'Aquí puedes gestionar los usuarios de tu instalación de Lychee. Puedes crear, editar y eliminar usuarios.',
+    'create' => 'Crear un nuevo usuario',
+    'username' => 'Nombre de usuario',
+    'password' => 'Contraseña',
+    'legend' => 'Leyenda',
+    'upload_rights' => 'Cuando se selecciona, el usuario puede subir contenido.',
+    'edit_rights' => 'Cuando se selecciona, el usuario puede modificar su perfil (nombre de usuario, contraseña).',
+    'quota' => 'When set, the user has a space quota for pictures (in kB).',
+    'user_deleted' => 'User deleted',
+    'user_created' => 'User created',
+    'user_updated' => 'User updated',
+    'change_saved' => 'Change saved!',
+    'create_edit' => [
+        'upload_rights' => 'User can upload content.',
+        'edit_rights' => 'User can modify their profile (username, password).',
+        'admin_rights' => 'User has admin rights.',
+        'quota' => 'User has quota limit.',
+        'quota_kb' => 'quota in kB (0 for default)',
+        'note' => 'Admin note (not publically visible)',
+        'create' => 'Create',
+        'edit' => 'Edit',
+    ],
+    'invite' => [
+        'button' => 'Invite user',
+        'links_are_not_revokable' => 'Invitation links are not revokable.',
+        'link_is_valid_x_days' => 'This link is valid for %d days.',
+    ],
+    'line' => [
+        'owner' => 'Owner',
+        'admin' => 'Admin user',
+        'edit' => 'Edit',
+        'delete' => 'Delete',
+    ],
 ];
