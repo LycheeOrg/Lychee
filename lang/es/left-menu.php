0a72dff2704a787125b4d8cafa5c8dd4fd1f2ec3
Iván Campillo <info@ivancampillo.com>
2025-08-07 23:45:50 +0000

    Translated using Weblate (Spanish)
    Currently translated at 53.3% (8 of 15 strings)
    
    Translation: Lychee/left-menu
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/left-menu/es/

--- lang/es/left-menu.php
+++ lang/es/left-menu.php
@@ -1,27 +1,23 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Left Menu
 	|--------------------------------------------------------------------------
 	*/
-	'back_to_gallery' => 'Back to Gallery',
-
-	'login' => 'Login',
-	'frame' => 'Frame',
-	'map' => 'Map',
-	'admin' => 'Admin',
-	'clockwork' => 'Clockwork App',
-	'logs' => 'Show Logs',
-	'jobs' => 'Show Job History',
-	'user' => 'User',
-
-	'sign_out' => 'Sign Out',
-
-	'about' => 'About',
-	'changelog' => 'Changelog',
-	'api' => 'API Documentation',
-	'source_code' => 'Source Code',
-	'support' => 'Support',
+    'back_to_gallery' => 'Volver a galería',
+    'login' => 'Iniciar sesión',
+    'frame' => 'Marco',
+    'map' => 'Mapa',
+    'admin' => 'Administrador',
+    'clockwork' => 'Aplicación Clockwork',
+    'logs' => 'Mostrar registros',
+    'jobs' => 'Mostrar historial laboral',
+    'user' => 'User',
+    'sign_out' => 'Sign Out',
+    'about' => 'About',
+    'changelog' => 'Changelog',
+    'api' => 'API Documentation',
+    'source_code' => 'Source Code',
+    'support' => 'Support',
 ];
