8eb1d3bb0b40dfe8050f7ef1596f8e94ad24496e
Iván Campillo <info@ivancampillo.com>
2025-08-08 02:24:19 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Lychee/landing
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/landing/es/

--- lang/es/landing.php
+++ lang/es/landing.php
@@ -8,5 +8,5 @@ return [
     'gallery' => 'Galería',
     'access_gallery' => 'Acceder a la galería',
     'Powered_by_Lychee' => 'Desarrollado por Lychee',
-    'copyright' => 'Todas las imágenes de este sitio web están sujetas a derechos de autor por %1$s © %2$s.',
+    'copyright' => 'Todas las imágenes de este sitio web están sujetas a derechos de autor por %1$s © %2$s',
 ];
