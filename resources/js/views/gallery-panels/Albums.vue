b0b6cdff68ade3f648f4534403947972f5388040
ildyria <beviguier@gmail.com>
2025-08-16 02:53:19 +0200

    and mix selection

--- resources/js/views/gallery-panels/Albums.vue
+++ resources/js/views/gallery-panels/Albums.vue
@@ -8,13 +8,7 @@
 	<WebauthnModal v-if="user?.id === null" @logged-in="refresh" />
 	<LiveMetrics v-if="user?.id" />
 
-	<div
-		v-if="rootConfig && rootRights"
-		id="galleryView"
-		class="relative w-full overflow-y-auto h-full select-none"
-		@click="unselect"
-		@scroll="onScroll"
-	>
+	<div v-if="rootConfig && rootRights" id="galleryView" class="relative w-full overflow-y-auto h-full select-none" @scroll="onScroll">
 		<SelectDrag :photos="photos" :albums="selectableAlbums" :with-scroll="false" />
 		<Collapse :when="!is_full_screen">
 			<AlbumsHeader
