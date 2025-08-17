e7eccc21a99261d85d0684b9000e7500bbd07b3c
Vegar  Oyfoss <vegaroyfoss@gmail.com>
2025-08-17 11:14:14 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 68.2% (28 of 41 strings)
    
    Translation: Lychee/profile
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/profile/nb_NO/

--- lang/no/profile.php
+++ lang/no/profile.php
@@ -1,66 +1,59 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Profile page
 	|--------------------------------------------------------------------------
 	*/
-	'title' => 'Profile',
-
-	'login' => [
-		'header' => 'Profile',
-		'enter_current_password' => 'Enter your current password:',
-		'current_password' => 'Current password',
-		'credentials_update' => 'Your credentials will be changed to the following:',
-		'username' => 'Username',
-		'new_password' => 'New password',
-		'confirm_new_password' => 'Confirm new password',
-		'email_instruction' => 'Add your email below to enable receiving email notifications. To stop receiving emails, simply remove your email below.',
-		'email' => 'Email',
-		'change' => 'Change Login',
-		'api_token' => 'API Token ...',
-
-		'missing_fields' => 'Missing fields',
-	],
-
-	'register' => [
-		'username_exists' => 'Username already exists.',
-		'password_mismatch' => 'The passwords do not match.',
-		'signup' => 'Sign Up',
-		'error' => 'An error occurred while registering your account.',
-		'success' => 'Your account has been successfully created.',
-	],
-	
-	'token' => [
-		'unavailable' => 'You have already viewed this token.',
-		'no_data' => 'No token API have been generated.',
-		'disable' => 'Disable',
-		'disabled' => 'Token disabled',
-		'warning' => 'This token will not be displayed again. Copy it and keep it in a safe place.',
-		'reset' => 'Reset the token',
-		'create' => 'Create a new token',
-	],
-
-	'oauth' => [
-		'header' => 'OAuth',
-		'header_not_available' => 'OAuth is not available',
-		'setup_env' => 'Set up the credentials in your .env',
-		'token_registered' => '%s token registered.',
-		'setup' => 'Set up %s',
-		'reset' => 'reset',
-		'credential_deleted' => 'Credential deleted!',
-	],
-
-	'u2f' => [
-		'header' => 'Passkey/MFA/2FA',
-		'info' => 'This only provides the ability to use WebAuthn to authenticate instead of username & password.',
-		'empty' => 'Credentials list is empty!',
-		'not_secure' => 'Environment not secured. U2F not available.',
-		'new' => 'Register new device.',
-		'credential_deleted' => 'Credential deleted!',
-		'credential_updated' => 'Credential updated!',
-		'credential_registred' => 'Registration successful!',
-		'5_chars' => 'At least 5 chars.',
-	],
+    'title' => 'Profil',
+    'login' => [
+        'header' => 'Profil',
+        'enter_current_password' => 'Skriv inn ditt nåværende passord:',
+        'current_password' => 'Nåværende passord',
+        'credentials_update' => 'Dine innloggingsdetalier din vil bli endret til følgende:',
+        'username' => 'Brukernavn',
+        'new_password' => 'Nytt passord',
+        'confirm_new_password' => 'Bekreft nytt passord',
+        'email_instruction' => 'Legg til e-postadressen din nedenfor for å aktivere mottak av e-postvarsler. For å slutte å motta e-poster, fjern ganske enkelt e-postadressen din nedenfor.',
+        'email' => 'Epostadresse',
+        'change' => 'Endre pålogging',
+        'api_token' => 'API-token …',
+        'missing_fields' => 'Manglende felt',
+    ],
+    'register' => [
+        'username_exists' => 'Brukernavnet finnes allerede.',
+        'password_mismatch' => 'Passordene stemmer ikke overens.',
+        'signup' => 'Registrer deg',
+        'error' => 'Det oppsto en feil under registrering av kontoen din.',
+        'success' => 'Kontoen din er opprettet.',
+    ],
+    'token' => [
+        'unavailable' => 'Du har allerede sett denne tokenen.',
+        'no_data' => 'Ingen token-API er generert.',
+        'disable' => 'Deaktiver',
+        'disabled' => 'Token deaktivert',
+        'warning' => 'Denne tokenen vil ikke vises igjen. Kopier den og oppbevar den på et trygt sted.',
+        'reset' => 'Tilbakestill tokenet',
+        'create' => 'Opprett et nytt token',
+    ],
+    'oauth' => [
+        'header' => 'OAuth',
+        'header_not_available' => 'OAuth er ikke tilgjengelig',
+        'setup_env' => 'Konfigurer legitimasjonen i .env-filen din',
+        'token_registered' => '%s token registered.',
+        'setup' => 'Set up %s',
+        'reset' => 'reset',
+        'credential_deleted' => 'Credential deleted!',
+    ],
+    'u2f' => [
+        'header' => 'Passkey/MFA/2FA',
+        'info' => 'This only provides the ability to use WebAuthn to authenticate instead of username & password.',
+        'empty' => 'Credentials list is empty!',
+        'not_secure' => 'Environment not secured. U2F not available.',
+        'new' => 'Register new device.',
+        'credential_deleted' => 'Credential deleted!',
+        'credential_updated' => 'Credential updated!',
+        'credential_registred' => 'Registration successful!',
+        '5_chars' => 'At least 5 chars.',
+    ],
 ];
