702a29707cb31918c7ec3f92249a342e15058bb0
ildyria <beviguier@gmail.com>
2025-08-18 19:25:39 +0200

    fix

--- tests/Feature_v2/Timeline/TimelineTest.php
+++ tests/Feature_v2/Timeline/TimelineTest.php
@@ -51,10 +51,10 @@ class TimelineTest extends BaseApiWithDataTest
 		$response = $this->getJson('Timeline::dates');
 		$this->assertOk($response);
 
-		$response = $this->getJson('Timeline::dates', ['date' => '2021-01-01']);
+		$response = $this->getJson('Timeline::dates?date=2021-01-01');
 		$this->assertOk($response);
 
-		$response = $this->getJson('Timeline::dates', ['photoId' => $this->photo1->id]);
+		$response = $this->getJson('Timeline::dates?photoId=' . $this->photo1->id);
 		$this->assertOk($response);
 
 		$response = $this->getJson('Timeline::init');
