46f991600b11441c89c0a948447f26ce336cba08
ildyria <beviguier@gmail.com>
2025-08-23 20:49:59 +0200

    fix copyright requirement

--- database/migrations/2025_08_23_204700_extract_zip_on_upload.php
+++ database/migrations/2025_08_23_204700_extract_zip_on_upload.php
@@ -1,10 +1,19 @@
 <?php
 
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
 use App\Models\Extensions\BaseConfigMigration;
 
 return new class() extends BaseConfigMigration {
 	public const PROCESSING = 'Image Processing';
 
+	/**
+	 * @return array<int,array{key:string,value:string,is_secret:bool,cat:string,type_range:string,description:string,order?:int,not_on_docker?:bool,is_expert?:bool}>
+	 */
 	public function getConfigs(): array
 	{
 		return [
@@ -16,6 +25,8 @@ return new class() extends BaseConfigMigration {
 				'description' => 'Extract uploaded zip file and import content.',
 				'details' => 'Zip file will stay on your server unless it is properly extracted without faults (after which it is removed).',
 				'is_secret' => false,
+				'order' => 27,
+				'not_on_docker' => false,
 				'level' => 1, // Only for SE.
 			],
 		];
