9949d730c18db75b8c04f4b1d24bf86f4cd2b1ee
ildyria <beviguier@gmail.com>
2025-10-06 23:15:47 +0200

    clean up comments

--- app/SmartAlbums/UntaggedAlbum.php
+++ app/SmartAlbums/UntaggedAlbum.php
@@ -34,30 +34,4 @@ class UntaggedAlbum extends BaseSmartAlbum
 	{
 		return self::$instance ??= new self();
 	}
-
-	// /**
-	//  * In the case of UNTAGGED, we cannot determine whether the photo is visible or not from its parent.
-	//  * If the UNTAGGED album is made private, then all the pictures in it are visible to owner only.
-	//  *
-	//  * @return \App\Eloquent\FixedQueryBuilder<Photo>
-	//  */
-	// public function photos(): Builder
-	// {
-	//     $user_id = Auth::id();
-	//     $query = Photo::where('owner_id', $user_id)
-	//         ->with(['size_variants', 'statistics', 'palette', 'tags'])
-	//         ->where($this->smart_photo_condition);
-
-	//     return $query;
-	// }
-
-	// public function getPaginatedPhotos($per_page = null, $page = null)
-	// {
-	//     $photo_per_page = $per_page ?? Configs::getValueAsInt('untagged_photos_pagination_limit');
-	//     $current_page = $page ?? 1;
-
-	//     return $this->photos()
-	//         ->orderBy('created_at', 'desc')
-	//         ->paginate($photo_per_page, ['*'], 'page', $current_page);
-	// }
 }
