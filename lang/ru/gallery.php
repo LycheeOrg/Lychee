cb0f0867643bef47863e94ea9f8327f755ad4762
Nikita Shkapin <nikita.shkapin@gmail.com>
2025-10-08 22:32:02 +0000

    Translated using Weblate (Russian)
    Currently translated at 90.4% (171 of 189 strings)
    
    Translation: Lychee/gallery
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/gallery/ru/

--- lang/ru/gallery.php
+++ lang/ru/gallery.php
@@ -41,7 +41,7 @@ return [
         'recent' => 'Недавние',
         'public' => 'Публичные',
         'on_this_day' => 'В этот день',
-		'untagged' => 'Untagged',
+        'untagged' => 'Untagged',
     ],
     'layout' => [
         'squares' => 'Квадратные миниатюры',
@@ -208,7 +208,7 @@ return [
         'merge_all' => 'Объединить выбранные',
         'upload_photo' => 'Загрузить фото',
         'import_link' => 'Импортировать по ссылке',
-		'import_server' => 'Import from Server',
+        'import_server' => 'Import from Server',
         'import_dropbox' => 'Импортировать из Dropbox',
         'new_album' => 'Новый альбом',
         'new_tag_album' => 'Новый альбом с тегами',
