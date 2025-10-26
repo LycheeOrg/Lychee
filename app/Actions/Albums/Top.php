a6468eb00189eff698fd172e251a09b0d798472a
Chris Dzombak <chris@dzombak.com>
2025-10-26 05:54:38 -0500

    Deduplicate top-level featured/pinned albums (#3762)

--- app/Actions/Albums/Top.php
+++ app/Actions/Albums/Top.php
@@ -17,6 +17,7 @@ use App\Exceptions\ConfigurationKeyMissingException;
 use App\Exceptions\Internal\InvalidOrderDirectionException;
 use App\Factories\AlbumFactory;
 use App\Models\Album;
+use App\Models\Builders\AlbumBuilder;
 use App\Models\Configs;
 use App\Models\Extensions\SortingDecorator;
 use App\Models\TagAlbum;
@@ -91,9 +92,14 @@ class Top
 			)
 			->get();
 
-		/** @return AlbumBuilder $query */
+		/** @var AlbumBuilder $query */
 		$query = $this->album_query_policy
-			->applyVisibilityFilter(Album::query()->with(['access_permissions', 'owner'])->whereIsRoot());
+			->applyVisibilityFilter(Album::query()->with(['access_permissions', 'owner'])->whereIsRoot()
+			->when(
+				Configs::getValueAsBool('deduplicate_pinned_albums'),
+				fn ($q) => $q
+					->joinSub(DB::table('base_albums')->select(['id', 'is_pinned'])->where('is_pinned', '=', false), 'not_pinned', 'not_pinned.id', '=', 'albums.id')
+			));
 
 		$user_id = Auth::id();
 		if ($user_id !== null) {
