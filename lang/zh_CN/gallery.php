286f2709b953c90c7c79f918407751ef874acd5e
Anonymous <noreply@weblate.org>
2025-10-29 12:49:44 +0000

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 89.4% (169 of 189 strings)
    
    Translation: Lychee/gallery
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/gallery/zh_Hans/

--- lang/zh_CN/gallery.php
+++ lang/zh_CN/gallery.php
@@ -41,7 +41,7 @@ return [
         'recent' => '最近',
         'public' => '公开',
         'on_this_day' => '历史上的今天',
-		'untagged' => 'Untagged',
+        'untagged' => 'Untagged',
     ],
     'layout' => [
         'squares' => '方形缩略图',
@@ -208,7 +208,7 @@ return [
         'merge_all' => '合并所选',
         'upload_photo' => '上传照片',
         'import_link' => '从链接导入',
-		'import_server' => 'Import from Server',
+        'import_server' => 'Import from Server',
         'import_dropbox' => '从 Dropbox 导入',
         'new_album' => '新建相册',
         'new_tag_album' => '新建标签相册',
