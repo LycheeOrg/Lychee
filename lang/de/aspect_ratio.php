e719ac60050a8a73ceeb3c6cdbb7be29ae256d48
Anonymous <noreply@weblate.org>
2025-10-29 12:49:53 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Lychee/aspect_ratio
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/aspect_ratio/de/

--- lang/de/aspect_ratio.php
+++ lang/de/aspect_ratio.php
@@ -1,15 +1,14 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Aspect Ratio lines.
 	|--------------------------------------------------------------------------
 	*/
-	'5by4' => '5/4 (instagram Querformat)',
-	'4by5' => '4/5 (instagram Portrait)',
-	'2by3' => '2/3 (Portrait)',
-	'3by2' => '3/2 (Querformat)',
-	'1by1' => 'square',
-	'1byx9' => '16/9 (Querformat)',
+    '5by4' => '5/4 (instagram Querformat)',
+    '4by5' => '4/5 (instagram Portrait)',
+    '2by3' => '2/3 (Portrait)',
+    '3by2' => '3/2 (Querformat)',
+    '1by1' => 'square',
+    '1byx9' => '16/9 (Querformat)',
 ];
