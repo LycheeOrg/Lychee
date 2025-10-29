bd3f93a08cfc571179595a552cad9cbc84d0247b
Anonymous <noreply@weblate.org>
2025-10-29 12:49:44 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Lychee/landing
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/landing/fr/

--- lang/fr/landing.php
+++ lang/fr/landing.php
@@ -1,13 +1,12 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Landing page
 	|--------------------------------------------------------------------------
 	*/
-	'gallery' => 'Galerie',
-	'access_gallery' => 'Accéder à la galerie',
-	'Powered_by_Lychee' => 'Propulsé avec Lychee',
-	'copyright' => 'Toutes les images de ce site sont protégées par le droit d’auteur de %1$s © %2$s',
+    'gallery' => 'Galerie',
+    'access_gallery' => 'Accéder à la galerie',
+    'Powered_by_Lychee' => 'Propulsé avec Lychee',
+    'copyright' => 'Toutes les images de ce site sont protégées par le droit d’auteur de %1$s © %2$s',
 ];
