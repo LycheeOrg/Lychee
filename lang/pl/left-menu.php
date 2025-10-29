ae9b0d579387a7e10e8d26fcc82215c3ef6a1185
Anonymous <noreply@weblate.org>
2025-10-29 12:49:45 +0000

    Translated using Weblate (Polish)
    Currently translated at 73.3% (11 of 15 strings)
    
    Translation: Lychee/left-menu
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/left-menu/pl/

--- lang/pl/left-menu.php
+++ lang/pl/left-menu.php
@@ -1,27 +1,23 @@
 <?php
-
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Left Menu
 	|--------------------------------------------------------------------------
 	*/
-	'back_to_gallery' => 'Powrót do galerii',
-
-	'login' => 'Login',
-	'frame' => 'Frame',
-	'map' => 'Map',
-	'admin' => 'Administrator',
-	'clockwork' => 'clockwork',
-	'logs' => 'Pokaż dzienniki',
-	'jobs' => 'Pokaż historię zadań',
-	'user' => 'Użytkownik',
-
-	'sign_out' => 'Wyloguj się',
-
-	'about' => 'O',
-	'changelog' => 'Changelog',
-	'api' => 'Dokumentacja API',
-	'source_code' => 'Kod źródłowy',
-	'support' => 'Wsparcie',
+    'back_to_gallery' => 'Powrót do galerii',
+    'login' => 'Login',
+    'frame' => 'Frame',
+    'map' => 'Map',
+    'admin' => 'Administrator',
+    'clockwork' => 'clockwork',
+    'logs' => 'Pokaż dzienniki',
+    'jobs' => 'Pokaż historię zadań',
+    'user' => 'Użytkownik',
+    'sign_out' => 'Wyloguj się',
+    'about' => 'O',
+    'changelog' => 'Changelog',
+    'api' => 'Dokumentacja API',
+    'source_code' => 'Kod źródłowy',
+    'support' => 'Wsparcie',
 ];
