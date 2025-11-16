0c7b397bea243d18747819f5f4834bfdc89f2197
ildyria <beviguier@gmail.com>
2025-11-16 13:12:10 +0100

    fix missing cache

--- app/Metadata/Cache/RouteCacheManager.php
+++ app/Metadata/Cache/RouteCacheManager.php
@@ -83,6 +83,7 @@ final readonly class RouteCacheManager
 			'api/v2/Maintenance::searchDuplicates' => false,
 			'api/v2/Maintenance::statisticsIntegrity' => false,
 			'api/v2/Maintenance::missingPalettes' => false,
+			'api/v2/Maintenance::oldOrders' => false,
 
 			'api/v2/Map' => new RouteCacheConfig(tag: CacheTag::GALLERY, user_dependant: true, extra: [RequestAttribute::ALBUM_ID_ATTRIBUTE]),
 			'api/v2/Map::provider' => new RouteCacheConfig(tag: CacheTag::SETTINGS),
