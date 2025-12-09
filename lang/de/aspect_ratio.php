d438f46f0ec081aa6c86be3f4b3e2957f9e596fb
Benoît Viguier <ildyria@users.noreply.github.com>
2025-12-09 19:40:16 +0100

    Translations update from LycheeOrg - Weblate (#3854)
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Lychee/aspect_ratio
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/aspect_ratio/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: Lychee/changelogs
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/changelogs/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 81.5% (150 of 184 strings)
    
    Translation: Lychee/dialogs
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/dialogs/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: Lychee/fix-tree
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/fix-tree/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 97.3% (187 of 192 strings)
    
    Translation: Lychee/gallery
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/gallery/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Lychee/landing
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/landing/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Lychee/left-menu
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/left-menu/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (46 of 46 strings)
    
    Translation: Lychee/maintenance
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/maintenance/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (41 of 41 strings)
    
    Translation: Lychee/profile
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/profile/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (64 of 64 strings)
    
    Translation: Lychee/settings
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/settings/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: Lychee/sharing
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/sharing/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: Lychee/users
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/users/de/
    
    * Translated using Weblate (German)
    
    Currently translated at 31.2% (184 of 588 strings)
    
    Translation: Lychee/All settings
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/all-settings/de/
    
    ---------
    
    Co-authored-by: Dominik <hamster_joggt7f@icloud.com>

--- lang/de/aspect_ratio.php
+++ lang/de/aspect_ratio.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     /*
     |--------------------------------------------------------------------------
@@ -10,6 +9,6 @@ return [
     '4by5' => '4/5 (instagram Portrait)',
     '2by3' => '2/3 (Portrait)',
     '3by2' => '3/2 (Querformat)',
-    '1by1' => 'square',
+    '1by1' => 'Quadrat',
     '16by9' => '16/9 (Querformat)',
 ];
