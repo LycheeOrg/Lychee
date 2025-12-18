ec8755f3f7c6116b6786ef042dd32420dab99e22
nbalkandzhiyski <nikolay.balkandzhiyski@gmail.com>
2025-12-18 17:01:48 +0200

    Normalize image extension input in ConvertableImageType enum

--- app/Enum/ConvertableImageType.php
+++ app/Enum/ConvertableImageType.php
@@ -9,6 +9,8 @@ enum ConvertableImageType: string
 
 	public static function isHeifImageType(string $extension): bool
 	{
+		$extension = str($extension)->lower()->toString();
+		
 		return in_array($extension, [
 			self::HEIC->value,
 			self::HEIF->value,
