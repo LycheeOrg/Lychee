0c7cc6989f026df982f56c7a974a5fd41a815a84
nbalkandzhiyski <nikolay.balkandzhiyski@gmail.com>
2025-12-22 13:19:00 +0200

    Add migration to change 'title' column type to TEXT in photos table because titles over 100 chars throws error when uploading images

---
+++ database/migrations/2025_12_22_105523_change_title_to_text_on_photos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+require_once 'TemporaryModels/OptimizeTables.php';
+
+return new class extends Migration {
+
+	private OptimizeTables $optimize;
+
+	public function __construct()
+	{
+		$this->optimize = new OptimizeTables();
+	}
+
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::table('photos', function (Blueprint $table) {
+			DB::statement('ALTER TABLE `photos` MODIFY `title` TEXT');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::table('photos', function (Blueprint $table) {
+			$table->string('title', 100)->change();
+		});
+	}
+};
