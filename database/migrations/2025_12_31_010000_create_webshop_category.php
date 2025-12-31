8c52fb089b09c833d27958ff1a1bbe3bb0d96cd5
ildyria <beviguier@gmail.com>
2025-12-31 01:26:44 +0100

    more emphasis

--- database/migrations/2025_12_31_010000_create_webshop_category.php
+++ database/migrations/2025_12_31_010000_create_webshop_category.php
@@ -23,9 +23,9 @@ return new class() extends Migration {
 				'cat' => self::CAT,
 				'name' => 'Webshop',
 				'description' => 'This module allows you manage and sell your pictures.<br><br>
-				<span class="pi pi-exclamation-triangle text-orange-500 ltr:mr-2 rtl:ml-2"></span>Lychee is developed under the <a href="https://lycheeorg.dev/license" class="text-primary-400">MIT license</a>.
+				<span class="pi pi-exclamation-triangle text-orange-500 ltr:mr-2 rtl:ml-2"></span><span class="text-muted-color">Lychee is developed under the <a href="https://lycheeorg.dev/license" class="text-primary-400">MIT license</a>.
 				This means that <span class="text-muted-color-emphasis">LycheeOrg is not responsible</span> nor liable <span class="text-muted-color-emphasis">for any issues or losses</span> arising from the use of the webshop module and/or the payment processing capabilities.
-				It is critical that you verify and <span class="text-muted-color-emphasis">ensure that your setup is working correctly and securely before using it in a production environment.',
+				It is critical that you verify and <span class="text-muted-color-emphasis">ensure that your setup is working correctly and securely before using it in a production environment.</span>',
 				'order' => 25,
 			],
 		]);
