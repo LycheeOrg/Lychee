11d2576cd8d2f3458c1f1575a1f4f98d063ca2ee
ildyria <beviguier@gmail.com>
2026-01-01 16:25:57 +0100

    update header to v7

---
+++ database/migrations/2026_01_01_162100_change_header.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2025 LycheeOrg.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class() extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		DB::table('configs')->where('value', '=', 'Lychee v6')->update(['value' => 'Lychee v7']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		DB::table('configs')->where('value', '=', 'Lychee v7')->update(['value' => 'Lychee v6']);
+	}
+};
