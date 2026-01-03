9f62189eff451292bd6f9cc4fd8e33bd520c2794
ildyria <beviguier@gmail.com>
2026-01-03 22:16:45 +0100

    fix jobs count

--- tests/ImageProcessing/Import/ImportFromServerTest.php
+++ tests/ImageProcessing/Import/ImportFromServerTest.php
@@ -83,6 +83,6 @@ class ImportFromServerTest extends BaseApiWithDataTest
 
 		// Check that jobs were created
 		$this->assertGreaterThan(0, $response->json('job_count'));
-		Queue::assertCount($response->json('job_count') + 1); // +1 for the preprocessing job
+		Queue::assertCount($response->json('job_count') + 2); // +2 for the preprocessing job
 	}
 }
