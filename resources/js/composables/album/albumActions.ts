3bd78984ed1be438ec3d244589fc31bc32e05b1c
ildyria <beviguier@gmail.com>
2026-01-08 22:19:53 +0100

    Fix fornt-end

--- resources/js/composables/album/albumActions.ts
+++ resources/js/composables/album/albumActions.ts
@@ -1,13 +1,12 @@
 import { useAlbumStore } from "@/stores/AlbumState";
 
 export function useAlbumActions() {
-	const albumStore = useAlbumStore();
-
 	function canInteractAlbum(album: App.Http.Resources.Models.ThumbAlbumResource): boolean {
 		return album.rights.can_download || album.rights.can_move || album.rights.can_edit || album.rights.can_delete;
 	}
 
 	function canInteractPhoto(): boolean {
+		const albumStore = useAlbumStore();
 		return albumStore.rights?.can_download || albumStore.rights?.can_edit || false;
 	}
 
