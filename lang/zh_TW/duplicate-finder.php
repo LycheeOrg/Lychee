ec95490b713f3893d34367a841fefbde65cafb5f
Ray Lau <raylyc@gmail.com>
2026-03-04 16:13:15 +0000

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 69.2% (9 of 13 strings)
    
    Translation: Lychee/duplicate-finder
    Translate-URL: http://weblate.lycheeorg.dev/projects/lycheeorg/duplicate-finder/zh_Hant/

--- lang/zh_TW/duplicate-finder.php
+++ lang/zh_TW/duplicate-finder.php
@@ -1,26 +1,25 @@
 <?php
-
 return [
     /*
     |--------------------------------------------------------------------------
     | Duplicate Finder Page
     |--------------------------------------------------------------------------
     */
-    'title' => 'Maintenance',
-    'intro' => 'On this page you will find the duplicate pictures found in your database.',
-    'found' => ' duplicates found!',
+    'title' => '維護',
+    'intro' => '你可以在本頁找到重覆的相片。',
+    'found' => ' 發現重覆!',
     'invalid-search' => ' At least the checksum or title condition must be checked.',
     'checksum-must-match' => 'Checksum must match.',
-    'title-must-match' => 'Title must match.',
-    'must-be-in-same-album' => 'Must be in the same album.',
+    'title-must-match' => '標題必需要一致。',
+    'must-be-in-same-album' => '必需要在同一個相本裡面。',
     'columns' => [
-        'album' => 'Album',
-        'photo' => 'Photo',
+        'album' => '相本',
+        'photo' => '相片',
         'checksum' => 'Checksum',
     ],
     'warning' => [
         'no-original-left' => 'No original left.',
-        'keep-one' => 'You selected all duplicates in this group. Please chose at least one duplicate to keep.',
+        'keep-one' => '你在這組裡選擇了所有重覆的。請至少保留一個。',
     ],
-    'delete-selected' => 'Delete selected',
+    'delete-selected' => '刪除已選',
 ];
