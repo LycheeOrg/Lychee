c1e0cc95ad82b1df3666e7b95df93b5c316736bf
Antoine Gademer <antoinegademer@gmail.com>
2026-03-07 19:14:11 +0100

    Solution to #4146 : Adding the option to specify the Microsoft Tenant… (#4151)

--- config/services.php
+++ config/services.php
@@ -96,6 +96,7 @@ return [
 		'client_id' => env('MICROSOFT_CLIENT_ID'),
 		'client_secret' => env('MICROSOFT_CLIENT_SECRET'),
 		'redirect' => env('MICROSOFT_REDIRECT_URI', '/auth/microsoft/redirect'),
+		'tenant' => env('MICROSOFT_TENANT_ID', 'common'),
 	],
 
 	'nextcloud' => [
