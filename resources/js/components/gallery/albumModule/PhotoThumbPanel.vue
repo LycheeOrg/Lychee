8fbb4cbe6d05ae84edfc0b174c2e6a8bcd89a186
ildyria <beviguier@gmail.com>
2026-03-14 22:24:36 +0100

    some more fixes

--- resources/js/components/gallery/albumModule/PhotoThumbPanel.vue
+++ resources/js/components/gallery/albumModule/PhotoThumbPanel.vue
@@ -5,7 +5,7 @@
 		</template>
 		<!-- <div class="flex justify-end" v-if="is_filters_visible"> -->
 		<Collapse :when="is_filters_visible">
-			<AlbumTagFilter v-if="albumStore.modelAlbum" :album-id="albumStore.modelAlbum.id" @apply="handleTagFilterApply" @clear="handleTagFilterClear" />
+			<AlbumTagFilter v-if="albumStore.modelAlbum" :album-id="albumStore.modelAlbum.id" @apply="handleTagFilterApply" @clear="handleTagFilterClear" :key="`tags_list_album${albumStore.modelAlbum.id}`" />
 		</Collapse>
 		<PhotoThumbPanelList
 			v-if="isTimeline === false"
