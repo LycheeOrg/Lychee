aa31217f50d198d37835b4de6ec1d9959b510b92
ildyria <beviguier@gmail.com>
2026-03-22 18:13:47 +0100

    fix table

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -38,6 +38,8 @@ class Person extends Model
 	use ThrowsConsistentExceptions;
 	use ToArrayThrowsNotImplemented;
 
+	protected $table = 'persons';
+
 	/**
 	 * @var string The type of the primary key
 	 */
