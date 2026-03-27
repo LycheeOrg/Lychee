3ca189f2fc58cb75405ba955baecb3f05e8ad565
Martin <39337420+hyazinthh@users.noreply.github.com>
2026-03-27 23:49:35 +0100

    Finalize German translation (#4241)

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -29,14 +29,14 @@ return [
         'nsfw_album_visibility' => 'Sensible Alben standardmäßig sichtbar machen.',
         'nsfw_album_explanation' => 'Wenn das Album öffentlich ist, ist es immer noch zugänglich, es ist nur nicht sichtbar und<b>kann durch Drücken von <kbd>H</kbd></b> angezeigt werden.',
         'cache_enabled' => 'Zwischenspeicherung von Antworten aktivieren.',
-        'cache_enabled_details' => 'Das wird die Antwortzeit von Lychee erheblich beschleunigen.<br> <i class="pi pi-exclamation-triangle text-warning-600 mr-2"></i>Wenn Sie passwortgeschützte Alben verwenden, sollten Sie diese Funktion nicht aktivieren.',
+        'cache_enabled_details' => 'Beschleunigt die Antwortzeit von Lychee erheblich.<br> <i class="pi pi-exclamation-triangle text-warning-600 mr-2"></i>Wenn Sie passwortgeschützte Alben verwenden, sollten Sie diese Funktion nicht aktivieren.',
     ],
     'lychee_se' => [
         'header' => 'Lychee <span class="text-primary-emphasis">SE</span>',
         'call4action' => 'Erhalte exklusive Funktionen und unterstütze die Entwicklung von Lychee. Schalten die <a href="https://lycheeorg.dev/get-supporter-edition/" class="text-primary-500 underline">SE Edition</a> frei.',
         'preview' => 'Vorschau der Lychee SE-Funktionen einschalten',
         'hide_call4action' => 'Lychee SE-Anmeldeformular ausblenden. Ich bin mit Lychee so zufrieden, wie es ist. :)',
-        'hide_warning' => 'Wenn diese Option aktiviert ist, können Sie Ihren Lizenzschlüssel nur über die obige Registerkarte "Mehr" registrieren. Die Änderungen werden beim Neuladen der Seite übernommen.',
+        'hide_warning' => 'Wenn diese Option aktiviert ist, können Sie Ihren Lizenzschlüssel nur über die obige Registerkarte „Mehr“ registrieren. Die Änderungen werden beim Neuladen der Seite übernommen.',
     ],
     'dropbox' => [
         'header' => 'Dropbox',
@@ -76,7 +76,7 @@ return [
         'change_js' => 'JS ändern',
     ],
     'all' => [
-        'old_setting_style' => 'Stil der "Alten Einstellungen"',
+        'old_setting_style' => 'Stil der „Alten Einstellungen“',
         'expert_settings' => 'Experten Modus',
         'change_detected' => 'Einige Einstellungen wurden geändert.',
         'save' => 'Speichern',
