74ff1de682252bcc7012fcc82af1d06286049754
ildyria <beviguier@gmail.com>
2026-04-12 23:46:33 +0200

    more fixes

--- app/Http/Middleware/ConfigIntegrity.php
+++ app/Http/Middleware/ConfigIntegrity.php
@@ -107,6 +107,8 @@ class ConfigIntegrity
 		'contact_form_custom_consent_required',
 		'ai_vision_enabled',
 		'ai_vision_face_enabled',
+		'ai_vision_face_overlay_enabled',
+		'ai_vision_face_overlay_default_visibility',
 		'ai_vision_face_permission_mode',
 		'ai_vision_face_selfie_confidence_threshold',
 		'ai_vision_face_person_is_searchable_default',
