0d6f6cb6d1e00d580e41dc8276f3aa3cc4ab45a4
Michal Heppler <mheppler@redhat.com>
2026-04-15 14:47:26 +0200

    CZ translation of changelogs.php, duplicate-finder.php, flow.php, gallery.php, import_from_server.php, jobs.php, left-menu.php

--- lang/cz/changelogs.php
+++ lang/cz/changelogs.php
@@ -6,6 +6,6 @@ return [
     | Changelogs page
     |--------------------------------------------------------------------------
     */
-    'title' => 'Change logs',
-    'description' => 'This page contains the changelogs up to the latest available version of Lychee. It is voluntarily not limited to the changes on your installed version. With this, we want to give you a better overview of what to expect when you decide to upgrade.',
+    'title' => 'Seznam změn',
+    'description' => 'Tato stránka obsahuje seznam změn až po nejnovější dostupnou verzi Lychee. Úmyslně se neomezuje pouze na změny ve vaší nainstalované verzi. Tímto způsobem vám chceme poskytnout lepší přehled o tom, co můžete očekávat, pokud se rozhodnete provést aktualizaci.',
 ];
