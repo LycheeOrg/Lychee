a45d1413a1d897cf9a88a23f0bb192c6783d0d55
ildyria <beviguier@gmail.com>
2026-06-17 20:59:51 +0200

    add Version 8 gate

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -30,6 +30,12 @@ use Illuminate\Support\Facades\Storage;
  */
 class SettingsController extends Controller
 {
+	const V8_CONFIGS = [
+		'site_logo',
+		'landing_logo',
+		'landing_header_logo',
+	];
+
 	/**
 	 * Fetch all the settings available in Lychee.
 	 *
@@ -49,7 +55,8 @@ class SettingsController extends Controller
 				->when($docker_info->isDocker(), fn ($q) => $q->where('not_on_docker', '!=', true))
 				->when(!$request->verify()->is_supporter() && !$request->configs()->getValueAsBool('enable_se_preview'), fn ($q) => $q->where('level', '=', 0))
 				->when(!$request->verify()->is_pro(), fn ($q) => $q->where('level', '<', 2))
-				->when(config('features.webshop') === false, fn ($q) => $q->where('key', 'NOT LIKE', 'webshop_%')),
+				->when(config('features.webshop') === false, fn ($q) => $q->where('key', 'NOT LIKE', 'webshop_%'))
+				->when(config('features.v8') === false, fn ($q) => $q->whereNotIn('key', self::V8_CONFIGS)),
 		])->orderBy('order', 'asc')->get();
 
 		return ConfigCategoryResource::collect($editable_configs->filter(fn ($cat) => $cat->configs->isNotEmpty())->values());
