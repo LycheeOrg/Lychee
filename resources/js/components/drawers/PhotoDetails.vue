29edb115079b35d0be34f2eb74b99502a13f4b6e
ildyria <beviguier@gmail.com>
2026-06-28 21:53:04 +0200

    use router link

--- resources/js/components/drawers/PhotoDetails.vue
+++ resources/js/components/drawers/PhotoDetails.vue
@@ -72,14 +72,14 @@
 							{{ $t("gallery.photo.details.tags") }}
 						</h2>
 						<span class="pb-2 flex flex-wrap">
-							<a
+							<RouterLink
 								v-for="tag in photoStore.photo.tags"
 								:key="`tag-${tag.id}`"
 								class="text-xs rounded-full py-1 px-2.5 mr-1.5 mb-2.5 bg-black/50 cursor-pointer hover:bg-black/70 transition-colors"
-								@click="router.push({ name: 'tag', params: { tagId: tag.id } })"
+								:to="{ name: 'tag', params: { tagId: tag.id } }"
 							>
 								{{ tag.name }}
-							</a>
+							</RouterLink>
 						</span>
 					</template>
 
