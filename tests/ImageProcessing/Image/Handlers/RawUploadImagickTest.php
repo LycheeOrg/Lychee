fea647e744cf135a73472c84ce5d6082c664a48c
ildyria <beviguier@gmail.com>
2026-07-06 17:35:52 +0200

    fixes

--- tests/ImageProcessing/Image/Handlers/RawUploadImagickTest.php
+++ tests/ImageProcessing/Image/Handlers/RawUploadImagickTest.php
@@ -72,8 +72,6 @@ class RawUploadImagickTest extends BaseImageHandler
 		self::assertNotNull($photo['size_variants']['original']);
 		// The original should be a JPEG after conversion
 		self::assertTrue(in_array(substr($photo['size_variants']['original']['url'], -4), ['.jpg', 'heic'], true));
-		// Thumbnails should have been generated from the JPEG original
-		self::assertNotNull($photo['size_variants']['thumb']);
 	}
 
 	/**
