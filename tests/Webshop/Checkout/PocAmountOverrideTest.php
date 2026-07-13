33af7e19a00d9188c383c26d4240d0ecc29f3b01
ildyria <beviguier@gmail.com>
2026-07-13 18:20:07 +0200

    WRTC

--- tests/Webshop/Checkout/PocAmountOverrideTest.php
+++ tests/Webshop/Checkout/PocAmountOverrideTest.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2026 LycheeOrg.
+ */
+
+/**
+ * We don't care for unhandled exceptions in tests.
+ * It is the nature of a test to throw an exception.
+ * Without this suppression we had 100+ Linter warning in this file which
+ * don't help anything.
+ *
+ * @noinspection PhpDocMissingThrowsInspection
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+
 namespace Tests\Webshop\Checkout;
 
 use App\Enum\OmnipayProviderType;
