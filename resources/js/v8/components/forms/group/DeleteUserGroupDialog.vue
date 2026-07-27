1d1505f58369ce527dfc577530b5f23cd1bf3a82
ildyria <beviguier@gmail.com>
2026-07-27 22:28:23 +0200

    Quick pass on UserGroups

---
+++ resources/js/v8/components/forms/group/DeleteUserGroupDialog.vue
@@ -0,0 +1,52 @@
+<template>
+	<UModal v-model:open="visible" :title="$t('user-groups.confirm_delete_header')" :dismissible="true">
+		<template #body>
+			<p class="text-center text-muted text-wrap">
+				{{ $t("user-groups.confirm_delete_message") }}
+			</p>
+		</template>
+		<template #footer>
+			<div class="flex w-full gap-2">
+				<UButton
+					color="neutral"
+					variant="soft"
+					class="flex-1 justify-center font-bold"
+					@click="
+						() => {
+							visible = false;
+						}
+					"
+				>
+					{{ $t("user-groups.cancel") }}
+				</UButton>
+				<UButton color="error" class="flex-1 justify-center font-bold" @click="execute">
+					{{ $t("user-groups.delete") }}
+				</UButton>
+			</div>
+		</template>
+	</UModal>
+</template>
+<script setup lang="ts">
+import { UserGroupService } from "@/services/user-group-service";
+
+const props = defineProps<{
+	group?: App.Http.Resources.Models.UserGroupResource;
+}>();
+
+const visible = defineModel<boolean>("open", { default: false });
+const emits = defineEmits<{
+	deleted: [];
+}>();
+
+function execute() {
+	visible.value = false;
+
+	if (props.group === undefined) {
+		return;
+	}
+
+	UserGroupService.deleteUserGroup(props.group.id).then(() => {
+		emits("deleted");
+	});
+}
+</script>
