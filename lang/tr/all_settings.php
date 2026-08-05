1468bc3a1426c7b0857bfb58fb2007242f10056c
ildyria <beviguier@gmail.com>
2026-08-05 23:45:40 +0200

    notes on sorting

--- lang/tr/all_settings.php
+++ lang/tr/all_settings.php
@@ -833,7 +833,7 @@ return [
     'category_description' => [
         'config' => '',
         'lychee SE' => 'Unlock the full capabilities of Lychee with the <a href="https://lycheeorg.dev/get-supporter-edition/" class="text-primary-500 font-bold underline">Supporter Edition</a>. Get exclusive features and support the development of Lychee. Sponsor LycheeOrg on GitHub and get your License key <a href="https://github.com/sponsors/LycheeOrg" class="text-primary-500 font-bold underline">here</a>.',
-        'Gallery' => '',
+        'Gallery' => '<span class="text-warning-600 font-bold uppercase">Important:</span> Natural sorting and lexicographical sorting can produce different results.<br><span class="text-muted-color text-muted">Natural: img_1, img_2, img_10.<br>Lexicographical: img_1, img_10, img_2.<br>Lexicographical sorting is performed directly in the database, while natural sorting is performed in PHP after the database query runs. As a result, when natural sorting is combined with pagination, results are fetched from the database in an arbitrary order before being sorted, which can lead to unexpected results when browsing through pages. We recommend using natural sorting for a small number of photos, and lexicographical sorting for a large number of photos. Alternatively, you can prefix your numbers with 0s (e.g. img_01, img_02, img_10) so that both sorting methods produce the same result.</span>',
         'Mod Welcome' => 'This module enables a landing page. Instead of arriving directly to the gallery view, it adds a bit of grandiose to the website entrance. Provide the url of the picture to be displayed and set the title.',
         'Footer' => '',
         'Smart Albums' => '',
