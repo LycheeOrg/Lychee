4ac36bc772780be2fc96e5fa93d67bfc609915f5
Benoît Viguier <ildyria@users.noreply.github.com>
2026-08-30 12:28:28 +0200

    Version 7.8.1 : fix missing album migration (#4682)

---
+++ database/migrations/2026_08_30_093447_fix_sorting_albums.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2017-2018 Tobias Reich
+ * Copyright (c) 2018-2026 LycheeOrg.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class() extends Migration {
+	private const REMOVED_VALUES = ['title_strict', 'description', 'description_strict'];
+
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		DB::table('albums')
+			->whereIn('album_sorting_col', self::REMOVED_VALUES)
+			->update(['album_sorting_col' => 'title']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		// no coming back.
+		// Sowwy
+	}
+};
