df2940d6105d5c7bcc3251e7e63c1bb56847ab3b
ildyria <beviguier@gmail.com>
2026-09-15 19:56:05 +0200

    Fix

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -22,10 +22,10 @@
 <meta property="og:image" content="{{ $image_url }}">
 <meta property="og:url" content="{{ $page_url }}">
 <meta name="twitter:card" content="summary_large_image">
-@if($width !== 0 && $height !== 0) {
-	<meta property="og:image:width" content="{{ $width }}"  data-dynamic="true">
-	<meta property="og:image:height" content="{{ $height }}"  data-dynamic="true">
-}
+@if($width !== 0 && $height !== 0)
+<meta property="og:image:width" content="{{ $width }}"  data-dynamic="true">
+<meta property="og:image:height" content="{{ $height }}"  data-dynamic="true">
+@endif
 
 @if(Features::active('nuxt_ui'))
 <link rel="icon" type="image/svg+xml" href="{{ URL::asset('img/logo4.svg') }}">
